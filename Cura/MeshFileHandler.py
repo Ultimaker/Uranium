37c742c3c13abff14c9ae8639fec9b2459956431
Jaime van Kessel <nallath@gmail.com>
2014-11-10 17:31:37 +0100

    Really the last one

---
+++ Cura/MeshFileHandler.py
@@ -0,0 +1,10 @@
+class MeshFileHandler(object):
+    __init__(self):
+        self._mesh_readers = []
+        self._mesh_writers = []
+        
+    def read(self, file_name):
+        pass
+    
+    def write(self, file_name):
+        pass
