21942050242d5286bf71437524b0a9f2176f2737
Arjen Hiemstra <ahiemstra@heimr.nl>
2014-11-11 13:48:41 +0100

    Support recursive searching for plugins and matching a subset of metadata

--- Cura/PluginRegistry.py
+++ Cura/PluginRegistry.py
@@ -3,6 +3,22 @@ from Cura.PluginError import PluginError, PluginNotFoundError, InvalidMetaDataEr
 import imp
 import os
 
+'''
+\brief A central object to dynamically load modules as plugins.
+
+The PluginRegistry class can load modules dynamically and use
+them as plugins. Each plugin module is expected to be a directory with
+and __init__ file defining a getMetaData and a register function. 
+
+getMetaData should return a dictionary of metadata, with the "name" 
+and "type" keys expected to be set. The register function is passed 
+the application object as parameter and is expected to register the 
+appropriate classes with the appropriate objects.
+
+Plugins can be located in any location listed in the plugin locations.
+The plugin locations are scanned recursively for plugins.
+
+'''
 class PluginRegistry(object):
     def __init__(self):
         self._plugins = {}
@@ -17,8 +33,8 @@ class PluginRegistry(object):
             # Already loaded, do not load again
             return
         
-        if not self._metaData:
-            self._populateMetaData()
+        if name not in self._metaData:
+            self._populateMetaData(name)
         
         plugin = self._findPlugin(name)
         if not plugin:
@@ -32,36 +48,35 @@ class PluginRegistry(object):
         except AttributeError as e:
             print(e)
     
-    # Load a set of plugins of a certain type
-    # \param type The type of plugin to load
-    def loadPlugins(self, type):
-        pluginNames = []
-        for folder in self._pluginLocations:
-            for file in os.listdir(folder):
-                if os.path.isdir(file):
-                    pluginNames.append(file)
-                    
+    # Load all plugins matching a certain set of metadata
+    # \param metaData The metaData that needs to be matched.
+    def loadPlugins(self, metaData):
+        pluginNames = self._findAllPlugins()
+        
         for name in pluginNames:
-            self.loadPlugin(name)
+            pluginData = self.getMetaData(name)
+            
+            if self._subsetInDict(pluginData, metaData):
+                self.loadPlugin(name)
 
     # Get the metadata for a certain plugin
     # \param name The name of the plugin
     def getMetaData(self, name):
-        if not self._metaData:
-            self._populateMetaData()
+        if name not in self._metaData:
+            self._populateMetaData(name)
 
         return self._metaData[name]
     
-    # Get a list of metadata for a certain plugin type
-    # \param type The type of plugin to get metadata for
-    def getAllMetaData(self, type):
-        if not self._metaData:
-            self._populateMetaData()
-            
+    # Get a list of all metadata matching a certain subset of metaData
+    # \param metaData The subset of metadata that should be matched.
+    def getAllMetaData(self, metaData):
+        pluginNames = self._findAllPlugins()
+        
         returnVal = []
-        for data in self._metaData:
-            if type in data and data["type"] == type:
-                returnVal.append(data)
+        for name in pluginNames:
+            pluginData = self.getMetaData(name)
+            if self._subsetInDict(pluginData, metaData):
+                returnVal.append(pluginData)
             
         return returnVal
     
@@ -83,40 +98,43 @@ class PluginRegistry(object):
     
     # Private
     # Populate the list of metadata
-    def _populateMetaData(self):
-        pluginNames = []
-        for folder in self._pluginLocations:
-            #find plugins in folder and load metadata
-            for name in os.listdir(folder):
-                if os.path.isdir(os.path.join(folder, name)):
-                    pluginNames.append(name)
+    def _populateMetaData(self, name):
+        #pluginNames = []
+        #for folder in self._pluginLocations:
+            ##find plugins in folder and load metadata
+            #for name in os.listdir(folder):
+                #if os.path.isdir(os.path.join(folder, name)):
+                    #pluginNames.append(name)
 
-        for name in pluginNames:
-            plugin = self._findPlugin(name)
-            
-            if not plugin:
-                print("Could not load plugin: " + name)
-                continue
-            
-            metaData = None
-            try:
-                metaData = plugin.getMetaData()
-            except AttributeError as e:
-                print(e)
-                continue
-            
-            if not metaData or (not "name" in metaData and not "type" in metaData):
-                raise InvalidMetaDataError(name)
-            
-            self._metaData[name] = plugin.getMetaData()
+        #for name in pluginNames:
+        plugin = self._findPlugin(name)
+        
+        if not plugin:
+            raise InvalidMetaDataError(name)
+        
+        metaData = None
+        try:
+            metaData = plugin.getMetaData()
+        except AttributeError as e:
+            print(e)
+            return
+        
+        if not metaData or (not "name" in metaData and not "type" in metaData):
+            raise InvalidMetaDataError(name)
+        
+        self._metaData[name] = plugin.getMetaData()
             
                 
     # Private
     # Try to find a module implementing a plugin
     # \param name The name of the plugin to find
     def _findPlugin(self, name):
+        location = None
+        for folder in self._pluginLocations:
+            location = self._locatePlugin(name, folder)
+        
         try:
-            file, path, desc = imp.find_module(name, self._pluginLocations)
+            file, path, desc = imp.find_module(name, [ location ])
         except ImportError as e:
             print(e)
             return False
@@ -129,3 +147,51 @@ class PluginRegistry(object):
                         
         return module
     
+    # Private
+    # Returns a list of all possible plugin names in the plugin locations
+    def _findAllPlugins(self, paths = None):
+        names = []
+        
+        if not paths:
+            paths = self._pluginLocations
+        
+        for folder in paths:
+            for file in os.listdir(folder):
+                filepath = os.path.join(folder, file)
+                if os.path.isdir(filepath):
+                    if os.path.isfile(os.path.join(filepath, '__init__.py')):
+                        names.append(file)
+                    else:
+                        names += self._findAllPlugins([ filepath ])
+                        
+        return names
+    
+    # Private
+    # Try to find a directory we can use to load a plugin from
+    # \param name The name of the plugin to locate
+    # \param folder The base folder to look into
+    def _locatePlugin(self, name, folder):
+        for file in os.listdir(folder):
+            filepath = os.path.join(folder, file)
+            if os.path.isdir(filepath):
+                if file == name:
+                    return folder
+                else:
+                    filepath = self._locatePlugin(name, filepath)
+                    if filepath:
+                        return filepath
+        
+        return False
+    
+    # Private
+    # Check if a certain dictionary contains a certain subset of key/value pairs
+    # \param dictionary The dictionary to search
+    # \param subset The subset to search for
+    def _subsetInDict(self, dictionary, subset):
+        for key, value in subset:
+            if key not in dictionary:
+                return False
+            if dictionary[key] != value:
+                return False
+        
+        return True
