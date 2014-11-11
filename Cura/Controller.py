38ce9371127e96ba851315e06c01bb9d83fedc8c
Jaime van Kessel <nallath@gmail.com>
2014-11-11 12:00:13 +0100

    Added view/tool handling code to controller

--- Cura/Controller.py
+++ Cura/Controller.py
@@ -10,6 +10,25 @@ class Controller(object):
         self._input_devices = {}
         self._active_view = None
         self._views = {}
+        
+    def addView(self, name, view):
+        self._views[name] = view
+    
+    def getView(self, name):
+        try:
+            return self._views[name]
+        except KeyError: #No such view
+            return None
+    
+    def getTool(self, name):
+        try:
+            return self._tools[name]
+        except KeyError: #No such tool
+            return None
+        
+    def addTool(self, name, tool):
+        self._tools[name] = tool
+    
 
     def getActiveTool(self):
         return self._active_tool
