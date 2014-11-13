e6a1c2e6069317547a51e5784fcefe58a1852619
Arjen Hiemstra <ahiemstra@heimr.nl>
2014-11-13 16:33:01 +0100

    Add renderMesh to Renderer and start implementing it

--- Cura/View/ClassicGLRenderer.py
+++ Cura/View/ClassicGLRenderer.py
@@ -1,4 +1,27 @@
-import Renderer
+from Cura.View.Renderer import Renderer
+
+from OpenGL.GL import *
+
 class ClassicGLRenderer(Renderer):
     def __init__(self):
         super(Renderer, self).__init__()
+
+    def renderMesh(self, position, mesh):
+        glMatrixMode(GL_MODELVIEW)
+        glPushMatrix()
+
+        glLoadMatrixd(position.getData())
+
+        glBegin(GL_TRIANGLES)
+        vertex_count = mesh.getNumVerts()
+        for index in xrange(vertex_count):
+            vertex = mesh.getVertex(index)
+
+            position = vertex.getPosition()
+            glVertex3f(position.x, position.y, position.z)
+            normal = vertex.getNormal()
+            glNormal3f(normal.x, normal.y, normal.z)
+
+        glEnd()
+
+        glPopMatrix()
--- Cura/View/Renderer.py
+++ Cura/View/Renderer.py
@@ -1,3 +1,6 @@
 class Renderer(object):
     def __init__(self):
+        super(Renderer, self).__init__()
+
+    def renderMesh(self, position, mesh):
         pass
