7facc8e4d4c526d744cc636c152aa0209a8ff424
Jaime van Kessel <nallath@gmail.com>
2014-11-17 16:56:25 +0100

    Added transferMeshCommand

--- Cura/Backend/Command.py
+++ Cura/Backend/Command.py
@@ -1,3 +1,7 @@
 class Command(object):
     def __init__(self):
         super(Command, self).__init__() # Call super to make multiple inheritence work.
+        
+    def exec(self):
+        raise NotImplementedError("Command is not correctly implemented")
+    
