38aa622580d57e05d67d844c6d09edd7c4248c68
Jaime van Kessel <nallath@gmail.com>
2014-11-17 15:37:07 +0100

    For grandfather documentation

--- Cura/Backend.py
+++ Cura/Backend.py
@@ -1,14 +1,16 @@
+##      Base class for any backend communication (seperate piece of software)
 class Backend(object):
     def __init__(self):
-        super(Backend, self).__init__()
+        super(Backend, self).__init__() # Call super to make multiple inheritence work.
         self._supported_commands = {}
         self._socket = None
-    ## Get a list of supported commands of this backend instance.
-    # \returns List of Command objects
+    
+    ##  Get a list of supported commands of this backend instance.
+    #   \returns List of Command objects
     def getSupportedCommands(self):
         return self._supported_commands
     
-    ## Add a command to be supported
-    # \param command The Command to be supported
+    ##  Add a command to be supported
+    #   \param command The Command to be supported
     def addSupportedCommand(self,command):
         pass
--- Cura/Command.py
+++ Cura/Command.py
@@ -1,3 +1,3 @@
 class Command(object):
     def __init__(self):
-        super(Command, self).__init__()
+        super(Command, self).__init__() # Call super to make multiple inheritence work.
