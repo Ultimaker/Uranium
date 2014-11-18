4eb5238df5719c95813424236f8294d887b15e0a
Arjen Hiemstra <ahiemstra@heimr.nl>
2014-11-18 17:58:29 +0100

    Create a Mouse input device and pass events to it in Qt/MainWindow

--- Cura/Qt/MainWindow.py
+++ Cura/Qt/MainWindow.py
@@ -5,6 +5,9 @@ from PyQt5.QtQuick import QQuickWindow, QQuickItem
 from OpenGL import GL
 from OpenGL.GL.GREMEDY.string_marker import *
 
+from Cura.Math.Vector import Vector
+from Cura.Qt.QtMouseDevice import QtMouseDevice
+
 class MainWindow(QQuickWindow):
     def __init__(self, parent = None):
         super(MainWindow, self).__init__(parent)
@@ -15,11 +18,22 @@ class MainWindow(QQuickWindow):
         self.setClearBeforeRendering(False)
         self.beforeRendering.connect(self._render, type=Qt.DirectConnection)
 
+        self._mouseDevice = QtMouseDevice()
+        self._keyboardDevice = None
+
     def getApplication(self):
         return self._app
 
     def setApplication(self, app):
+        if app == self._app:
+            return
+
+        if self._app:
+            self._app.getController().removeInputDevice("Mouse")
+
         self._app = app
+        if self._app:
+            self._app.getController().addInputDevice("Mouse", self._mouseDevice)
 
     application = pyqtProperty(QObject, fget=getApplication, fset=setApplication)
 
@@ -30,6 +44,25 @@ class MainWindow(QQuickWindow):
         self._backgroundColor = color
 
     backgroundColor = pyqtProperty(QColor, fget=getBackgroundColor, fset=setBackgroundColor)
+
+    def event(self, event):
+        super().event(event)
+
+        if event.isAccepted():
+           return True
+
+        e = None
+        if self._mouseDevice:
+            e = self._mouseDevice.handleEvent(event)
+
+        if not e and self._keyboardDevice:
+            e = self._keyboardDevice.handleEvent(event)
+
+        if e:
+            self._app.getController().event(e)
+            return True
+        else:
+            return False
 
     def _render(self):
         if bool(glStringMarkerGREMEDY):
