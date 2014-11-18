825df726b940d995e65c4ea8f100ab6b092a1a1c
Arjen Hiemstra <ahiemstra@heimr.nl>
2014-11-18 17:48:07 +0100

    Add an "activeCamera" getter and setter to Scene

--- Cura/Scene/Scene.py
+++ Cura/Scene/Scene.py
@@ -1,9 +1,17 @@
 from Cura.Scene.SceneObject import SceneObject
+from Cura.Signal import Signal
 
 class Scene(object):
     def __init__(self):
         self._root = SceneObject()
         
+        self._active_camera = None
         
     def getRoot(self):
         return self._root
+
+    def getActiveCamera(self):
+        return self._active_camera
+
+    def setActiveCamera(self, camera):
+        self._active_camera = camera
