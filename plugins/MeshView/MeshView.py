ea39c4ebba3d5ab42dfa202f88f7d76386e505fe
Arjen Hiemstra <ahiemstra@heimr.nl>
2014-11-18 18:03:17 +0100

    Allow SceneObjects to render themselves

--- plugins/MeshView/MeshView.py
+++ plugins/MeshView/MeshView.py
@@ -11,8 +11,9 @@ class MeshView(View):
         self._renderObject(scene.getRoot(), renderer)
 
     def _renderObject(self, object, renderer):
-        if object.getMeshData():
-            renderer.renderMesh(object.getGlobalTransformation(), object.getMeshData())
+        if not object.render():
+            if object.getMeshData():
+                renderer.renderMesh(object.getGlobalTransformation(), object.getMeshData())
 
         for child in object.getChildren():
             self._renderObject(child, renderer)
