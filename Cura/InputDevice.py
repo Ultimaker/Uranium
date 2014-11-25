086a297d690eaa110fc2502c59c0817237a1b488
Arjen Hiemstra <ahiemstra@heimr.nl>
2014-11-25 15:37:02 +0100

    Add an event signal to InputDevice

--- Cura/InputDevice.py
+++ Cura/InputDevice.py
@@ -1,6 +1,11 @@
+from Cura.Signal import Signal
+
 ##  Abstract base class for all input devices
 class InputDevice(object):
     def __init__(self):
         super(InputDevice, self).__init__()
-    
-    
+        self.event = Signal()
+
+    ## Signal. Emitted whenever the device produces an event.
+    #  \param event The event that is emitted.
+    event = None
