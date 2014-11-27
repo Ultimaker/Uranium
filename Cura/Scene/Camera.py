1c25fd5bc530c21875c2975d24ad6e22c6dd5040
Arjen Hiemstra <ahiemstra@heimr.nl>
2014-11-27 14:27:46 +0100

    Add a name to Camera

--- Cura/Scene/Camera.py
+++ Cura/Scene/Camera.py
@@ -6,12 +6,15 @@ from Cura.Math.Matrix import Matrix
 #   The camera provides a projection matrix and its transformation matrix
 #   can be used as view matrix.
 class Camera(SceneNode.SceneNode):
-    def __init__(self, parent = None):
+    def __init__(self, name, parent = None):
         super().__init__(parent)
+        self._name = name
         self._projectionMatrix = Matrix()
         self._projectionMatrix.setOrtho(-5, 5, 5, -5, -100, 100)
         #self._projectionMatrix.setPerspective(45, 1, 0, 5)
-        self._locked = False
+
+    def getName(self):
+        return self._name
 
     ##  Get the projection matrix of this camera.
     def getProjectionMatrix(self):
