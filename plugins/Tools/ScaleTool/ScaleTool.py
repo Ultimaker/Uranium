422e9309abc9ee76c37c5d58ab6f1a5ba6e9501d
Arjen Hiemstra <ahiemstra@heimr.nl>
2014-11-28 10:16:14 +0100

    Add a ScaleTool

---
+++ plugins/Tools/ScaleTool/ScaleTool.py
@@ -0,0 +1,12 @@
+from Cura.Tool import Tool
+from Cura.Event import Event
+
+class ScaleTool(Tool):
+    def __init__(self):
+        super().__init__()
+
+    def event(self, event):
+        if event.type == Event.ToolActivateEvent:
+            print("Activate Scale tool")
+
+        pass
