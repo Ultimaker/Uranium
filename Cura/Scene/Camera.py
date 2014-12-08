f4c786a3fef52753263e7d4fe3811752f2104c4c
Arjen Hiemstra <ahiemstra@heimr.nl>
2014-12-08 17:48:10 +0100

    Add setPerspective and lookAt to Camera

--- Cura/Scene/Camera.py
+++ Cura/Scene/Camera.py
@@ -2,6 +2,8 @@ from . import SceneNode
 
 from Cura.Math.Matrix import Matrix
 
+import numpy
+
 ##  A SceneNode subclass that provides a camera object.
 #
 #   The camera provides a projection matrix and its transformation matrix
@@ -12,6 +14,7 @@ class Camera(SceneNode.SceneNode):
         self._name = name
         self._projectionMatrix = Matrix()
         self._projectionMatrix.setOrtho(-5, 5, 5, -5, -100, 100)
+        self._perspective = False
         #self._projectionMatrix.setPerspective(45, 1, 0, 5)
 
     def getName(self):
@@ -25,3 +28,26 @@ class Camera(SceneNode.SceneNode):
     #   \param matrix The projection matrix to use for this camera.
     def setProjectionMatrix(self, matrix):
         self._projectionMatrix = matrix
+
+    def isPerspective(self):
+        return self._perspective
+
+    def setPerspective(self, pers):
+        self._perspective = pers
+
+    def lookAt(self, center, up):
+        eye = self.getPosition()
+        f = (center - eye).normalize()
+        up.normalize()
+        s = f.cross(up).normalize()
+        u = s.cross(f).normalize()
+
+        m = Matrix(numpy.array([
+            [ s.x,  u.x, -f.x, eye.x],
+            [ s.y,  u.y, -f.y, eye.y],
+            [ s.z,  u.z, -f.z, eye.z],
+            [ 0.0,  0.0,  0.0,   1.0]],
+            dtype=numpy.float32))
+
+        self.setLocalTransformation(m)
+        #self.translate(eye)
