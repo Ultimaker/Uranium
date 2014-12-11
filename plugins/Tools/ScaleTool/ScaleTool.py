41420aa8cbb1fd17da35d2fe6d1846579720a66f
Arjen Hiemstra <ahiemstra@heimr.nl>
2014-12-11 16:43:34 +0100

    Display very basic tool handles when the scale tool is selected

--- plugins/Tools/ScaleTool/ScaleTool.py
+++ plugins/Tools/ScaleTool/ScaleTool.py
@@ -1,13 +1,19 @@
 from Cura.Tool import Tool
 from Cura.Event import Event
 
+from . import ScaleToolHandle
+
 class ScaleTool(Tool):
     def __init__(self):
         super().__init__()
+        self._handle = ScaleToolHandle.ScaleToolHandle()
 
     def event(self, event):
         if event.type == Event.ToolActivateEvent:
-            print("Activate Scale tool")
+            self._handle.setParent(self.getController().getScene().getRoot())
+
+        if event.type == Event.ToolDeactivateEvent:
+            self._handle.setParent(None)
 
     def getIconName(self):
         return 'scale.png'
---
+++ plugins/Tools/ScaleTool/ScaleToolHandle.py
@@ -0,0 +1,14 @@
+from Cura.Scene.ToolHandle import ToolHandle
+
+class ScaleToolHandle(ToolHandle):
+    def __init__(self, parent = None):
+        super().__init__(parent)
+
+        md = self.getMeshData()
+
+        md.addVertex(0, 0, 0)
+        md.addVertex(0, 20, 0)
+        md.addVertex(0, 0, 0)
+        md.addVertex(20, 0, 0)
+        md.addVertex(0, 0, 0)
+        md.addVertex(0, 0, 20)
