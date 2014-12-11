bc66841216e2b4f095c964f911501d14a0e09243
Arjen Hiemstra <ahiemstra@heimr.nl>
2014-12-11 16:38:18 +0100

    Add an addMesh method to ControllerProxy
    This is mostly a convenience shortcut method to add a mesh to the
    current scene.

--- Cura/Qt/Bindings/ControllerProxy.py
+++ Cura/Qt/Bindings/ControllerProxy.py
@@ -1,9 +1,12 @@
-from PyQt5.QtCore import QObject, QCoreApplication, pyqtSlot
+from PyQt5.QtCore import QObject, QCoreApplication, pyqtSlot, QUrl
+
+from Cura.Application import Application
+from Cura.Scene.SceneNode import SceneNode
 
 class ControllerProxy(QObject):
     def __init__(self, parent = None):
         super().__init__(parent)
-        self._controller = QCoreApplication.instance().getController()
+        self._controller = Application.getInstance().getController()
 
     @pyqtSlot(str)
     def setActiveView(self, view):
@@ -12,3 +15,12 @@ class ControllerProxy(QObject):
     @pyqtSlot(str)
     def setActiveTool(self, tool):
         self._controller.setActiveTool(tool)
+
+    @pyqtSlot(QUrl)
+    def addMesh(self, file_name):
+        if not file_name.isValid():
+            return
+
+        mesh = SceneNode(self._controller.getScene().getRoot())
+        app = Application.getInstance()
+        mesh.setMeshData(app.getMeshFileHandler().read(file_name.toLocalFile(), app.getStorageDevice('local')))
