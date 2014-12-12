224f55c3c5573536878d167afdc0d4e1f01a379c
Arjen Hiemstra <ahiemstra@heimr.nl>
2014-12-12 17:05:05 +0100

    Clean up STL reader code slightly

--- plugins/FileHandlers/STLReader/STLReader.py
+++ plugins/FileHandlers/STLReader/STLReader.py
@@ -47,17 +47,27 @@ class STLReader(MeshReader):
                     num_verts += 1
 
         mesh.reserveVertexCount(num_verts)
-        f.seek(5, os.SEEK_SET)
-        num_verts = 0
-        data = [None,None,None]
+        f.seek(0, os.SEEK_SET)
+        vertex = 0
+        face = [None, None, None]
         for lines in f:
             for line in lines.split('\r'):
                 if 'vertex' in line:
-                    data[num_verts] = line.split()[1:]
-                    num_verts += 1
-                    if num_verts == 3:
-                        mesh.addFace(float(data[0][0]), float(data[0][1]), float(data[0][2]), float(data[1][0]), float(data[1][1]), float(data[1][2]), float(data[2][0]), float(data[2][1]), float(data[2][2]))
-                        num_verts = 0
+                    face[vertex] = line.split()[1:]
+                    vertex += 1
+                    if vertex == 3:
+                        mesh.addFace(
+                            float(face[0][0]),
+                            float(face[0][1]),
+                            float(face[0][2]),
+                            float(face[1][0]),
+                            float(face[1][1]),
+                            float(face[1][2]),
+                            float(face[2][0]),
+                            float(face[2][1]),
+                            float(face[2][2])
+                        )
+                        vertex = 0
 
     # Private
     ## Load the STL data from file by consdering the data as Binary.
