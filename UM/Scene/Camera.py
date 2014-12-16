1bccafc0c6e4631f200f364992e15e9a00bf33c7
Arjen Hiemstra <ahiemstra@heimr.nl>
2014-12-16 17:35:43 +0100

    Add a getRay method to Camera that can be used to get a Ray object

--- UM/Scene/Camera.py
+++ UM/Scene/Camera.py
@@ -1,8 +1,11 @@
 from . import SceneNode
 
 from UM.Math.Matrix import Matrix
+from UM.Math.Ray import Ray
+from UM.Math.Vector import Vector
 
 import numpy
+import numpy.linalg
 
 ##  A SceneNode subclass that provides a camera object.
 #
@@ -16,6 +19,8 @@ class Camera(SceneNode.SceneNode):
         self._projectionMatrix.setOrtho(-5, 5, 5, -5, -100, 100)
         self._perspective = False
         #self._projectionMatrix.setPerspective(45, 1, 0, 5)
+
+        self._viewport = [0.0, 0.0, 0.0, 0.0]
 
     def getName(self):
         return self._name
@@ -50,4 +55,33 @@ class Camera(SceneNode.SceneNode):
             dtype=numpy.float32))
 
         self.setLocalTransformation(m)
-        #self.translate(eye)
+
+    ##  Get a ray from the camera into the world.
+    #
+    #   This will create a ray from the camera's origin, passing through (x, y)
+    #   on the near plane and continuing based on the projection matrix.
+    #
+    #   \param x The X coordinate on the near plane this ray should pass through.
+    #   \param y The Y coordinate on the near plane this ray should pass through.
+    #
+    #   \return A Ray object representing a ray from the camera origin through X, Y.
+    #
+    #   \note The near-plane coordinates should be in normalized form, that is within (-1, 1).
+    def getRay(self, x, y):
+        invp = numpy.linalg.inv(self._projectionMatrix.getData().copy())
+        invv = self.getGlobalTransformation().getData()
+
+        near = numpy.array([x, -y, -1.0, 1.0], dtype=numpy.float32)
+        near = numpy.dot(invp, near)
+        near = numpy.dot(invv, near)
+        near = near[0:3] / near[3]
+
+        far = numpy.array([x, -y, 1.0, 1.0], dtype = numpy.float32)
+        far = numpy.dot(invp, far)
+        far = numpy.dot(invv, far)
+        far = far[0:3] / far[3]
+
+        dir = far - near
+        dir /= numpy.linalg.norm(dir)
+
+        return Ray(self.getGlobalPosition(), Vector(-dir[0], -dir[1], -dir[2]))
