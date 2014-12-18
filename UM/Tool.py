5bcbb5e246857b890bddd5482cd1a9e6dd2fe04f
Arjen Hiemstra <ahiemstra@heimr.nl>
2014-12-18 12:12:48 +0100

    Add a name to Tool and remove the getIconName in favor of putting that information in the plugin metadata

--- UM/Tool.py
+++ UM/Tool.py
@@ -1,20 +1,21 @@
 ##  Abstract base class for tools that manipulate the scene.
 #
 class Tool(object):
-    def __init__(self):
+    def __init__(self, name):
         super(Tool, self).__init__() # Call super to make multiple inheritence work.
+        self._name = name
         self._renderer = None
         self._controller = None
+
+    ##  Get the name of this tool.
+    def getName(self):
+        return self._name
 
     ##  Handle an event.
     #   \param event \type{Event} The event to handle.
     #   \sa Event
     def event(self, event):
         pass
-
-    ##  Return the name of the icon to use for this tool.
-    def getIconName(self):
-        return 'default.png'
 
     def getController(self):
         return self._controller
