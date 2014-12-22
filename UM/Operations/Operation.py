74e739def286e411fb46ace1128d98aa2e3e24ec
Arjen Hiemstra <ahiemstra@heimr.nl>
2014-12-22 10:58:23 +0100

    Implement mergeWith for TranslateOperation

--- UM/Operations/Operation.py
+++ UM/Operations/Operation.py
@@ -1,6 +1,9 @@
+import time
+
 class Operation:
     def __init__(self):
         super().__init__()
+        self._timestamp = time.time()
 
     def undo(self):
         raise NotImplementedError("Undo should be reimplemented by subclasses")
@@ -9,4 +12,4 @@ class Operation:
         raise NotImplementedError("Redo should be reimplemented by subclasses")
 
     def mergeWith(self, other):
-        pass
+        return False
--- UM/Operations/TranslateOperation.py
+++ UM/Operations/TranslateOperation.py
@@ -11,3 +11,22 @@ class TranslateOperation(Operation.Operation):
 
     def redo(self):
         self._node.translate(self._translation)
+
+    def mergeWith(self, other):
+        if type(other) is not TranslateOperation:
+            return False
+
+        if other._node != self._node:
+            return False
+
+        if self._timestamp - other._timestamp > self._mergeWindow:
+            return False
+
+        return TranslateOperation(self._node, self._translation + other._translation)
+
+    def __repr__(self):
+        return "TranslateOperation(node = {0}, translation={1})".format(self._node, self._translation)
+
+    ## private:
+
+    _mergeWindow = 1.0 #If the time between this operation and a different operation is less than this they can be merged.
