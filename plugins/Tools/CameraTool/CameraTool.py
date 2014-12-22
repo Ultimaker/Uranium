eaf424e70f14817c5f5f4fda8970a96d5c7c31b9
Arjen Hiemstra <ahiemstra@heimr.nl>
2014-12-22 15:11:25 +0100

    Add a method to set origin and zoom range to CameraTool

--- plugins/Tools/CameraTool/CameraTool.py
+++ plugins/Tools/CameraTool/CameraTool.py
@@ -15,13 +15,23 @@ class CameraTool(Tool):
         self._pitch = 0
         self._zoom = 5.0
         self._origin = Vector(0, 0, 0)
-        self._minZoom = 5.0
-        self._maxZoom = 250.0
+        self._minZoom = 10.0
+        self._maxZoom = 300.0
+
+    def setZoomRange(self, min, max):
+        self._minZoom = min
+        self._maxZoom = max
+
+    def setOrigin(self, origin):
+        translation = origin - self._origin
+        self._origin = origin
+        self._scene.getActiveCamera().translate(translation)
+        self._rotateCamera(0.0, 0.0)
 
     def event(self, event):
         if type(event) is MouseEvent:
             if MouseEvent.RightButton in event.buttons:
-                self._rotateCamera(event)
+                self._rotateCamera(event.deltaX, event.deltaY)
                 return True
             elif MouseEvent.MiddleButton in event.buttons:
                 self._moveCamera(event)
@@ -54,12 +64,16 @@ class CameraTool(Tool):
 
         delta = event.vertical / 10.0
         r = (camera.getGlobalPosition() - self._origin).length() - delta
-        if r > self._minZoom and r < self._maxZoom:
-            camera.translate(Vector(0.0, 0.0, -delta))
+        if delta > 0:
+            if r > self._minZoom:
+                camera.translate(Vector(0.0, 0.0, -delta))
+        else:
+            if r < self._maxZoom:
+                camera.translate(Vector(0.0, 0.0, -delta))
 
 
     #   Rotates the camera in response to a mouse event.
-    def _rotateCamera(self, event):
+    def _rotateCamera(self, x, y):
         camera = self._scene.getActiveCamera()
         if not camera:
             return
@@ -67,8 +81,8 @@ class CameraTool(Tool):
         if camera.isLocked():
             return
 
-        dx = event.deltaX
-        dy = event.deltaY
+        dx = x
+        dy = y
 
         diff = camera.getGlobalPosition() - self._origin
         r = diff.length()
@@ -83,10 +97,10 @@ class CameraTool(Tool):
         # Limit the vertical rotation by calculating the dot product between the
         # new vector and the up vector then checking if that is within a certain
         # threshold. If not, perform the rotation only along the Y axis.
-        d = n.getNormalized().dot(Vector.Unit_Y)
-        if d < 0.05 or d > math.pi * 0.3:
-            m.setByRotationAxis(dx, Vector.Unit_Y)
-            n = diff.multiply(m)
+        #d = n.getNormalized().dot(Vector.Unit_Y)
+        #if d < 0.05 or d > math.pi * 0.3:
+            #m.setByRotationAxis(dx, Vector.Unit_Y)
+            #n = diff.multiply(m)
 
         camera.setPosition(n)
         camera.lookAt(self._origin, Vector(0, 1, 0))
