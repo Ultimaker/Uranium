53ced7675a2ecfeb7ac45340492cfde55aff6a11
Arjen Hiemstra <ahiemstra@heimr.nl>
2014-12-24 14:20:37 +0100

    Use a property to store the vertex/index buffers on the mesh so it gets cleaned up after the mesh is removed

--- UM/Qt/QtGL2Renderer.py
+++ UM/Qt/QtGL2Renderer.py
@@ -12,6 +12,9 @@ from . import QtGL2Material
 import numpy
 from ctypes import c_void_p
 
+vertexBufferProperty = '__qtgl2_vertex_buffer'
+indexBufferProperty = '__qtgl2_index_buffer'
+
 ##  A Renderer implementation using OpenGL2 to render.
 class QtGL2Renderer(Renderer):
     def __init__(self):
@@ -30,6 +33,8 @@ class QtGL2Renderer(Renderer):
         self._solidsQueue = []
         self._transparentQueue = []
         self._overlayQueue = []
+
+        self._camera = None
 
     def createMaterial(self, vert, frag):
         mat = QtGL2Material.QtGL2Material(self)
@@ -57,7 +62,8 @@ class QtGL2Renderer(Renderer):
         self._gl.glClearColor(self._backgroundColor.redF(), self._backgroundColor.greenF(), self._backgroundColor.blueF(), self._backgroundColor.alphaF())
         self._gl.glClear(self._gl.GL_COLOR_BUFFER_BIT | self._gl.GL_DEPTH_BUFFER_BIT)
 
-        self._gl.glPointSize(2)
+        if not QOpenGLContext.currentContext().isOpenGLES():
+            self._gl.glPointSize(2)
 
         self._solidsQueue.clear()
         self._transparentQueue.clear()
@@ -73,11 +79,10 @@ class QtGL2Renderer(Renderer):
             queueItem['mode'] = self._gl.GL_LINES
         elif mode is Renderer.RenderPoints:
             queueItem['mode'] = self._gl.GL_POINTS
-        elif mode is Renderer.RenderWireframe:
-            queueItem['mode'] = self._gl.GL_TRIANGLES
-            queueItem['wireframe'] = True
         else:
             queueItem['mode'] = self._gl.GL_TRIANGLES
+
+        queueItem['wireframe'] = (mode == Renderer.RenderWireframe)
 
         if kwargs.get('transparent', False):
             self._transparentQueue.append(queueItem)
@@ -91,6 +96,11 @@ class QtGL2Renderer(Renderer):
         self._gl.glDepthFunc(self._gl.GL_LESS)
         self._gl.glDepthMask(self._gl.GL_TRUE)
         self._gl.glEnable(self._gl.GL_CULL_FACE)
+
+        self._camera = Application.getInstance().getController().getScene().getActiveCamera()
+        if not self._camera:
+            Logger.log("e", "No active camera set, can not render")
+            return
 
         for item in self._solidsQueue:
             self._renderItem(item)
@@ -134,47 +144,12 @@ class QtGL2Renderer(Renderer):
         mode = item['mode']
         wireframe = item.get('wireframe', False)
 
-        if mesh not in self._vertexBufferCache:
-            ##TODO: Empty the cache when the meshdata gets destroyed
-            vertexBuffer = QOpenGLBuffer(QOpenGLBuffer.VertexBuffer)
-            vertexBuffer.create()
-            vertexBuffer.bind()
-            vertices = mesh.getVerticesAsByteArray()
-
-            if mesh.hasNormals():
-                normals = mesh.getNormalsAsByteArray()
-                ##Number of vertices * number of components (3 for vertex, 3 for normal) * size of 32-bit float (4)
-                vertexBuffer.allocate(mesh.getVertexCount() * 6 * 4)
-                vertexBuffer.write(0, vertices, len(vertices))
-                vertexBuffer.write(len(vertices), normals, len(normals))
-            else:
-                vertexBuffer.allocate(vertices, mesh.getVertexCount() * 3 * 4)
-
-            vertexBuffer.release()
-            self._vertexBufferCache[mesh] = vertexBuffer
-
-            if mesh.hasIndices():
-                index_buffer = QOpenGLBuffer(QOpenGLBuffer.IndexBuffer)
-                index_buffer.create()
-                index_buffer.bind()
-
-                data = mesh.getIndicesAsByteArray()
-                index_buffer.allocate(data, len(data))
-                index_buffer.release()
-                self._indexBufferCache[mesh] = index_buffer
-
-        camera = Application.getInstance().getController().getScene().getActiveCamera()
-        if not camera:
-            Logger.log("e", "No active camera set, can not render")
-            return
-
         material.bind()
-
-        material.setUniformValue("u_projectionMatrix", camera.getProjectionMatrix(), cache = False)
-        material.setUniformValue("u_viewMatrix", camera.getGlobalTransformation().getInverse(), cache = False)
-        material.setUniformValue("u_viewPosition", camera.getGlobalPosition(), cache = False)
+        material.setUniformValue("u_projectionMatrix", self._camera.getProjectionMatrix(), cache = False)
+        material.setUniformValue("u_viewMatrix", self._camera.getGlobalTransformation().getInverse(), cache = False)
+        material.setUniformValue("u_viewPosition", self._camera.getGlobalPosition(), cache = False)
         material.setUniformValue("u_modelMatrix", transform, cache = False)
-        material.setUniformValue("u_lightPosition", camera.getGlobalPosition(), cache = False)
+        material.setUniformValue("u_lightPosition", self._camera.getGlobalPosition(), cache = False)
 
         if mesh.hasNormals():
             normalMatrix = transform
@@ -183,11 +158,17 @@ class QtGL2Renderer(Renderer):
             normalMatrix = normalMatrix.getInverse().getTransposed()
             material.setUniformValue("u_normalMatrix", normalMatrix, cache = False)
 
-        vertexBuffer = self._vertexBufferCache[mesh]
+        try:
+            vertexBuffer = getattr(mesh, vertexBufferProperty)
+        except AttributeError:
+            vertexBuffer =  self._createVertexBuffer(mesh)
         vertexBuffer.bind()
 
         if mesh.hasIndices():
-            indexBuffer = self._indexBufferCache[mesh]
+            try:
+                indexBuffer = getattr(mesh, indexBufferProperty)
+            except AttributeError:
+                indexBuffer = self._createIndexBuffer(mesh)
             indexBuffer.bind()
 
         material.enableAttribute("a_vertex", Vector, 0)
@@ -214,3 +195,35 @@ class QtGL2Renderer(Renderer):
             indexBuffer.release()
 
         material.release()
+
+    def _createVertexBuffer(self, mesh):
+        buffer = QOpenGLBuffer(QOpenGLBuffer.VertexBuffer)
+        buffer.create()
+        buffer.bind()
+        vertices = mesh.getVerticesAsByteArray()
+
+        if mesh.hasNormals():
+            normals = mesh.getNormalsAsByteArray()
+            ##Number of vertices * number of components (3 for vertex, 3 for normal) * size of 32-bit float (4)
+            buffer.allocate(mesh.getVertexCount() * 6 * 4)
+            buffer.write(0, vertices, len(vertices))
+            buffer.write(len(vertices), normals, len(normals))
+        else:
+            buffer.allocate(vertices, mesh.getVertexCount() * 3 * 4)
+        buffer.release()
+
+        setattr(mesh, vertexBufferProperty, buffer)
+        return buffer
+
+    def _createIndexBuffer(self, mesh):
+        buffer = QOpenGLBuffer(QOpenGLBuffer.IndexBuffer)
+        buffer.create()
+        buffer.bind()
+
+        data = mesh.getIndicesAsByteArray()
+        buffer.allocate(data, len(data))
+        buffer.release()
+
+        setattr(mesh, indexBufferProperty, buffer)
+        return buffer
+
