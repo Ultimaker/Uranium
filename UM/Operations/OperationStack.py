9f41e14c2b39887277037a3bc36cc8ef31ad52cc
Arjen Hiemstra <ahiemstra@heimr.nl>
2015-01-06 17:18:15 +0100

    Expand documentation of OperationStack and add some signals

--- UM/Operations/OperationStack.py
+++ UM/Operations/OperationStack.py
@@ -1,13 +1,27 @@
+from UM.Signal import Signal, SignalEmitter
+
 import threading
 
-class OperationStack:
+##  A stack of operations
+class OperationStack(SignalEmitter):
     def __init__(self):
         self._operations = []
         self._current_index = -1
         self._lock = threading.Lock()
 
+    ##  Push an operation on the stack.
+    #
+    #   This will perform the follwing things in sequence:
+    #   - If the current index is pointing to an item lower in the stack than the top,
+    #     remove all operations from the current index to the top.
+    #   - Append the operation to the stack.
+    #   - Call redo() on the operation.
+    #   - Perform merging of operations.
+    #
+    #   \param operation \type{Operation} The operation to push onto the stack.
     def push(self, operation):
-        self._lock.acquire(True, 1)
+        if not self._lock.acquire(False):
+            return
 
         try:
             if self._current_index < len(self._operations) - 1:
@@ -18,25 +32,43 @@ class OperationStack:
             self._current_index += 1
 
             self._doMerge()
+
+            self.changed.emit()
         finally:
             self._lock.release()
 
+    ##  Undo the current operation.
+    #
+    #   This will call undo() on the current operation and decrement the current index.
     def undo(self):
         with self._lock:
             if self._current_index >= 0 and self._current_index < len(self._operations):
                 self._operations[self._current_index].undo()
                 self._current_index -= 1
+                self.changed.emit()
 
+    ##  Redo the next operation.
+    #
+    #   This will call redo() on the current operation and increment the current index.
     def redo(self):
         with self._lock:
             n = self._current_index + 1
             if n >= 0 and n < len(self._operations):
                 self._operations[n].redo()
                 self._current_index += 1
+                self.changed.emit()
 
     def getOperations(self):
         with self._lock:
             return self._operations
+
+    def canUndo(self):
+        return self._current_index >= 0
+
+    def canRedo(self):
+        return self._current_index < len(self._operations - 1)
+
+    changed = Signal()
 
     ## private:
 
