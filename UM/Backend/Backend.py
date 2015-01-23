28c4498ebda503c775914629fa71a9d3fc380520
Arjen Hiemstra <ahiemstra@heimr.nl>
2015-01-23 11:19:07 +0100

    Use libArcus for backend communication

--- UM/Backend/Backend.py
+++ UM/Backend/Backend.py
@@ -1,34 +1,35 @@
-from UM.Backend.CommandFactory import CommandFactory
-from UM.Backend.SocketThread import SocketThread
+from UM.Backend.SignalSocket import SignalSocket
 from UM.Preferences import Preferences
 from UM.Logger import Logger
+from UM.Signal import Signal, SignalEmitter
+
 import struct
 import subprocess
 from time import sleep
-from UM.Backend.SocketThread import ClientReply
+
 
 ##      Base class for any backend communication (seperate piece of software).
-#       It uses the command_handlers to know what to do with each command. 
-#       The command_handler dict should be filled with id, function pairs (where the function requres a byte stream to be passed to it)
-#       The ID is formed by the first 4 bits of the message. 
-class Backend(object):
+#       It makes use of the Socket class from libArcus for the actual communication bits.
+#       The message_handler dict should be filled with message class, function pairs.
+class Backend(SignalEmitter):
     def __init__(self,):
-        super(Backend, self).__init__() # Call super to make multiple inheritence work.
+        super().__init__() # Call super to make multiple inheritence work.
         self._supported_commands = {}
-        self._command_factory = CommandFactory()
-        
-        self._socket_thread = SocketThread()
-        self._socket_thread.start()
-       
-        self._socket_thread.connectTo('127.0.0.1' , 0xC20A)
-        self._command_handlers = {}
-        self._socket_thread.socketOpen.connect(self.startEngine)
-        self._socket_thread.replyAdded.connect(self.handleNextReply)
+
+        self._message_handlers = {}
+
+        self._socket = SignalSocket()
+        self._socket.stateChanged.connect(self._onSocketStateChanged)
+        self._socket.messageReceived.connect(self._onMessageReceived)
+        self._socket.error.connect(self._onSocketError)
+
+        self._socket.listen('127.0.0.1', 0xC20A)
 
         self._process = None
+
+    processingProgress = Signal()
     
-    
-    ##   \brief Start the backend / engine. 
+    ##   \brief Start the backend / engine.
     #   Runs the engine, this is only called when the socket is fully opend & ready to accept connections
     def startEngine(self):
         try:
@@ -36,34 +37,7 @@ class Backend(object):
         except FileNotFoundError as e:
             Logger.log('e', "Unable to find backend executable")
     
-    ##   Parse the next reply and handle it (based on command_handlers)
-    def handleNextReply(self):
-        self.interpretData(self.recieveData())
-    
-    ##  Interpret a byte stream as a command. 
-    #   Based on the command_id (the fist 4 bits of the message) a different action will be taken.
-    #   \param data byte stream to interpret
-    #   \returns None if command was not recognised, result of command if it was (can still be None!)
-    def interpretData(self, data):
-        data_id = struct.unpack('i', data[0:4])[0]
-        if data_id in self._command_handlers:
-            return self._command_handlers[data_id](data[4:len(data)])
-        else:
-            Logger.log('e', "Command type %s not recognised" % (data_id))
-            return None
-    
-    ##  \brief Recieve a single package of data (this should be a 'full' command)
-    def recieveData(self):
-        while True:
-            reply = self._socket_thread.getNextReply()
-            if reply.type is ClientReply.SUCCESS:
-                if reply.data is not None:
-                    return reply.data
-            if reply.type is ClientReply.ERROR:
-                print("An error occured with connection with message: " + str(reply.data))
-                return None
-    
-    ##  \brief Convert byte array containing 3 floats per vertex  
+    ##  \brief Convert byte array containing 3 floats per vertex
     def convertBytesToVerticeList(self, data):
         result = []
         if not (len(data) % 12):
@@ -73,7 +47,7 @@ class Backend(object):
                 return result
         else:
             Logger.log('e', "Data length was incorrect for requested type")
-            return None            
+            return None
     
     ##  \brief Convert byte array containing 6 floats per vertex
     def convertBytesToVerticeWithNormalsList(self,data):
@@ -100,3 +74,22 @@ class Backend(object):
             kwargs['startupinfo'] = su
             kwargs['creationflags'] = 0x00004000 #BELOW_NORMAL_PRIORITY_CLASS
         return subprocess.Popen(command_list, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, **kwargs)
+
+    def _onSocketStateChanged(self, state):
+        if state == SignalSocket.ListeningState:
+            #self.startEngine()
+            pass
+        elif state == SignalSocket.ConnectedState:
+            print('Socket connected')
+
+    def _onMessageReceived(self):
+        message = self._socket.takeNextMessage()
+
+        if type(message) not in self._message_handlers:
+            Logger.log('e', "No handler defined for message of type %s", type(message))
+            return
+
+        self._message_handlers[type(message)](message)
+
+    def _onSocketError(self, error):
+        Logger.log('e', error)
