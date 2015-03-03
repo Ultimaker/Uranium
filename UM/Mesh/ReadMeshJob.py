5cb30c0e96d99b96a0c52649d8a5fb8c2ccf394a
Arjen Hiemstra <ahiemstra@heimr.nl>
2015-03-03 18:01:15 +0100

    Rename LoadMeshJob to ReadMeshJob

--- UM/Mesh/ReadMeshJob.py
+++ UM/Mesh/ReadMeshJob.py
@@ -6,12 +6,12 @@ import os.path
 ##  A Job subclass that performs mesh loading.
 #
 #   The result of this Job is a MeshData object.
-class LoadMeshJob(Job):
+class ReadMeshJob(Job):
     def __init__(self, filename):
         super().__init__(description = "Loading mesh {0}".format(os.path.basename(filename)), visible = True)
         self._filename = filename
         self._handler = Application.getInstance().getMeshFileHandler()
-        self._device = Application.getInstance().getStorageDevice('local')
+        self._device = Application.getInstance().getStorageDevice('LocalFileStorage')
 
     def getFileName(self):
         return self._filename
