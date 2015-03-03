eb8ab9649cf801c113ee80d9ef2a08bc5746e104
Arjen Hiemstra <ahiemstra@heimr.nl>
2015-03-03 12:35:13 +0100

    Cleanup documentation and naming relating to plugins
    "Name" is replaced with id so we can distinguish between what should be displayed
    and what should be used to reference plugins.

--- UM/PluginRegistry.py
+++ UM/PluginRegistry.py
@@ -10,13 +10,9 @@ import os
 #   them as plugins. Each plugin module is expected to be a directory with
 #   and `__init__` file defining a `getMetaData` and a `register` function.
 #
-#   `getMetaData` should return a dictionary of metadata, with the "name"
-#   and "type" keys expected to be set. The register function is passed
-#   the application object as parameter and is expected to register the
-#   appropriate classes with the appropriate objects.
+#   For more details, see the [plugins] file.
 #
-#   Plugins can be located in any location listed in the plugin locations.
-#   The plugin locations are scanned recursively for plugins.
+#   [plugins]: docs/plugins.md
 class PluginRegistry(object):
     def __init__(self):
         super(PluginRegistry,self).__init__() # Call super to make multiple inheritence work.
@@ -25,52 +21,56 @@ class PluginRegistry(object):
         self._plugin_locations = []
         self._application = None
         self._active_plugins = []
-    
-    #   Check if all required plugins are loaded. 
-    #   \param required_plugins List of keys of all plugins that must be activated.
-    def checkRequiredPlugins(self,required_plugins):
-        plugin_names = self._findAllPlugins()
-        for name in required_plugins:
-            if name not in plugin_names:
-                Logger.log('e', "Plugin %s is required, but not added or loaded",name)
+
+    ##  Check if all required plugins are loaded.
+    #   \param required_plugins List of ids of plugins that ''must'' be activated.
+    def checkRequiredPlugins(self, required_plugins):
+        plugins = self._findAllPlugins()
+        for id in required_plugins:
+            if id not in plugins:
+                Logger.log('e', "Plugin %s is required, but not added or loaded", id)
                 return False
         return True
-    
+
+    ##  Get the list of active plugins.
     def getActivePlugins(self):
         return self._active_plugins
-    
-    ##  Remove pugin name from active list
-    def removeActivePlugin(self,name):
-        if name in self._active_plugins:
-            self._active_plugins.remove(name)
-    
-    ##  Set a plugin to active
-    def addActivePlugin(self, name):
-        if name not in self._active_plugins:
-            self._active_plugins.append(name)
-    
-    ##  Load a single plugin by name
-    #   \param name \type{string} The name of the plugin
+
+    ##  Remove plugin from the list of active plugins.
+    #
+    #   \param id The id of the plugin to remove.
+    def removeActivePlugin(self, id):
+        if id in self._active_plugins:
+            self._active_plugins.remove(id)
+
+    ##  Add a plugin to the list of active plugins.
+    #
+    #   \param id The id of the plugin to add.
+    def addActivePlugin(self, id):
+        if id not in self._active_plugins:
+            self._active_plugins.append(id)
+
+    ##  Load a single plugin by id
+    #   \param id \type{string} The ID of the plugin, i.e. its directory name.
     #   \exception PluginNotFoundError Raised when the plugin could not be found.
-    def loadPlugin(self, name):
-        if name in self._plugins:
+    def loadPlugin(self, id):
+        if id in self._plugins:
             # Already loaded, do not load again
-            if(self._application is not None):
-                Logger.log('w', 'Plugin %s was already loaded',name)
+            Logger.log('w', 'Plugin %s was already loaded', id)
             return
-        
-        plugin = self._findPlugin(name)
+
+        plugin = self._findPlugin(id)
         if not plugin:
-            raise PluginNotFoundError(name)
-        
-        if name not in self._meta_data:
-            self._populateMetaData(name)
-            
+            raise PluginNotFoundError(id)
+
+        if id not in self._meta_data:
+            self._populateMetaData(id)
+
         try:
             plugin.register(self._application)
-            self.addActivePlugin(self.getMetaData(name)["name"]) #Use the name in meta data
-            Logger.log('i', 'Loaded plugin %s', name)
-            self._plugins[name] = plugin
+            self._plugins[id] = plugin
+            self.addActivePlugin(id)
+            Logger.log('i', 'Loaded plugin %s', id)
         except PluginError as e:
             Logger.log('e', str(e))
         except AttributeError as e:
@@ -80,50 +80,49 @@ class PluginRegistry(object):
     #   \param metaData \type{dict} The metaData that needs to be matched.
     #   \sa loadPlugin
     def loadPlugins(self, meta_data):
-        plugin_names = self._findAllPlugins()
-        
-        for name in plugin_names:
-            plugin_data = self.getMetaData(name)
-            
+        plugins = self._findAllPlugins()
+
+        for id in plugins:
+            plugin_data = self.getMetaData(id)
             if self._subsetInDict(plugin_data, meta_data):
-                self.loadPlugin(name)
+                self.loadPlugin(id)
 
     ##  Get the metadata for a certain plugin
-    #   \param name \type{string} The name of the plugin
+    #   \param id \type{string} The ID of the plugin
     #   \return \type{dict} The metadata of the plugin. Can be an empty dict.
     #   \exception InvalidMetaDataError Raised when no metadata can be found or the metadata misses the right keys.
-    def getMetaData(self, name):
-        if name not in self._meta_data:
-            if not self._populateMetaData(name):
+    def getMetaData(self, id):
+        if id not in self._meta_data:
+            if not self._populateMetaData(id):
                 return {}
 
-        return self._meta_data[name]
-    
+        return self._meta_data[id]
+
     ##  Get a list of all metadata matching a certain subset of metaData
     #   \param metaData \type{dict} The subset of metadata that should be matched.
     #   \sa getMetaData
     def getAllMetaData(self, meta_data):
-        plugin_names = self._findAllPlugins()
         
+        plugins = self._findAllPlugins()
         return_values = []
-        for name in plugin_names:
-            plugin_data = self.getMetaData(name)
             if self._subsetInDict(plugin_data, meta_data):
+        for id in plugins:
+            plugin_data = self.getMetaData(id)
                 return_values.append(plugin_data)
-            
+
         return return_values
-    
+
     ##  Get the list of plugin locations
     #   \return \type{list} The plugin locations
     def getPluginLocations(self):
         return self._plugin_locations
-    
+
     ##  Add a plugin location to the list of locations to search
     #   \param location \type{string} The location to add to the list
     def addPluginLocation(self, location):
         #TODO: Add error checking!
         self._plugin_locations.append(location)
-        
+
     ##  Set the central application object
     #   This is used by plugins as a central access point for other objects
     #   \param app \type{Application} The application object to use
@@ -140,86 +139,89 @@ class PluginRegistry(object):
     ## private:
 
     #   Populate the list of metadata
-    def _populateMetaData(self, name):
-        plugin = self._findPlugin(name)
+    def _populateMetaData(self, id):
+        plugin = self._findPlugin(id)
         if not plugin:
-            Logger.log('e', 'Could not find plugin %s', name)
+            Logger.log('e', 'Could not find plugin %s', id)
             return False
-        
+
         meta_data = None
         try:
             meta_data = plugin.getMetaData()
         except AttributeError as e:
             print(e)
-            raise InvalidMetaDataError(name)
+            raise InvalidMetaDataError(id)
 
-        if not meta_data or (not "name" in meta_data and not "type" in meta_data):
-            raise InvalidMetaDataError(name)
+        if not meta_data or not "type" in meta_data:
+            raise InvalidMetaDataError(id)
 
-        self._meta_data[name] = meta_data
+        meta_data['id'] = id
+
+
+        self._meta_data[id] = meta_data
         return True
 
     #   Try to find a module implementing a plugin
     #   \param name The name of the plugin to find
     #   \returns module if it was found None otherwise
-    def _findPlugin(self, name):
+    def _findPlugin(self, id):
         location = None
         for folder in self._plugin_locations:
-            location = self._locatePlugin(name, folder)
+            location = self._locatePlugin(id, folder)
 
         if not location:
             return None
 
         try:
-            file, path, desc = imp.find_module(name, [ location ])
+            file, path, desc = imp.find_module(id, [ location ])
         except ImportError as e:
-            Logger.log('e', "Import error when importing %s: %s", name, e)
+            Logger.log('e', "Import error when importing %s: %s", id, str(e))
             return False
 
         try:
-            module = imp.load_module(name, file, path, desc)
+            module = imp.load_module(id, file, path, desc)
         except ImportError as e:
-            Logger.log('e', "Import error loading module %s: %s", name, e)
+            Logger.log('e', "Import error loading module %s: %s", id, str(e))
             return False
         finally:
             if file:
                 os.close(file)
 
         return module
-    
-    #   Returns a list of all possible plugin names in the plugin locations
+
+    #   Returns a list of all possible plugin ids in the plugin locations
     def _findAllPlugins(self, paths = None):
-        names = []
-        
+        ids = []
+
         if not paths:
             paths = self._plugin_locations
-        
+
         for folder in paths:
             for file in os.listdir(folder):
                 filepath = os.path.join(folder, file)
                 if os.path.isdir(filepath):
                     if os.path.isfile(os.path.join(filepath, '__init__.py')):
-                        names.append(file)
+                        ids.append(file)
                     else:
-                        names += self._findAllPlugins([ filepath ])
-        return names
-    
+                        ids += self._findAllPlugins([ filepath ])
+        return ids
+
     #   Try to find a directory we can use to load a plugin from
-    #   \param name The name of the plugin to locate
+    #   \param id The id of the plugin to locate
     #   \param folder The base folder to look into
-    def _locatePlugin(self, name, folder):
+    def _locatePlugin(self, id, folder):
         for file in os.listdir(folder):
             filepath = os.path.join(folder, file)
             if os.path.isdir(filepath):
-                if file == name:
+                if file == id:
                     return folder
                 else:
-                    filepath = self._locatePlugin(name, filepath)
+                    filepath = self._locatePlugin(id, filepath)
                     if filepath:
                         return filepath
 
         return False
-    
+
     #   Check if a certain dictionary contains a certain subset of key/value pairs
     #   \param dictionary The dictionary to search
     #   \param subset The subset to search for
@@ -231,3 +233,4 @@ class PluginRegistry(object):
                 return False
         return True
     _instance = None
+
--- docs/overview.md
+++ docs/overview.md
@@ -16,14 +16,7 @@ application. Its primary function is providing a central access point
 for other central objects like the Controller and MeshFileHandler.
 
 The PluginRegistry class is responsible for loading plugins and their
-metadata. Plugins are expected to be Python modules with an `__init__.py`
-file that provides a `getMetaData()` and a `register()` function. Plugins
-are loaded from all configured plugin paths. The `register()` function is
-called to register any objects exposed by the plugin. Plugins are free to
-expose any number of objects as long as they can be registered with the
-rest of the system. The `getMetaData()` function should return a dictionary
-object containing metadata for the plugin. For more details on writing
-plugins, see [plugins].
+metadata. For more details on writing plugins, see [plugins].
 
 Another important class is the [Signal] class. This provides a mechanism for
 simplified event handling using callbacks. This is used throughout the
