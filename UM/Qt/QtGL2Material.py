473e42bacb5840c766b6737596b8ecc867d3a98e
Arjen Hiemstra <ahiemstra@heimr.nl>
2015-03-06 16:03:38 +0100

    Add support for Color to QtGL2Material

--- UM/Qt/QtGL2Material.py
+++ UM/Qt/QtGL2Material.py
@@ -1,9 +1,10 @@
-from PyQt5.QtGui import QOpenGLShader, QOpenGLShaderProgram, QVector2D, QVector3D, QVector4D, QMatrix4x4
+from PyQt5.QtGui import QOpenGLShader, QOpenGLShaderProgram, QVector2D, QVector3D, QVector4D, QMatrix4x4, QColor
 from UM.View.Material import Material
 from UM.Logger import Logger
 
 from UM.Math.Vector import Vector
 from UM.Math.Matrix import Matrix
+from UM.Math.Color import Color
 
 class QtGL2Material(Material):
     def __init__(self, renderer):
@@ -120,6 +121,8 @@ class QtGL2Material(Material):
             self._shader_program.setUniformValue(uniform, QVector3D(value.x, value.y, value.z))
         elif type(value) is Matrix:
             self._shader_program.setUniformValue(uniform, self._matrixToQMatrix4x4(value))
+        elif type(value) is Color:
+            self._shader_program.setUniformValue(uniform, QColor(value.r * 255, value.g * 255, value.b * 255, value.a * 255))
         elif type(value) is list and len(value) is 2:
             self._shader_program.setUniformValue(uniform, QVector2D(value[0], value[1]))
         elif type(value) is list and len(value) is 3:
