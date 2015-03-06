f12a63d5fa9b9bfee881ce15ac714b9f81c50780
Arjen Hiemstra <ahiemstra@heimr.nl>
2015-03-06 16:28:37 +0100

    Add a handle property to tool

--- UM/Scene/ToolHandle.py
+++ UM/Scene/ToolHandle.py
@@ -3,8 +3,21 @@ from . import SceneNode
 from UM.View.Renderer import Renderer
 from UM.Mesh.MeshData import MeshData
 from UM.Resources import Resources
+from UM.Math.Color import Color
 
 class ToolHandle(SceneNode.SceneNode):
+    NoAxis = 1
+    XAxis = 2
+    YAxis = 3
+    ZAxis = 4
+    AllAxis = 5
+
+    DisabledColor = Color(0.5, 0.5, 0.5, 1.0)
+    XAxisColor = Color(1.0, 0.0, 0.0, 1.0)
+    YAxisColor = Color(0.0, 0.0, 1.0, 1.0)
+    ZAxisColor = Color(0.0, 1.0, 0.0, 1.0)
+    AllAxisColor = Color(1.0, 1.0, 1.0, 1.0)
+
     def __init__(self, parent = None):
         super().__init__(parent)
 
