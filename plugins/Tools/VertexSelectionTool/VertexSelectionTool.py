35f0aec0e45f02ba591c7f60f6a681cbd5d57a79
Jaime van Kessel <nallath@gmail.com>
2015-03-11 16:43:09 +0100

    Small square is now drawn on selected point

--- plugins/Tools/VertexSelectionTool/VertexSelectionTool.py
+++ plugins/Tools/VertexSelectionTool/VertexSelectionTool.py
@@ -1,10 +1,13 @@
 from UM.Event import MouseEvent
 from UM.Tool import Tool
+from UM.Math.Vector import Vector
+from UM.Event import Event
 from UM.Application import Application
 from UM.Scene.BoxRenderer import BoxRenderer
 from UM.Scene.RayRenderer import RayRenderer
 from UM.Scene.Selection import Selection
 from UM.Scene.Iterator.BreadthFirstIterator import BreadthFirstIterator
+from . import VertexSelectionToolHandle
 
 from PyQt5.QtGui import qAlpha, qRed, qGreen, qBlue
 
@@ -14,6 +17,8 @@ class VertexSelectionTool(Tool):
 
         self._scene = Application.getInstance().getController().getScene()
         self._renderer = Application.getInstance().getRenderer()
+        self._handle = VertexSelectionToolHandle.VertexSelectionToolHandle()
+        self._selected_point_coordinates = None
 
 
     def event(self, event):
@@ -21,11 +26,13 @@ class VertexSelectionTool(Tool):
             pixel_color = self._renderer.getSelectionColorAtCoordinate(event.x,event.y)
             if pixel_color:
                 if pixel_color.a:
-                    print("cloud index" ,  255 - pixel_color.a*255)
                     index  = int(255 - pixel_color.a*255)
                     targeted_node = Application.getInstance().getCloudNodeByIndex(index)
                     #Pixel colors are encoded in (r,g,b) with r being least significant and b being most.
                     pixel_index = int(pixel_color.r *255) + (int(pixel_color.g * 255) << 8) + (int(pixel_color.b * 255) << 16)
                     print("pixel index", pixel_index)
-                    Selection.add(targeted_node)
+                    temp = targeted_node.getMeshData().getVertex(pixel_index)
+                    self._selected_point_coordinates = Vector(temp[0],temp[1],temp[2])
+                    self._handle.setParent(self.getController().getScene().getRoot())
+                    self._handle.setPosition(self._selected_point_coordinates)
                 
