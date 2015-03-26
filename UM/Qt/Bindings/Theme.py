0d99a4f2bbe32038a40f1e1ef3eada6e992689cc
Arjen Hiemstra <ahiemstra@heimr.nl>
2015-03-26 14:33:35 +0100

    Update Theme to add default values and improve API

--- UM/Qt/Bindings/Theme.py
+++ UM/Qt/Bindings/Theme.py
@@ -3,6 +3,7 @@ from PyQt5.QtGui import QColor, QFont, QFontMetrics
 from PyQt5.QtQml import QQmlComponent
 
 import json
+import os
 import os.path
 
 from UM.Logger import Logger
@@ -13,18 +14,15 @@ class Theme(QObject):
         super().__init__(parent)
 
         self._engine = engine
-
-        self._fonts = {}
-        self._colors = {}
-        self._sizes = {}
-
         self._styles = None
-
         self._path = ""
+        self._icons = {}
+        self._images = {}
 
-        self._line_height = QFontMetrics(QCoreApplication.instance().font()).height()
+        self._em_height = int(QFontMetrics(QCoreApplication.instance().font()).ascent())
+        self._em_width = self._em_height;
 
-        Logger.log('d', 'Using a line height of %s', self._line_height)
+        self._initializeDefaults()
 
     themeLoaded = pyqtSignal()
 
@@ -32,27 +30,13 @@ class Theme(QObject):
     def styles(self):
         return self._styles
 
-    @pyqtSlot(str, result = str)
-    def getIcon(self, name):
-        svg = os.path.join(self._path, "icons", name + ".svg")
-        if os.path.isfile(svg):
-            return svg
+    @pyqtProperty('QVariantMap', notify = themeLoaded)
+    def icons(self):
+        return self._icons
 
-        png = os.path.join(self._path, "icons", name + ".png")
-        if os.path.isfile(png):
-            return png
-
-        Logger.log('e', "Icon {0} not found in theme".format(name))
-        return Resources.getPath(Resources.ImagesLocation, 'checkerboard.png')
-
-    @pyqtSlot(str, result = str)
-    def getImage(self, name):
-        png = os.path.join(self._path, "images", name + ".png")
-        if os.path.isfile(png):
-            return png
-
-        Logger.log('e', "Image {0} not found in theme".format(name))
-        return Resources.getPath(Resources.ImagesLocation, 'checkerboard.png')
+    @pyqtProperty('QVariantMap', notify = themeLoaded)
+    def images(self):
+        return self._images
 
     @pyqtProperty('QVariantMap', notify = themeLoaded)
     def colors(self):
@@ -73,9 +57,7 @@ class Theme(QObject):
         with open(os.path.join(self._path, 'theme.json')) as f:
             data = json.load(f)
 
-        self._fonts = {}
-        self._colors = {}
-        self._sizes = {}
+        self._initializeDefaults()
 
         if 'colors' in data:
             for name, color in data['colors'].items():
@@ -86,18 +68,19 @@ class Theme(QObject):
             for name, font in data['fonts'].items():
                 f = QFont()
 
-                f.setFamily(font.get('family', ''))
+                f.setFamily(font.get('family', QCoreApplication.instance().font().family()))
                 f.setBold(font.get('bold', False))
                 f.setItalic(font.get('italic', False))
-                f.setPixelSize(font.get('size', 1) * self._line_height)
+                f.setPixelSize(int(font.get('size', 1) * self._em_height))
+                f.setCapitalization(QFont.AllUppercase if font.get('capitalize', False) else QFont.MixedCase)
 
                 self._fonts[name] = f
 
         if 'sizes' in data:
             for name, size in data['sizes'].items():
                 s = QSizeF()
-                s.setWidth(size[0] * self._line_height)
-                s.setHeight(size[1] * self._line_height)
+                s.setWidth(size[0] * self._em_width)
+                s.setHeight(size[1] * self._em_height)
 
                 self._sizes[name] = s
 
@@ -106,8 +89,27 @@ class Theme(QObject):
             c = QQmlComponent(self._engine, styles)
             self._styles = c.create()
 
+        for icon in os.listdir(os.path.join(self._path, 'icons')):
+            name = os.path.splitext(icon)[0]
+            self._icons[name] = os.path.join(self._path, 'icons', icon)
+
         Logger.log('d', 'Loaded theme %s', self._path)
         self.themeLoaded.emit()
+
+    def _initializeDefaults(self):
+        self._fonts = {
+            'system': QCoreApplication.instance().font()
+        }
+
+        palette = QCoreApplication.instance().palette()
+        self._colors = {
+            'system_window': palette.window(),
+            'system_text': palette.text()
+        }
+
+        self._sizes = {
+            'line': QSizeF(self._em_width, self._em_height)
+        }
 
 def createTheme(engine, script_engine):
     return Theme(engine)
