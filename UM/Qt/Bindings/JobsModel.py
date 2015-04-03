95e0627e55f059ee83fe66985b8b3426bbed88e2
Arjen Hiemstra <ahiemstra@heimr.nl>
2015-04-03 17:53:33 +0200

    Remove backend progress from JobsModel

--- UM/Qt/Bindings/JobsModel.py
+++ UM/Qt/Bindings/JobsModel.py
@@ -9,8 +9,6 @@ class JobsModel(ListModel):
     DescriptionRole = Qt.UserRole + 2
     ProgressRole = Qt.UserRole + 3
 
-    ProcessingJob = 0xDEADBEEF #Unique ID for the processing message
-
     def __init__(self, parent = None):
         super().__init__(parent)
 
@@ -18,16 +16,10 @@ class JobsModel(ListModel):
         jobQueue.jobStarted.connect(self._onJobStarted)
         jobQueue.jobFinished.connect(self._onJobFinished)
 
-        backend = Application.getInstance().getBackend()
-        if backend:
-            backend.processingProgress.connect(self._onProcessingProgress)
-
         self._watchedJobIndices = {}
         self.addRoleName(self.IdRole, 'id')
         self.addRoleName(self.DescriptionRole, 'description')
         self.addRoleName(self.ProgressRole, 'progress')
-    #def roleNames(self):
-     #   return { self.IdRole: 'id', self.DescriptionRole: 'description', self.ProgressRole: 'progress' }
 
     def _onJobStarted(self, job):
         if job.isVisible():
@@ -45,13 +37,3 @@ class JobsModel(ListModel):
             job.progress.disconnect(self._onJobProgress)
             del self._watchedJobIndices[job]
 
-    def _onProcessingProgress(self, progress):
-        if not self.ProcessingJob in self._watchedJobIndices:
-            self.appendItem({ 'id': self.ProcessingJob, 'description': 'Processing...', 'progress': 0 })
-            self._watchedJobIndices[self.ProcessingJob] = self.rowCount() - 1
-
-        self.setProperty(self._watchedJobIndices[self.ProcessingJob], 'progress', round(progress * 100))
-
-        if round(progress * 100) >= 100:
-            self.removeItem(self._watchedJobIndices[self.ProcessingJob])
-            del self._watchedJobIndices[self.ProcessingJob]
