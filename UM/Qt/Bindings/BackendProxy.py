252a56980e02f544fdbee106ea5b381ba82c8b62
Arjen Hiemstra <ahiemstra@heimr.nl>
2015-04-03 17:52:28 +0200

    Add backend processing progress as a property to BackendProxy

--- UM/Qt/Bindings/BackendProxy.py
+++ UM/Qt/Bindings/BackendProxy.py
@@ -1,4 +1,4 @@
-from PyQt5.QtCore import QObject, pyqtSignal
+from PyQt5.QtCore import QObject, pyqtSignal, pyqtProperty
 
 from UM.Application import Application
 
@@ -6,10 +6,15 @@ class BackendProxy(QObject):
     def __init__(self, parent = None):
         super().__init__(parent)
         self._backend = Application.getInstance().getBackend()
+        self._progress = -1;
         if self._backend:
             self._backend.processingProgress.connect(self._onProcessingProgress)
 
     processingProgress = pyqtSignal(float, arguments = ['amount'])
+    @pyqtProperty(float, notify = processingProgress)
+    def progress(self):
+        return self._progress
 
     def _onProcessingProgress(self, amount):
+        self._progress = amount
         self.processingProgress.emit(amount)
