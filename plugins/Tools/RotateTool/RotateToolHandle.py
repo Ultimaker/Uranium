8ab24f6b3c2036bcf9dc0d44403b57f485040179
Arjen Hiemstra <ahiemstra@heimr.nl>
2015-04-16 11:52:35 +0200

    Add a selection mesh to RotateToolHandle

--- plugins/Tools/RotateTool/RotateToolHandle.py
+++ plugins/Tools/RotateTool/RotateToolHandle.py
@@ -3,6 +3,8 @@ from UM.Mesh.MeshData import MeshData
 from UM.Mesh.MeshBuilder import MeshBuilder
 from UM.Math.Vector import Vector
 
+import math
+
 class RotateToolHandle(ToolHandle):
     def __init__(self, parent = None):
         super().__init__(parent)
@@ -28,3 +30,32 @@ class RotateToolHandle(ToolHandle):
         )
 
         self.setLineMesh(mb.getData())
+
+        mb = MeshBuilder()
+
+        mb.addDonut(
+            inner_radius = 18,
+            outer_radius = 22,
+            width = 2,
+            color = ToolHandle.ZAxisColor
+        )
+
+        mb.addDonut(
+            inner_radius = 18,
+            outer_radius = 22,
+            width = 2,
+            axis = Vector.Unit_X,
+            angle = math.pi / 2,
+            color = ToolHandle.YAxisColor
+        )
+
+        mb.addDonut(
+            inner_radius = 18,
+            outer_radius = 22,
+            width = 2,
+            axis = Vector.Unit_Y,
+            angle = math.pi / 2,
+            color = ToolHandle.XAxisColor
+        )
+
+        self.setSelectionMesh(mb.getData())
