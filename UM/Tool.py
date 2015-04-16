0ed1b91c78769241e892cf1fb3144722129779ad
Arjen Hiemstra <ahiemstra@heimr.nl>
2015-04-16 11:44:01 +0200

    Perform tool handle highlights in Tool instead of each subclass

--- UM/Tool.py
+++ UM/Tool.py
@@ -1,5 +1,7 @@
 from UM.Signal import Signal, SignalEmitter
 from UM.PluginObject import PluginObject
+from UM.Event import Event
+from UM.Scene.Selection import Selection
 
 ##  Abstract base class for tools that manipulate the scene.
 #
@@ -8,12 +10,38 @@ class Tool(PluginObject, SignalEmitter):
         super().__init__() # Call super to make multiple inheritence work.
         self._controller = None
         self._handle = None
+        self._locked_axis = None
+        self._drag_plane = None
+        self._drag_start = None
 
     ##  Handle an event.
     #   \param event \type{Event} The event to handle.
     #   \sa Event
     def event(self, event):
-        pass
+        if event.type == Event.ToolActivateEvent:
+            if Selection.hasSelection() and self._handle:
+                self._handle.setParent(self.getController().getScene().getRoot())
+                self._handle.setPosition(Selection.getAveragePosition())
+
+        if event.type == Event.MouseMoveEvent and self._handle:
+            if self._locked_axis:
+                return
+
+            id = self._renderer.getIdAtCoordinate(event.x, event.y)
+            if not id:
+                self._handle.setActiveAxis(None)
+
+            if self._handle.isAxis(id):
+                self._handle.setActiveAxis(id)
+
+        if event.type == Event.ToolDeactivateEvent and self._handle:
+            self._handle.setParent(None)
+
+        return False
+
+    def updateHandlePosition(self):
+        if Selection.hasSelection():
+            self._handle.setPosition(Selection.getAveragePosition())
 
     def getController(self):
         return self._controller
