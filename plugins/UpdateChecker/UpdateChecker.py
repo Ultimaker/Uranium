0c38a458b53bb28e6bcf2a77d084f67cedb3ff83
Jaime van Kessel <nallath@gmail.com>
2015-04-24 13:51:57 +0200

    Added more logging + code cleanup

--- plugins/UpdateChecker/UpdateChecker.py
+++ plugins/UpdateChecker/UpdateChecker.py
@@ -12,34 +12,42 @@ from UM.Message import Message
         
 i18n_catalog = i18nCatalog('plugins')
 
-
+## This extention checks for new versions of the application based on the application name and the version number.
+#  The plugin is currently only usuable for applications maintained by Ultimaker. But it should be relatively easy
+#  to change it to work for other applications. 
 class UpdateChecker(Extension):
     def __init__(self):
         super().__init__()
         self.addMenuItem(i18n_catalog.i18n("Check new version"), self.checkNewVersion)
         self._url = None
-        thread = Thread(target=self.checkNewVersion)
+        
+        thread = Thread(target = self.checkNewVersion)
         thread.daemon = True
         thread.start()
-
+    
+    ##  Callback for the message that is spawned when there is a new version.  
     def actionTriggered(self, action):
         if action == "Download":
             if self._url is not None: 
                 webbrowser.open(self._url)
- 
+    
+    ##  Connect with software.ultimaker.com, load latest.json and check version info.
+    #   If the version info is higher then the current version, spawn a message to
+    #   allow the user to download it.
     def checkNewVersion(self):
-        Logger.log('i', "Checking new version")
-        
+        application_name = Application.getInstance().getApplicationName()
+        Logger.log('i', "Checking for new version of %s" % application_name)
+
         try:
             latest_version_file = urllib.request.urlopen("http://software.ultimaker.com/latest.json")
-        except:
-            Logger.log('e', "Failed to check for new version")
-        
+        except Exception as e:
+            Logger.log('e', "Failed to check for new version. %s" %e)
+
         try:
             reader = codecs.getreader("utf-8")
-            data = json.load(reader(latest_version_file))
-            application_name = Application.getInstance().getApplicationName()
+            data = json.load(reader(latest_version_file)) 
             local_version = list(map(int, Application.getInstance().getVersion().split('.')))
+            
             if application_name in data:
                 for key, value in data[application_name].items():
                     if "major" in value and "minor" in value and "revision" in value and "url" in value:
@@ -47,11 +55,15 @@ class UpdateChecker(Extension):
                         if platform.system() == os: #TODO: add architecture check
                             newest_version = [int(value["major"]), int(value['minor']), int(value['revision'])]
                             if local_version < newest_version:
+                                Logger.log('i', "Found a new version of the software. Spawning message")
                                 message = Message(i18n_catalog.i18n("A new version is available!"))
                                 message.addAction("Download", "[no_icon]", "[no_description]")
                                 self._url = value["url"]
                                 message.actionTriggered.connect(self.actionTriggered)
                                 message.show()
-
+                    else:
+                        Logger.log('e', "Could not find version information or download url for update.")
+            else:
+                Logger.log('e', "Did not find any version information for %s." % application_name)
         except Exception as e:
             Logger.log('e', "Exception in update checker: %s" % (e))
