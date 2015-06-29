0d39afc4dd347b00364e6e7c12d253fbbea55f2d
Tamara Hogenhout <t.hogenhout@ultimaker.com>
2015-06-29 11:59:41 +0200

    Adds keybord shortcuts for interacting with the build platform (the camera to be precise)
    Fixes Ultimaker/Cura#75

--- plugins/Tools/CameraTool/CameraTool.py
+++ plugins/Tools/CameraTool/CameraTool.py
@@ -2,7 +2,7 @@
 # Uranium is released under the terms of the AGPLv3 or higher.
 
 from UM.Tool import Tool
-from UM.Event import Event, MouseEvent
+from UM.Event import Event, MouseEvent, KeyEvent
 from UM.Math.Vector import Vector
 from UM.Math.Matrix import Matrix
 from UM.Application import Application
@@ -19,10 +19,14 @@ class CameraTool(Tool):
         self._origin = Vector(0, 0, 0)
         self._min_zoom = 10.0
         self._max_zoom = 1000.0
+        self._manual_zoom = 200
 
         self._rotate = False
         self._move = False
         self._dragged = False
+
+        self._shift_is_active = None
+        self._ctrl_is_active = None
 
         self._start_drag = None
 
@@ -41,16 +45,62 @@ class CameraTool(Tool):
     def getOrigin(self):
         return self._origin
 
-    def event(self, event):
-        if event.type is Event.MousePressEvent:
-            if MouseEvent.RightButton in event.buttons:
-                self._rotate = True
-                self._start_drag = (event.x, event.y)
+    def checkModifierKeys(self, event):
+        #checks for the press and release events of the modifier keys (shift and control)
+        #sets the accompanying variabel ( _[key]_is_active) on true or false
+        if event.type is Event.KeyPressEvent:
+            if event.key == KeyEvent.ShiftKey:
+                self._shift_is_active = True
+            if event.key == KeyEvent.ControlKey:
+                self._ctrl_is_active = True
+        if event.type is Event.KeyReleaseEvent:
+            if  event.key == KeyEvent.ShiftKey:
+                self._shift_is_active = False
+            if  event.key == KeyEvent.ControlKey:
+                self._ctrl_is_active = False
+
+    def moveEvent(self, event):
+        #defines at which events the _moveCamera method is called
+        if MouseEvent.MiddleButton in event.buttons: #mousewheel
+            return True
+        elif MouseEvent.LeftButton in event.buttons and self._shift_is_active is True: #shift -> leftbutton
+            return True
+        elif MouseEvent.RightButton in event.buttons and self._shift_is_active is True: #shift -> rightbutton
+            return True
+
+    def rotateEvent(self, event):
+        #defines at which events the _rotateCamera method is called
+        if MouseEvent.RightButton in event.buttons: #rightbutton
+            return True
+        elif MouseEvent.LeftButton in event.buttons and self._ctrl_is_active is True: #ctr -> leftbutton
+            return True
+
+    def initiateZoom(self, event):
+        #calls the zoomaction method for the mousewheel event and when the plus or minus keys are used
+        if event.type is Event.MouseWheelEvent:
+            self._zoomCamera(event.vertical)
+            return True
+        elif event.type is Event.KeyPressEvent:
+            if event.key == KeyEvent.MinusKey or event.key == KeyEvent.UnderscoreKey: #checks for both the minus and underscore key because they usually share a button on the keyboard and are sometimes interchanged
+                self._zoomCamera(-(self._manual_zoom))
                 return True
-            elif MouseEvent.MiddleButton in event.buttons:
+            elif event.key == KeyEvent.PlusKey or event.key == KeyEvent.EqualKey: #same story as the minus and underscore key: it checks for both the plus and equal key (so you won't have to do shift -> equal, to use the plus-key)
+                self._zoomCamera(self._manual_zoom)
+                return True
+
+    def event(self, event):
+        self.initiateZoom(event)
+        self.checkModifierKeys(event)
+        if event.type is Event.MousePressEvent:
+            if self.moveEvent(event) == True:
                 self._move = True
                 self._start_drag = (event.x, event.y)
                 return True
+            elif self.rotateEvent(event) == True:
+                self._rotate = True
+                self._start_drag = (event.x, event.y)
+                return True
+
         elif event.type is Event.MouseMoveEvent:
             if self._rotate or self._move:
                 diff = (event.x - self._start_drag[0], event.y - self._start_drag[1])
@@ -73,9 +123,6 @@ class CameraTool(Tool):
             if self._dragged:
                 self._dragged = False
                 return True
-        elif event.type is Event.MouseWheelEvent:
-            self._zoomCamera(event)
-            return True
 
         return False
 
@@ -95,7 +142,7 @@ class CameraTool(Tool):
         self._scene.releaseLock()
 
     #   Zooms the camera in response to a mouse event.
-    def _zoomCamera(self, event):
+    def _zoomCamera(self, zoomRange):
         camera = self._scene.getActiveCamera()
         if not camera or not camera.isEnabled():
             return
@@ -103,7 +150,7 @@ class CameraTool(Tool):
         self._scene.acquireLock()
 
         r = (camera.getWorldPosition() - self._origin).length()
-        delta = r * (event.vertical / 128 / 10.0)
+        delta = r * (zoomRange / 128 / 10.0)
         r -= delta
         if delta > 0:
             if r > self._min_zoom:
@@ -127,7 +174,6 @@ class CameraTool(Tool):
         dy = math.radians(y * 180.0)
 
         diff = camera.getPosition() - self._origin
-
         my = Matrix()
         my.setByRotationAxis(dx, Vector.Unit_Y)
 
