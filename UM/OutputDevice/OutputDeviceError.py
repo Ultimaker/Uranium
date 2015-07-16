bfec6f3e2db99e20baf9b87fcd85da9ff050b030
Arjen Hiemstra <ahiemstra@heimr.nl>
2015-07-16 09:13:31 +0200

    Replace error codes with error subclasses
    This provides the same information but is a cleaner solution for python

--- UM/OutputDevice/OutputDeviceError.py
+++ UM/OutputDevice/OutputDeviceError.py
@@ -1,12 +1,14 @@
 # Copyright (c) 2015 Ultimaker B.V.
 # Uranium is released under the terms of the AGPLv3 or higher.
 
-class ErrorCodes:
-    UserCanceledError = 1
-    DeviceBusyError = 2
-
 class WriteRequestFailedError(Exception):
-    def __init__(self, code, message):
-        super().__init__(message)
-        self.code = code
-        self.message = message
+    pass
+
+class UserCancelledError(WriteRequestFailedError):
+    pass
+
+class PermissionDeniedError(WriteRequestFailedError):
+    pass
+
+class DeviceBusyError(WriteRequestFailedError):
+    pass
