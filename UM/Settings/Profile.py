be1eaf9cd47b6bbd3a2e00e8359f719c2f901ce8
Arjen Hiemstra <ahiemstra@heimr.nl>
2015-08-05 20:09:54 +0200

    Rename read/write to load/save and add a file version check

--- UM/Settings/Profile.py
+++ UM/Settings/Profile.py
@@ -1,7 +1,13 @@
 # Copyright (c) 2015 Ultimaker B.V.
 # Uranium is released under the terms of the AGPLv3 or higher.
+
 import configparser
+
+from UM.Settings import SettingsError
+
 class Profile():
+    ProfileVersion = 1
+
     def __init__(self):
         super().__init__()
         self._changed_settings = {}
@@ -18,27 +24,32 @@ class Profile():
     def getChangedSettings(self):
         return self._changed_settings
     
-    def readFromFile(self, file):
+    def loadFromFile(self, path):
         parser = configparser.ConfigParser()
-        parser.read(file)
+        parser.read(path)
+
         if not parser.has_section("General"):
             raise SettingsError.InvalidFileError(path)
 
         if not parser.has_option("General", "version") or parser.get("General", "version") != self.ProfileVersion:
             raise SettingsError.InvalidVersionError(path)
+
         for group in parser:
             if group == "DEFAULT":
                 continue
-            if group == "settings":
+            if group == "Settings":
                 for key, value in parser[group].items():
                     self.setSettingValue(key, value)
     
-    def writeToFile(self, file):
+    def saveToFile(self, file):
         parser = configparser.ConfigParser()
-        parser.add_section("settings")
+
+        parser.add_section("General")
+        parser.set("General", "version", self.ProfileVersion)
+
+        parser.add_section("Settings")
         for setting_key in self._changed_settings:
-            parser.set("settings", setting_key , str(self._changed_settings[setting_key]))
-            #parser[setting_key] = self._changed_settings[setting_key])
+            parser.set("Settings", setting_key , str(self._changed_settings[setting_key]))
         
         with open(file, "wt") as f:
             parser.write(f)
