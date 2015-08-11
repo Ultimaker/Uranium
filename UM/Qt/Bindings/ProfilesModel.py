25afd8353e4bd9dd2a89461a60db25de5785e0a1
Arjen Hiemstra <ahiemstra@heimr.nl>
2015-08-11 17:32:14 +0200

    Add a property that adds a <Use Global> entry to ProfilesModel

--- UM/Qt/Bindings/ProfilesModel.py
+++ UM/Qt/Bindings/ProfilesModel.py
@@ -4,15 +4,20 @@
 from UM.Qt.ListModel import ListModel
 from UM.Application import Application
 
-from PyQt5.QtCore import Qt, pyqtSlot
+from PyQt5.QtCore import Qt, pyqtSlot, pyqtSignal, pyqtProperty
+
+from UM.i18n import i18nCatalog
+catalog = i18nCatalog("uranium")
 
 class ProfilesModel(ListModel):
     IdRole = Qt.UserRole + 1
     NameRole = Qt.UserRole + 2
     ActiveRole = Qt.UserRole + 3
 
-    def __init__(self):
-        super().__init__()
+    def __init__(self, parent = None):
+        super().__init__(parent)
+
+        self._select_global = False
 
         self.addRoleName(self.IdRole, "id")
         self.addRoleName(self.NameRole, "name")
@@ -24,14 +29,30 @@ class ProfilesModel(ListModel):
         self._manager.activeProfileChanged.connect(self._onActiveProfileChanged)
         self._onProfilesChanged()
 
+    selectGlobalChanged = pyqtSignal()
+
+    def setSelectGlobal(self, select):
+        if select != self._select_global:
+            self._select_global = select
+            self._onProfilesChanged()
+            self.selectGlobalChanged.emit()
+
+    @pyqtProperty(bool, fset = setSelectGlobal, notify = selectGlobalChanged)
+    def selectGlobal(self):
+        return self._select_global
+
     def _onProfilesChanged(self):
         self.clear()
+
+        if self._select_global:
+            self.appendItem({ "id": 1, "name": catalog.i18nc("", "<Use Global Profile>"), "active": False })
+
         profiles = self._manager.getProfiles()
         profiles.sort(key = lambda k: k.getName())
         for profile in profiles:
             self.appendItem({ "id": id(profile), "name": profile.getName(), "active": self._manager.getActiveProfile() == profile })
 
-    def _onActiveMachineChanged(self):
+    def _onActiveProfileChanged(self):
         active_profile = self._manager.getActiveProfile()
         for index in range(len(self.items)):
             self.setProperty(index, "active", id(active_profile) == self.items[index]["id"])
