f63b63912f4e98bf273c0ead9c6aea436ad1b514
Arjen Hiemstra <ahiemstra@heimr.nl>
2015-08-13 09:21:33 +0200

    Nothing to do in PerObjectSettingsTool since everything is handled from QML

--- plugins/Tools/PerObjectSettingsTool/PerObjectSettingsTool.py
+++ plugins/Tools/PerObjectSettingsTool/PerObjectSettingsTool.py
@@ -23,59 +23,10 @@ class PerObjectSettingsTool(Tool):
         super().__init__()
 
         self._renderer = Application.getInstance().getRenderer()
-        self._handle = PerObjectSettingsToolHandle.PerObjectSettingsToolHandle()
-        self._model = PerObjectSettingsModel.PerObjectSettingsModel()
-
         self.setExposedProperties("Model")
 
     def event(self, event):
-        if event.type == Event.ToolActivateEvent:
-            self._handle.setParent(self.getController().getScene().getRoot())
-
-        if event.type == Event.ToolDeactivateEvent:
-            self._handle.setParent(None)
-
-        if event.type == Event.MouseReleaseEvent:
-            if MouseEvent.LeftButton not in event.buttons:
-                return False
-
-            id = self._renderer.getIdAtCoordinate(event.x, event.y)
-            if not id:
-                return False
-
-            #if self.getLockedAxis():
-                #op = None
-                #if Selection.getCount() == 1:
-                    #node = Selection.getSelectedObject(0)
-                    #scale = node.getScale()
-                    #if self.getLockedAxis() == ToolHandle.XAxis:
-                        #scale.setX(-scale.x)
-                    #elif self.getLockedAxis() == ToolHandle.YAxis:
-                        #scale.setY(-scale.y)
-                    #elif self.getLockedAxis() == ToolHandle.ZAxis:
-                        #scale.setZ(-scale.z)
-
-                    #op = ScaleOperation(node, scale, set_scale=True)
-                #else:
-                    #op = GroupedOperation()
-
-                    #for node in Selection.getAllSelectedObjects():
-                        #scale = node.getScale()
-                        #if self.getLockedAxis() == ToolHandle.XAxis:
-                            #scale.setX(-scale.x)
-                        #elif self.getLockedAxis() == ToolHandle.YAxis:
-                            #scale.setY(-scale.y)
-                        #elif self.getLockedAxis() == ToolHandle.ZAxis:
-                            #scale.setZ(-scale.z)
-
-                        #op.addOperation(ScaleOperation(node, scale, set_scale = True))
-
-                #op.push()
-
-                #self.setLockedAxis(None)
-                #return True
-
         return False
 
     def getModel(self):
-        return self._model
+        return PerObjectSettingsModel.PerObjectSettingsModel()
