28bb6d3b021d6149dcc1ce9277583f56b2d9eef7
Arjen Hiemstra <ahiemstra@heimr.nl>
2015-08-14 01:37:16 +0200

    Remove obsolete code

--- plugins/Tools/PerObjectSettingsTool/PerObjectSettingsTool.py
+++ plugins/Tools/PerObjectSettingsTool/PerObjectSettingsTool.py
@@ -2,27 +2,13 @@
 # Uranium is released under the terms of the AGPLv3 or higher.
 
 from UM.Tool import Tool
-from UM.Event import Event, MouseEvent
 
-from UM.Math.Plane import Plane
-from UM.Math.Vector import Vector
-from UM.Math.Float import Float
-
-from UM.Operations.ScaleOperation import ScaleOperation
-from UM.Operations.GroupedOperation import GroupedOperation
-from UM.Application import Application
-
-from UM.Scene.Selection import Selection
-from UM.Scene.ToolHandle import ToolHandle
-
-from . import PerObjectSettingsToolHandle
 from . import PerObjectSettingsModel
 
 class PerObjectSettingsTool(Tool):
     def __init__(self):
         super().__init__()
 
-        self._renderer = Application.getInstance().getRenderer()
         self.setExposedProperties("Model")
 
     def event(self, event):
