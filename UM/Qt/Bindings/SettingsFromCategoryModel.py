aa95dd1a0b00f2a8801019cf840af9a184f342a7
Arjen Hiemstra <ahiemstra@heimr.nl>
2015-08-25 17:46:52 +0200

    Expose Setting depth in SettingsFromCategoryModel

--- UM/Qt/Bindings/SettingsFromCategoryModel.py
+++ UM/Qt/Bindings/SettingsFromCategoryModel.py
@@ -19,6 +19,7 @@ class SettingsFromCategoryModel(ListModel, SignalEmitter):
     UnitRole = Qt.UserRole + 7
     DescriptionRole = Qt.UserRole + 8
     VisibleRole = Qt.UserRole + 9
+    DepthRole = Qt.UserRole + 10
     
     def __init__(self, category, parent = None):
         super().__init__(parent)
@@ -35,6 +36,7 @@ class SettingsFromCategoryModel(ListModel, SignalEmitter):
         self.addRoleName(self.UnitRole,"unit")
         self.addRoleName(self.DescriptionRole, "description")
         self.addRoleName(self.VisibleRole, "visible")
+        self.addRoleName(self.DepthRole, "depth")
     
     settingChanged = Signal()
 
@@ -76,6 +78,7 @@ class SettingsFromCategoryModel(ListModel, SignalEmitter):
                 "options": self.createOptionsModel(setting.getOptions()),
                 "unit": setting.getUnit(),
                 "visible": (setting.isVisible() and setting.isActive())
+                "depth": setting.getDepth()
             })
             setting.visibleChanged.connect(self._onSettingChanged)
             setting.activeChanged.connect(self._onSettingChanged)
