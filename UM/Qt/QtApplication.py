8d3d0e57aa7532833c28ed14187be4155975e3e9
Jaime van Kessel <nallath@gmail.com>
2015-08-28 10:54:41 +0200

    Moved engine created signal to later point in creation

--- UM/Qt/QtApplication.py
+++ UM/Qt/QtApplication.py
@@ -122,7 +122,7 @@ class QtApplication(QApplication, Application, SignalEmitter):
         Bindings.register()
 
         self._engine = QQmlApplicationEngine()
-        self.engineCreatedSignal.emit()
+
         
         self._engine.addImportPath(os.path.join(os.path.dirname(sys.executable), "qml"))
         self._engine.addImportPath(os.path.join(Application.getInstallPrefix(), "Resources", "qml"))
@@ -132,6 +132,7 @@ class QtApplication(QApplication, Application, SignalEmitter):
         self.registerObjects(self._engine)
         
         self._engine.load(self._main_qml)
+        self.engineCreatedSignal.emit()
     
     engineCreatedSignal = Signal()
     
