1e3a5bc29f76526b83cb76ab974faf629fb5d800
Jaime van Kessel <nallath@gmail.com>
2015-09-02 11:15:44 +0200

    Added check for missing profile

--- UM/Settings/SettingsCategory.py
+++ UM/Settings/SettingsCategory.py
@@ -72,9 +72,10 @@ class SettingsCategory(SignalEmitter):
     ##  Get the number of settings in this category that are not visible and have a custom value set.
     def getHiddenValuesCount(self):
         count = 0
-        for setting in self.getAllSettings():
-            if not setting.isVisible() and self._machine_manager.getActiveProfile().hasSettingValue(setting.getKey()):
-                count += 1
+        if self._machine_manager.getActiveProfile():
+            for setting in self.getAllSettings():
+                if not setting.isVisible() and self._machine_manager.getActiveProfile().hasSettingValue(setting.getKey()):
+                    count += 1
 
         return count
 
