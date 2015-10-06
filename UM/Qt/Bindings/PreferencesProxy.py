07a107e18001caea7c2ddbe195a1a5ad6fac674f
Arjen Hiemstra <ahiemstra@heimr.nl>
2015-10-06 12:08:35 +0200

    Add a preferenceChanged signal to PreferencesProxy

--- UM/Qt/Bindings/PreferencesProxy.py
+++ UM/Qt/Bindings/PreferencesProxy.py
@@ -1,7 +1,7 @@
 # Copyright (c) 2015 Ultimaker B.V.
 # Uranium is released under the terms of the AGPLv3 or higher.
 
-from PyQt5.QtCore import QObject, pyqtSlot
+from PyQt5.QtCore import QObject, pyqtSlot, pyqtSignal
 
 from UM.Preferences import Preferences
 
@@ -9,6 +9,9 @@ class PreferencesProxy(QObject):
     def __init__(self, parent = None):
         super().__init__(parent)
         self._preferences = Preferences.getInstance()
+        self._preferences.preferenceChanged.connect(self._onPreferenceChanged)
+
+    preferenceChanged = pyqtSignal(str, arguments = ["preference"])
 
     @pyqtSlot(str, result = "QVariant")
     def getValue(self, key):
@@ -21,6 +24,9 @@ class PreferencesProxy(QObject):
     @pyqtSlot(str)
     def resetPreference(self, key):
         self._preferences.resetPreference(key)
+
+    def _onPreferenceChanged(self, preference):
+        self.preferenceChanged.emit(preference)
 
 def createPreferencesProxy(engine, script_engine):
     return PreferencesProxy()
