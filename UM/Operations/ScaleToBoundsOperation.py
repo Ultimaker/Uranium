64200bfb4d20e329416cc0556d29062b2a9b8ed2
Tamara Hogenhout <t.hogenhout@ultimaker.com>
2015-11-20 17:38:46 +0100

    Calculates the scale to max in a different way so it doesn't scale to big
    Alse it makes sure that the dimensions are respected also if the user scaled without uniform scaling.
    
    Contributes to #CURA-435

--- UM/Operations/ScaleToBoundsOperation.py
+++ UM/Operations/ScaleToBoundsOperation.py
@@ -8,22 +8,30 @@ from UM.Math.Vector import Vector
 class ScaleToBoundsOperation(Operation):
     def __init__(self, node, bounds):
         super().__init__()
+
+        #StartingPoint: get the old scale, boundingbox and largest dimension
         self._node = node
         self._old_scale = node.getScale()
 
+        #Get the sizes of the boundingbox but substract the disallowed area's
         bbox = self._node.getBoundingBox()
-
+        printable_area_width = bounds.width
+        printable_area_depth  =  bounds.depth
+        printable_area_height = bounds.height
         largest_dimension = max(bbox.width, bbox.height, bbox.depth)
 
+        #Get the maximum scale factor by deviding the the size of the bounding box by the largest dimension
         scale_factor = 1.0
         if largest_dimension == bbox.depth:
-            scale_factor = self._old_scale.z * (bounds.depth / bbox.depth)
+            scale_factor = printable_area_depth / bbox.depth
         elif largest_dimension == bbox.width:
-            scale_factor = self._old_scale.x * (bounds.width / bbox.width)
+            scale_factor = printable_area_width / bbox.width
         elif largest_dimension == bbox.height:
-            scale_factor = self._old_scale.y * (bounds.height / bbox.height)
+            scale_factor = printable_area_height / bbox.height
 
-        self._new_scale = Vector(scale_factor, scale_factor, scale_factor)
+        #Aplly scale factor on all different sizes to respect the (non-uniform) scaling that already has been done by the user
+        self._new_scale = Vector(self._old_scale.x * scale_factor, self._old_scale.y * scale_factor, self._old_scale.z * scale_factor)
+
 
     def undo(self):
         self._node.setScale(self._old_scale)
