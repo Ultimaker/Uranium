0728b2647dc36f77cd57bcc3124f90cec15dffec
Arjen Hiemstra <ahiemstra@heimr.nl>
2015-11-24 17:25:30 +0100

    Introduce a Platform class to simplify platform abstraction checking

---
+++ UM/Platform.py
@@ -0,0 +1,35 @@
+# Copyright (c) 2015 Ultimaker B.V.
+# Cura is released under the terms of the AGPLv3 or higher.
+
+import sys
+
+class Platform:
+    class PlatformType:
+        Windows = 1
+        Linux = 2
+        OSX = 3
+        Other = 4
+
+    @classmethod
+    def isOSX(cls):
+        return cls.__platform_type == cls.PlatformType.OSX
+
+    @classmethod
+    def isWindows(cls):
+        return cls.__platform_type == cls.PlatformType.Windows
+
+    @classmethod
+    def isLinux(cls):
+        return cls.__platform_type == cls.PlatformType.Linux
+
+    @classmethod
+    def getType(cls):
+        return cls.__platform_type
+
+    __platform_type = PlatformType.Other
+    if sys.platform == "win32":
+        __platform_type = PlatformType.Windows
+    elif sys.platform == "linux":
+        __platform_type = PlatformType.Linux
+    elif sys.platform == "darwin":
+        __platform_type = PlatformType.OSX
