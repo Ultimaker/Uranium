0c16ff5e3d95ff45dfed6ebb68d6586bc7ef4d44
Arjen Hiemstra <ahiemstra@heimr.nl>
2015-12-01 18:06:17 +0100

    Add DefaultPass class that performs the default rendering

---
+++ UM/View/DefaultPass.py
@@ -0,0 +1,21 @@
+# Copyright (c) 2015 Ultimaker B.V.
+# Cura is released under the terms of the AGPLv3 or higher.
+
+from UM.Application import Application
+from UM.View.RenderPass import RenderPass
+
+class DefaultPass(RenderPass):
+    def __init__(self, width, height):
+        super().__init__("default", width, height, 0)
+
+        self._renderer = Application.getInstance().getRenderer()
+
+    def render(self):
+        self.bind()
+
+        camera = Application.getInstance().getController().getScene().getActiveCamera()
+
+        for batch in self._renderer.getBatches():
+            batch.render(camera)
+
+        self.release()
