a36a0f751985f2a2d67656afc1826c323ce09324
Arjen Hiemstra <ahiemstra@heimr.nl>
2015-12-08 13:46:47 +0100

    Set tool handle axis alpha to 1, otherwise selection fails

--- UM/Scene/ToolHandle.py
+++ UM/Scene/ToolHandle.py
@@ -23,9 +23,9 @@ class ToolHandle(SceneNode.SceneNode):
     AllAxis = 5
 
     DisabledColor = Color(0.5, 0.5, 0.5, 1.0)
-    XAxisColor = Color(1.0, 0.0, 0.0, 0.8)
-    YAxisColor = Color(0.0, 0.0, 1.0, 0.8)
-    ZAxisColor = Color(0.0, 1.0, 0.0, 0.8)
+    XAxisColor = Color(1.0, 0.0, 0.0, 1.0)
+    YAxisColor = Color(0.0, 0.0, 1.0, 1.0)
+    ZAxisColor = Color(0.0, 1.0, 0.0, 1.0)
     AllAxisColor = Color(1.0, 1.0, 1.0, 1.0)
 
     def __init__(self, parent = None):
