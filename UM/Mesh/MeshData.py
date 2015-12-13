31cd736cbd3b4b463b5b67fecf6f97ed8f84d078
daid <daid303@gmail.com>
2015-12-13 17:45:53 +0100

    In line with the previous commit. The calculateNormals function also gives problems if the _vertices numpy array is resized by the addVertex function. As this can make it not divible by 3. This function also calculates too many normals when the vertices array is bigger then the _vertex_count. So this also makes the function a bit more efficitent.

--- UM/Mesh/MeshData.py
+++ UM/Mesh/MeshData.py
@@ -460,7 +460,7 @@ class MeshData(SignalEmitter):
             # zero. The second array is built from the difference between every third item in the array starting at
             # two and every third item in the array starting at zero. The cross operation then returns an array of
             # the normals of each set of three vertices.
-            n = numpy.cross(self._vertices[1::3] - self._vertices[::3], self._vertices[2::3] - self._vertices[::3])
+            n = numpy.cross(self._vertices[1:self._vertex_count:3] - self._vertices[:self._vertex_count:3], self._vertices[2:self._vertex_count:3] - self._vertices[:self._vertex_count:3])
             # We then calculate the length for each normal and perform normalization on the normals.
             l = numpy.linalg.norm(n, axis=1)
             n[:, 0] /= l
