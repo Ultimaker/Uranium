9ca5ef66cc0f2b4ddb90b32ce84c270f8d066755
Ghostkeeper <ghost_keeper+github@hotmail.com>
2016-01-08 16:10:25 +0100

    Rename start/end to endpoint_a/endpoint_b
    This is more indicative of the line segment having no direction. Any mention of start and end points is removed.
    
    Contributes to issue CURA-260.

--- UM/Math/LineSegment.py
+++ UM/Math/LineSegment.py
@@ -5,27 +5,27 @@ from UM.Math.Float import Float #For fuzzy comparison of edge cases.
 
 ##  Represents a line segment in 2D.
 #
-#   The line segment is represented by a beginning and an end point.
+#   The line segment is represented by two endpoints.
 class LineSegment(object):
-    ##  Creates a new line segment with the specified start and end points.
+    ##  Creates a new line segment with the specified endpoints.
     #
-    #   \param start The start point of the line segment.
-    #   \param end The end point of the line segment.
-    def __init__(self, start, end):
-        self._start = start
-        self._end = end
+    #   \param endpoint_a An endpoint of the line segment.
+    #   \param endpoint_b An endpoint of the line segment.
+    def __init__(self, endpoint_a, endpoint_b):
+        self._endpoint_a = endpoint_a
+        self._endpoint_b = endpoint_b
 
-    ##  Gets the ending point of the line segment.
+    ##  Gets the second endpoint (B) of the line segment.
     #
-    #   \return The ending point of the line segment.
+    #   \return The second endpoint of the line segment.
     def getEnd(self):
-        return self._end
+        return self._endpoint_b
 
-    ##  Gets the starting point of the line segment.
+    ##  Gets the first endpoint (A) of the line segment.
     #
-    #   \return The starting point of the line segment.
+    #   \return The first endpoint of the line segment.
     def getStart(self):
-        return self._start
+        return self._endpoint_a
 
     ##  Returns the point of intersection of this line segment with another line
     #   segment, if any.
@@ -33,17 +33,17 @@ class LineSegment(object):
     #   \param other The line segment to check intersection with.
     #   \return The intersection point if they intersect, or None otherwise.
     def intersection(self, other):
-        if not self.intersectsWithLine(other._start, other._end) or not other.intersectsWithLine(self._start, self._end): #Line segments don't intersect.
+        if not self.intersectsWithLine(other._endpoint_a, other._endpoint_b) or not other.intersectsWithLine(self._endpoint_a, self._endpoint_b): #Line segments don't intersect.
             return None
-        direction_me = self._end - self._start
-        direction_other = other._end - other._start
-        diff_starts = self._start - other._start
+        direction_me = self._endpoint_b - self._endpoint_a
+        direction_other = other._endpoint_b - other._endpoint_a
+        diff_endpoint_a = self._endpoint_a - other._endpoint_a
         perpendicular = direction_me.perpendicular()
         denominator = perpendicular.dot(direction_other) #Project onto the perpendicular.
-        numerator = perpendicular.dot(diff_starts)
+        numerator = perpendicular.dot(diff_endpoint_a)
         if denominator == 0: #Lines are parallel.
             return None
-        return (numerator / denominator.astype(float)) * direction_other + other._start
+        return (numerator / denominator.astype(float)) * direction_other + other._endpoint_a
 
     ##  Returns whether the line segment intersects the specified (infinite)
     #   line.
@@ -58,14 +58,13 @@ class LineSegment(object):
     def intersectsWithLine(self, a, b):
         shifted_b = b - a
         #It intersects if either endpoint is on the line, or if one endpoint is on the right but the other is not.
-        return Float.fuzzyCompare(shifted_b.cross(self._start), 0) or Float.fuzzyCompare(shifted_b.cross(self._end), 0) or (self._pointIsRight(self._start, a, b) != self._pointIsRight(self._end, a, b))
+        return Float.fuzzyCompare(shifted_b.cross(self._endpoint_a), 0) or Float.fuzzyCompare(shifted_b.cross(self._endpoint_b), 0) or (self._pointIsRight(self._endpoint_a, a, b) != self._pointIsRight(self._endpoint_b, a, b))
 
-    ##  Determines whether point p is to the right of the line through start and
-    #   end.
+    ##  Determines whether point p is to the right of the line through a and b.
     #
     #   \param p The point to determine whether it is to the right of the line.
-    #   \param start A point on the line.
-    #   \param end Another point on the line.
-    def _pointIsRight(self, p, start, end):
-        shifted_end = end - start
-        return shifted_end.cross(p - start) < 0
+    #   \param a A point on the line.
+    #   \param b Another point on the line.
+    def _pointIsRight(self, p, a, b):
+        shifted_end = b - a
+        return shifted_end.cross(p - a) < 0
