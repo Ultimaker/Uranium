42cf4496ac8be2bc79847c3f0d8d3e5cf4f98523
Jaime van Kessel <nallath@gmail.com>
2016-01-26 11:14:21 +0100

    Added compose matrix function
    CURA-685

--- UM/Math/Matrix.py
+++ UM/Math/Matrix.py
@@ -157,6 +157,59 @@ class Matrix(object):
             M[:3, 3] = point - numpy.dot(R, point)
         self._data = M
 
+    def compose(self, scale = None, shear = None, angles = None, translate = None, perspective = None):
+        """Return transformation matrix from sequence of transformations.
+
+        This is the inverse of the decompose_matrix function.
+
+        Sequence of transformations:
+            scale : vector of 3 scaling factors
+            shear : list of shear factors for x-y, x-z, y-z axes
+            angles : list of Euler angles about static x, y, z axes
+            translate : translation vector along x, y, z axes
+            perspective : perspective partition of matrix
+
+        >>> scale = numpy.random.random(3) - 0.5
+        >>> shear = numpy.random.random(3) - 0.5
+        >>> angles = (numpy.random.random(3) - 0.5) * (2*math.pi)
+        >>> trans = numpy.random.random(3) - 0.5
+        >>> persp = numpy.random.random(4) - 0.5
+        >>> M0 = compose_matrix(scale, shear, angles, trans, persp)
+        >>> result = decompose_matrix(M0)
+        >>> M1 = compose_matrix(*result)
+        >>> is_same_transform(M0, M1)
+        True
+
+        """
+        M = numpy.identity(4)
+        if perspective is not None:
+            P = numpy.identity(4)
+            P[3, :] = perspective.getData()[:4]
+            M = numpy.dot(M, P)
+        if translate is not None:
+            T = numpy.identity(4)
+            T[:3, 3] = translate.getData()[:3]
+            M = numpy.dot(M, T)
+        if angles is not None:
+            R = Matrix()
+            R.setByEuler(angles.x, angles.y, angles.z, 'sxyz')
+            M = numpy.dot(M, R.getData())
+        if shear is not None:
+            Z = numpy.identity(4)
+            Z[1, 2] = shear.x
+            Z[0, 2] = shear.y
+            Z[0, 1] = shear.z
+            M = numpy.dot(M, Z)
+        if scale is not None:
+            S = numpy.identity(4)
+            S[0, 0] = scale.x
+            S[1, 1] = scale.y
+            S[2, 2] = scale.z
+            M = numpy.dot(M, S)
+        M /= M[3, 3]
+        self._data = M
+
+
 
     def setByEuler(self, ai, aj, ak, axes = "sxyz"):
         """Return homogeneous rotation matrix from Euler angles and axis sequence.
