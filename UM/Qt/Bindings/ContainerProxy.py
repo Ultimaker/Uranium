8a5e3a54e5817a9ad2bae59736374872a69e1890
Arjen Hiemstra <ahiemstra@heimr.nl>
2016-01-29 15:17:10 +0100

    Add some documentation to ContainerProxy

--- UM/Qt/Bindings/ContainerProxy.py
+++ UM/Qt/Bindings/ContainerProxy.py
@@ -1,5 +1,14 @@
 from PyQt5.QtCore import QObject, pyqtSlot, pyqtSignal
 
+# This class wraps a Python container and provides access to its elements
+#
+# It is primarily intended as a reasonably fast wrapper around containers
+# that need to be updated and need to notify QML of their changes.
+#
+# To use it, define a property that returns a QObject with a notify signal.
+# From the property getter, return an instance of this object. Whenever the
+# contents of the container change, simply emit the notify signal, do not
+# recreate the proxy.
 class ContainerProxy(QObject):
     def __init__(self, container, parent = None):
         super().__init__(parent)
