44c2f8e731641998f997552c40ab9accb7a1e062
Jaime van Kessel <nallath@gmail.com>
2016-02-01 09:53:47 +0100

    Removed commented import

--- UM/Qt/Bindings/ActiveProfileProxy.py
+++ UM/Qt/Bindings/ActiveProfileProxy.py
@@ -5,7 +5,6 @@ from PyQt5.QtCore import pyqtSlot, pyqtProperty, pyqtSignal, QObject, QVariant, 
 
 from UM.Application import Application
 from UM.PluginRegistry import PluginRegistry
-#from UM.Logger import deprecated
 
 from . import ContainerProxy
 
