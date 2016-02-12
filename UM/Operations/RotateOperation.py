d6b15a7618768412a4de3393d5de8a2cfc136b33
Jaime van Kessel <nallath@gmail.com>
2016-02-12 11:05:18 +0100

    Added rotate around center option
    CURA-685

--- UM/Operations/RotateOperation.py
+++ UM/Operations/RotateOperation.py
@@ -2,21 +2,29 @@
 # Uranium is released under the terms of the AGPLv3 or higher.
 
 from UM.Scene.SceneNode import SceneNode
+from UM.Math.Vector import Vector
 
 from . import Operation
 
 class RotateOperation(Operation.Operation):
-    def __init__(self, node, rotation):
+    def __init__(self, node, rotation, **kwargs):
         super().__init__()
         self._node = node
-        self._old_orientation = node.getOrientation()
+        self._old_transformation = node.getLocalTransformation()
         self._rotation = rotation
 
+        self._rotate_around_center = kwargs.get("rotate_around_center", False)
+
     def undo(self):
-        self._node.setOrientation(self._old_orientation)
+        self._node.setTransformation(self._old_transformation)
 
     def redo(self):
+        if self._rotate_around_center:
+            center = self._node.getBoundingBox().center
+            self._node.setPosition(-center)
         self._node.rotate(self._rotation, SceneNode.TransformSpace.World)
+        if self._rotate_around_center:
+            self._node.setPosition(center)
 
     def mergeWith(self, other):
         if type(other) is not RotateOperation:
@@ -26,7 +34,7 @@ class RotateOperation(Operation.Operation):
             return False
 
         op = RotateOperation(self._node, other._rotation * self._rotation)
-        op._old_orientation = other._old_orientation
+        op._old_transformation = other._old_transformation
         return op
 
     def __repr__(self):
