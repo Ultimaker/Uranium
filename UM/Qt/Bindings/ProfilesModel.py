4f163d7f4a947325f4c10321420b4974d3aff271
fieldOfView <aldo@fieldofview.com>
2016-02-16 21:48:58 +0100

    Always make sure imported profiles are available for the currently active machine
    Contributes to CURA-874

--- UM/Qt/Bindings/ProfilesModel.py
+++ UM/Qt/Bindings/ProfilesModel.py
@@ -124,9 +124,8 @@ class ProfilesModel(ListModel):
                 file_name = os.path.basename(os.path.splitext(path)[0])
                 profile.setName(self._manager.makeUniqueProfileName(file_name))
 
-                if profile.getMachineTypeId():
-                    #Make sure the profile is available for the currently selected printer
-                    profile.setMachineTypeId(self._manager.getActiveMachineInstance().getMachineDefinition().getProfilesMachineId())
+                #Make sure the profile is available for the currently selected printer
+                profile.setMachineTypeId(self._manager.getActiveMachineInstance().getMachineDefinition().getProfilesMachineId())
                 self._manager.addProfile(profile) #Add the new profile to the list of profiles.
                 return { "status": "ok", "message": catalog.i18nc("@info:status", "Successfully imported profile {0}", profile.getName()) }
 
