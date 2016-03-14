0b8ce0ca94ebf9b35b6f7888792043f999e523f6
Jaime van Kessel <nallath@gmail.com>
2016-03-14 14:55:17 +0100

    Setting enabled is now correctly handled when dependant on instance values
    Required in order to fix CURA-926

--- UM/Settings/Setting.py
+++ UM/Settings/Setting.py
@@ -372,7 +372,7 @@ class Setting(SignalEmitter):
     #   Enabled settings can be displayed and used. Disabled settings should be ignored.
     def isEnabled(self):
         if self._enabled_function:
-            return self._enabled_function() == True #Force check. 
+            return bool(self._enabled_function()) == True #Force check.
 
         return True
 
