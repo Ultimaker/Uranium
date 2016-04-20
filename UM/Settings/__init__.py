1281121a21b78e47f4e9bdf32593e4a38b69b6ff
Arjen Hiemstra <ahiemstra@heimr.nl>
2016-04-21 01:46:25 +0200

    Remove all files related to machines
    Starting from a clean slate so we have no leftovers like the last
    time...

--- UM/Settings/__init__.py
+++
@@ -1,7 +0,0 @@
-# Copyright (c) 2015 Ultimaker B.V.
-# Uranium is released under the terms of the AGPLv3 or higher.
-
-#Shoop da woop!
-
-## \package Settings
-#  Machine setting storage and validation.
