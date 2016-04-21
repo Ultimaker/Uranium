98cdadf12fe93463df22a8f1fea171214638f2d6
Arjen Hiemstra <ahiemstra@heimr.nl>
2016-04-21 02:48:41 +0200

    If no loggers have been added, print logging messages to stdout
    This helps with getting logging output in tests

--- UM/Logger.py
+++ UM/Logger.py
@@ -33,6 +33,9 @@ class Logger:
             filled_message = message % args # Replace all the %s with the variables. Python formating is magic.
             logger.log(log_type, filled_message)
 
+        if not cls.__loggers:
+            print(message % args)
+
     ##
     @classmethod
     def logException(cls, log_type, message, *args):
