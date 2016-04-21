786a2d9b91f2838a241bba38e3f4004b524a8d74
Arjen Hiemstra <ahiemstra@heimr.nl>
2016-04-21 02:55:59 +0200

    Add some very limited initial tests for setting container classes
    Mostly they just try to create an instance of the containers. Since that
    fails at
    the moment the tests also fail.

---
+++ tests/Settings/TestInstanceContainer.py
@@ -0,0 +1,7 @@
+# Copyright (c) 2016 Ultimaker B.V.
+# Uranium is released under the terms of the AGPLv3 or higher.
+
+import UM.Settings
+
+def test_instance_container():
+    container = UM.Settings.InstanceContainer()
