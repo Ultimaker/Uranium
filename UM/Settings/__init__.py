737e2877cfad9ea801641b72094633a7c0178a44
Arjen Hiemstra <ahiemstra@heimr.nl>
2016-04-28 21:04:56 +0200

    Add ContainerRegistry to the exposed classes of UM.Settings

--- UM/Settings/__init__.py
+++ UM/Settings/__init__.py
@@ -1,8 +1,9 @@
 # Copyright (c) 2016 Ultimaker B.V.
 # Uranium is released under the terms of the AGPLv3 or higher.
 
-from .SettingDefinition import SettingDefinition
+from .ContainerRegistry import ContainerRegistry
 
+from .SettingDefinition import SettingDefinition
 from .SettingInstance import SettingInstance
 
 from .DefinitionContainer import DefinitionContainer
