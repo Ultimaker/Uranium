1f867075ec88e45c6f551f7c908b9a6198dd7b17
Ghostkeeper <rubend@tutanota.com>
2016-04-28 15:47:29 +0200

    Add boilerplate FloatValidator
    This implementation of FloatValidator implements nothing yet. Needs to be filled in.
    
    Contributes to issue CURA-1278.

---
+++ UM/Settings/Validators/FloatValidator.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2016 Ultimaker B.V.
+# Uranium is released under the terms of the AGPLv3 or higher.
+
+from UM.Settings.Validator import Validator
+
+##  Validates settings with floating point values.
+class FloatValidator(Validator):
+    pass #TODO: Implement this.
