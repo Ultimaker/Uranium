bf8469930d47294d0df3eaf3f0e15c250b4ba475
Ghostkeeper <rubend@tutanota.com>
2016-04-28 12:02:18 +0200

    Correct copyright year
    These files were made in 2016.
    
    Contributes to issue CURA-844.

--- tests/VersionUpgrade/TestVersionUpgradeManager.py
+++ tests/VersionUpgrade/TestVersionUpgradeManager.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2015 Ultimaker B.V.
+# Copyright (c) 2016 Ultimaker B.V.
 # Cura is released under the terms of the AGPLv3 or higher.
 
 from UM.Application import Application #To use the plug-in manager.
