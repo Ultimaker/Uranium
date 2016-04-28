afd32e4801d75ec7d4b0fa5466316048bb4ac87a
Arjen Hiemstra <ahiemstra@heimr.nl>
2016-04-28 21:13:05 +0200

    Expand DefinitionContainer test to include basic tests for SettingFunction

--- tests/Settings/TestDefinitionContainer.py
+++ tests/Settings/TestDefinitionContainer.py
@@ -7,40 +7,38 @@ import json
 import collections
 
 import UM.Settings
+from UM.Resources import Resources
 
-##  Custom subclass that overrides _loadFile so we search for files in test directories instead of resources.
-class CustomDefinitionContainer(UM.Settings.DefinitionContainer):
-    def _loadFile(self, file_name):
-        path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "definitions", file_name + ".json")
-        contents = {}
-        with open(path) as f:
-            contents = json.load(f, object_pairs_hook=collections.OrderedDict)
-        return contents
+Resources.addSearchPath(os.path.dirname(os.path.abspath(__file__)))
 
 test_definition_container_data = [
-    ("basic.json", { "name": "Test", "metadata": {}, "settings": {} }),
-    ("metadata.json", { "name": "Test", "metadata": { "author": "Ultimaker", "category": "Test" }, "settings": {} }),
-    ("single_setting.json", { "name": "Test", "metadata": {}, "settings": { "test_setting": { "label": "Test", "default_value": 10, "description": "A Test Setting" } } }),
-    ("multiple_settings.json", { "name": "Test", "metadata": {}, "settings": {
+    ("basic.def.json", { "name": "Test", "metadata": {}, "settings": {} }),
+    ("metadata.def.json", { "name": "Test", "metadata": { "author": "Ultimaker", "category": "Test" }, "settings": {} }),
+    ("single_setting.def.json", { "name": "Test", "metadata": {}, "settings": { "test_setting": { "label": "Test", "default_value": 10, "description": "A Test Setting" } } }),
+    ("multiple_settings.def.json", { "name": "Test", "metadata": {}, "settings": {
         "test_setting_0": { "label": "Test 0", "default_value": 10, "description": "A Test Setting" },
         "test_setting_1": { "label": "Test 1", "default_value": 10, "description": "A Test Setting" },
         "test_setting_2": { "label": "Test 2", "default_value": 10, "description": "A Test Setting" },
         "test_setting_3": { "label": "Test 3", "default_value": 10, "description": "A Test Setting" },
         "test_setting_4": { "label": "Test 4", "default_value": 10, "description": "A Test Setting" }
     }}),
-    ("children.json", { "name": "Test", "metadata": {}, "settings": {
+    ("children.def.json", { "name": "Test", "metadata": {}, "settings": {
         "test_setting": { "label": "Test", "default_value": 10, "description": "A Test Setting"},
         "test_child_0": { "label": "Test Child 0", "default_value": 10, "description": "A Test Setting"},
         "test_child_1": { "label": "Test Child 1", "default_value": 10, "description": "A Test Setting"},
     }}),
-    ("inherits.json", { "name": "Inherits", "metadata": {"author": "Ultimaker", "category": "Other", "manufacturer": "Ultimaker" }, "settings": {
+    ("inherits.def.json", { "name": "Inherits", "metadata": {"author": "Ultimaker", "category": "Other", "manufacturer": "Ultimaker" }, "settings": {
         "test_setting": { "label": "Test", "default_value": 10, "description": "A Test Setting" },
         "test_setting_1": { "label": "Test 1", "default_value": 10, "description": "A Test Setting" },
-    }})
+    }}),
+    ("functions.def.json", { "name": "Test", "metadata": {}, "settings": {
+        "test_setting_0": { "label": "Test 0", "default_value": 10, "description": "A Test Setting" },
+        "test_setting_1": { "label": "Test 1", "default_value": 10, "description": "A Test Setting", "value": UM.Settings.SettingFunction.SettingFunction("test_setting_0 * 10") },
+    }}),
 ]
 @pytest.mark.parametrize("file,expected", test_definition_container_data)
 def test_definition_container(file, expected):
-    container = CustomDefinitionContainer("test")
+    container = UM.Settings.DefinitionContainer("test")
     assert container.getId() == "test"
 
     json = ""
@@ -65,3 +63,33 @@ def test_definition_container(file, expected):
         for property, property_value in value.items():
             assert getattr(setting, property) == property_value
 
+def test_setting_function():
+    container = UM.Settings.DefinitionContainer("test")
+    with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), "definitions", "functions.def.json")) as data:
+        container.deserialize(data.read())
+
+    setting_0 = container.findDefinitions({"key": "test_setting_0"})[0]
+    setting_1 = container.findDefinitions({"key": "test_setting_1"})[0]
+
+    function = setting_1.value
+
+    assert len(setting_0.relations) == 1
+    assert len(setting_1.relations) == 1
+
+    relation_0 = setting_0.relations[0]
+    relation_1 = setting_1.relations[0]
+
+    assert relation_0.owner == setting_0
+    assert relation_0.target == setting_1
+    assert relation_0.type == UM.Settings.SettingRelation.SettingRelation.RelationType.RequiredByTarget
+    assert relation_0.role == "value"
+
+    assert relation_1.owner == setting_1
+    assert relation_1.target == setting_0
+    assert relation_1.type == UM.Settings.SettingRelation.SettingRelation.RelationType.RequiresTarget
+    assert relation_1.role == "value"
+
+    assert isinstance(function, UM.Settings.SettingFunction.SettingFunction)
+
+    result = function(container)
+    assert result == (setting_0.default_value * 10)
