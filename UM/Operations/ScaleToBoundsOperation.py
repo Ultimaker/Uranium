ae16750dd02d9e8509ac5fb3803586a03d106016
Ghostkeeper <rubend@tutanota.com>
2016-04-28 14:25:08 +0200

    Document ScaleToBoundsOperation
    Contributes to issue CURA-537.

--- UM/Operations/ScaleToBoundsOperation.py
+++ UM/Operations/ScaleToBoundsOperation.py
@@ -7,11 +7,15 @@ from UM.Math.Vector import Vector
 
 ##  Operation subclass that will scale a node to fit within the bounds provided.
 class ScaleToBoundsOperation(Operation):
+    ##  Creates a new operation that scales a nodes to the bounds.
+    #
+    #   \param node The node to scale to the bounds.
+    #   \param bounds The bounding box to scale the node to.
     def __init__(self, node, bounds):
         super().__init__()
 
         self._node = node
-        self._old_scale = node.getScale()
+        self._old_scale = node.getScale() # Store the old scale so that we can restore it.
 
         bbox = self._node.getBoundingBox()
 
@@ -20,8 +24,10 @@ class ScaleToBoundsOperation(Operation):
         # Apply scale factor on all different sizes to respect the (non-uniform) scaling that already has been done by the user.
         self._new_scale = Vector(self._old_scale.x * scale_factor, self._old_scale.y * scale_factor, self._old_scale.z * scale_factor)
 
+    ##  Undoes the scale to bounds, restoring the old scale.
     def undo(self):
         self._node.setScale(self._old_scale)
 
+    ##  Re-applies the scale to bounds after it has been undone.
     def redo(self):
         self._node.setScale(self._new_scale)
