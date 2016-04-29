dd87589c725ea5ddb2747cde6695580730f71ce0
Ghostkeeper <rubend@tutanota.com>
2016-04-29 13:06:14 +0200

    Add test for findDefinitionContainers
    The test currently adds mock containers with registry._containers.append(...), which is bad because it depends on the implementation of the registry (_containers is private). I don't know how to improve on this but it works.
    
    Contributes to issue CURA-1278.

--- tests/Settings/TestContainerRegistry.py
+++ tests/Settings/TestContainerRegistry.py
@@ -6,9 +6,40 @@ import os.path
 
 import UM.Settings
 import UM.PluginRegistry
+import UM.Settings.DefinitionContainer
 
 from UM.Resources import Resources
 from UM.MimeTypeDatabase import MimeType, MimeTypeDatabase
+
+##  Fake container class to add to the container registry.
+#
+#   This allows us to test the container registry without testing the container
+#   class. If something is wrong in the container class it won't influence this
+#   test.
+class MockContainer(UM.Settings.DefinitionContainer.DefinitionContainer):
+    ##  Initialise a new definition container.
+    #
+    #   The container will have the specified ID and all metadata in the
+    #   provided dictionary.
+    def __init__(self, id, metadata):
+        self._id = id
+        self._metadata = metadata
+
+    ##  Gets the ID that was provided at initialisation.
+    #
+    #   \return The ID of the container.
+    def getId(self):
+        return self._id
+
+    ##  Gets a metadata entry from the metadata dictionary.
+    #
+    #   \param key The key of the metadata entry.
+    #   \return The value of the metadata entry, or None if there is no such
+    #   entry.
+    def getMetaDataEntry(self, key):
+        if key in self._metadata:
+            return self._metadata[key]
+        return None
 
 @pytest.fixture
 def container_registry():
@@ -26,6 +57,104 @@ def container_registry():
 #   a fixture.
 def test_create(container_registry):
     assert container_registry != None
+
+##  Individual test cases for test_findDefinitionContainers.
+#
+#   Each entry has a descriptive name for debugging.
+#   Each entry also has a list of "containers", each of which is an arbitrary
+#   dictionary of metadata. Each dictionary MUST have an ID entry, which will be
+#   taken out and used as ID for the container.
+#   Each entry also has a "filter" dictionary, which is used as filter for the
+#   metadata of the containers.
+#   Each entry also has an "answer", which is a list of dictionaries again
+#   representing the containers that must be returned by the search.
+test_findDefinitionContainers_data = [
+    {
+        "name": "No containers",
+        "containers": [
+
+        ],
+        "filter": { "id": "a" },
+        "result": [
+            # Empty.
+        ]
+    },
+    {
+        "name": "Single ID match",
+        "containers": [
+            { "id": "a" },
+            { "id": "b" }
+        ],
+        "filter": { "id": "a" },
+        "result": [
+            { "id": "a" }
+        ]
+    },
+    {
+        "name": "Double ID match",
+        "containers": [
+            { "id": "a" },
+            { "id": "b" },
+            { "id": "a" }
+        ],
+        "filter": { "id": "a" },
+        "result": [
+            { "id": "a" },
+            { "id": "a" }
+        ]
+    },
+    {
+        "name": "Multiple constraints",
+        "containers": [
+            { "id": "a", "number": 1, "bool": False },
+            { "id": "b", "number": 2, "bool": False },
+            { "id": "c", "number": 2, "bool": True },
+            { "id": "d", "number": 2, "bool": False }
+        ],
+        "filter": { "number": 2, "bool": False },
+        "result": [
+            { "id": "b", "number": 2, "bool": False },
+            { "id": "d", "number": 2, "bool": False }
+        ]
+    },
+    {
+        "name": "Mixed Type",
+        "containers": [
+            { "id": "a", "number": 1, "mixed": "z" },
+            { "id": "b", "number": 1, "mixed": 9 },
+            { "id": "c", "number": 2, "mixed": 9 }
+        ],
+        "filter": { "number": 1, "mixed": 9 },
+        "result": [
+            { "id": "b", "number": 1, "mixed": 9 }
+        ]
+    }
+]
+
+@pytest.mark.parametrize("data", test_findDefinitionContainers_data)
+def test_findDefinitionContainers(container_registry, data):
+    for container in data["containers"]: # Fill the registry with mock containers.
+        id = container["id"]
+        del container["id"]
+        mock_container = MockContainer(id, container)
+        container_registry._containers.append(mock_container) # TODO: This is a private field we're adding to here...
+
+    result = container_registry.findDefinitionContainers(data["filter"]) # The actual function call we're testing.
+
+    for required_result in data["result"]:
+        # Each of these required results must appear somewhere in the result.
+        for observed_result in range(0, len(result)): # Need to iterate by index so we can delete the item if we found it.
+            for key, value in required_result.items():
+                if key not in result[observed_result]:
+                    break # No match.
+                if result[observed_result][key] != required_result[key]:
+                    break # No match.
+            else: # Not exited via a break, so it's a match!
+                del result[observed_result] # Delete it so we won't find it again.
+                break
+        else:
+            assert False # Didn't find any match.
+    assert len(result) == 0 # We've deleted all containers that were a match. There can be none left over.
 
 def test_load(container_registry):
     container_registry.load()
