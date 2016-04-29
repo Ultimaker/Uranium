923f6127eec0a6a576493f41d0f3b2fb9b6156d1
Ghostkeeper <rubend@tutanota.com>
2016-04-29 13:25:23 +0200

    Test creating container stack with fixture
    The fixture will automatically generate a unique ID.
    
    Contributes to issue CURA-1278.

--- tests/Settings/TestContainerStack.py
+++ tests/Settings/TestContainerStack.py
@@ -1,7 +1,18 @@
 # Copyright (c) 2016 Ultimaker B.V.
 # Uranium is released under the terms of the AGPLv3 or higher.
 
+import pytest
+import uuid # For creating unique ID's for each container stack.
+
 import UM.Settings
 
-def test_container_stack():
-    stack = UM.Settings.ContainerStack()
+##  Creates a brand new container stack to test with.
+#
+#   The container stack will get a new, unique ID.
+@pytest.fixture
+def container_stack():
+    return UM.Settings.ContainerStack(uuid.uuid4().int)
+
+def test_container_stack(container_stack):
+    assert container_stack != None
+
