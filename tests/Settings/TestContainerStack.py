6b20397f43286482b1b57bf16eb7507e4dc96dc1
Ghostkeeper <rubend@tutanota.com>
2016-04-29 15:32:01 +0200

    Add test for addContainer
    This test makes use of a mock container, which is an extremely basic implementation of the container. It shouldn't be used for anything complicated. But for addContainer it works fine... so if the container implementations break, this test won't!
    
    Contributes to issue CURA-1278.

--- tests/Settings/TestContainerStack.py
+++ tests/Settings/TestContainerStack.py
@@ -5,6 +5,80 @@ import pytest
 import uuid # For creating unique ID's for each container stack.
 
 import UM.Settings
+import UM.Settings.ContainerInterface
+
+##  A fake container class that implements ContainerInterface.
+#
+#   This allows us to test the container stack independent of any actual
+#   implementation of the containers. If something were to go wrong in the
+#   actual implementations, the tests in this suite are unaffected.
+class MockContainer(UM.Settings.ContainerInterface.ContainerInterface):
+    ##  Gets an arbitrary ID.
+    #
+    #   \return Some integer.
+    def getId(self):
+        return 42
+
+    ##  Gives an arbitrary name.
+    #
+    #   \return Some string.
+    def getName(self):
+        return "Fred"
+
+    ##  Gives empty metadata.
+    #
+    #   \return An empty dictionary representing the metadata.
+    def getMetaData(self):
+        return {}
+
+    ##  Gets an entry from the metadata.
+    #
+    #   Since the metadata is empty, this always returns the default.
+    #
+    #   \param entry The entry to get from the metadata.
+    #   \param default The default value in case the entry is missing.
+    #   \return The provided default value (None by default).
+    def getMetaDataEntry(self, entry, default = None):
+        return default
+
+    ##  Gets the value of a container item.
+    #
+    #   Since this container can't have any items, the result is always None. If
+    #   you wish to test something with actual items in it, please use an actual
+    #   implementation rather than this mock. This choice was made because
+    #   making this work for the mock container would have a high risk of bugs,
+    #   which would distract development time.
+    #
+    #   \param key The key of the item to get.
+    def getValue(self, key):
+        return None
+
+    ##  Serialises this container.
+    #
+    #   Since the container is always the same, a constant string suffices. For
+    #   a functional serialisation, please use an actual implementation rather
+    #   than this mock, because this container can't contain any items. This
+    #   choice was made because making this work for the mock container would
+    #   have a high risk of bugs, which would distract development time.
+    #
+    #   \return A static string representing a container.
+    def serialize(self):
+        return "x"
+
+    ##  Deserialises a string to a container.
+    #
+    #   Since this mock container is always the same, no deserialising can be
+    #   done and it only checks if the serialized string "has the proper
+    #   format". For a functional deserialisation, please use an actual
+    #   implementation rather than this mock, because this container can't
+    #   contain any items. This choice was made because making this work for the
+    #   mock container would have a high risk of bugs, which would distract
+    #   development time.
+    #
+    #   \param serialized A serialised mock container.
+    def deserialize(self, serialized):
+        if serialized != "x":
+            raise Exception("This serialisation doesn't have the correct format.")
 
 ##  Creates a brand new container stack to test with.
 #
@@ -20,6 +94,15 @@ def container_stack():
 #   \param container_stack A new container stack from a fixture.
 def test_container_stack(container_stack):
     assert container_stack != None
+
+##  Tests adding a container to the stack.
+#
+#   \param container_stack A new container stack from a fixture.
+def test_addContainer(container_stack):
+    assert container_stack.getContainers() == [] # First nothing.
+    container = MockContainer()
+    container_stack.addContainer(container)
+    assert container_stack.getContainers() == [container] # Then something!
 
 ##  Tests whether changing the name of the stack has the proper effects.
 #
