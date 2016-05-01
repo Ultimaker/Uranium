229fdc03331c0b05bea2bd169fc385d29c1d20df
Arjen Hiemstra <ahiemstra@heimr.nl>
2016-05-01 02:07:14 +0200

    Start implementing instancecontainer and associated test

--- UM/Settings/InstanceContainer.py
+++ UM/Settings/InstanceContainer.py
@@ -1,10 +1,21 @@
 # Copyright (c) 2016 Ultimaker B.V.
 # Uranium is released under the terms of the AGPLv3 or higher.
+import configparser
+
+import io
 
 from UM.Signal import Signal, signalemitter
 from UM.PluginObject import PluginObject
+from UM.Logger import Logger
 
 from . import ContainerInterface
+from . import SettingInstance
+
+class InvalidInstanceError(Exception):
+    pass
+
+class IncorrectInstanceVersionError(Exception):
+    pass
 
 ##  A container for SettingInstance objects.
 #
@@ -22,7 +33,7 @@ class InstanceContainer(ContainerInterface.ContainerInterface, PluginObject):
         self._id = container_id
         self._name = container_id
         self._metadata = {}
-        self._instances = []
+        self._instances = {}
 
     ##  \copydoc ContainerInterface::getId
     #
@@ -52,6 +63,10 @@ class InstanceContainer(ContainerInterface.ContainerInterface, PluginObject):
     #
     #   Reimplemented from ContainerInterface
     def getValue(self, key):
+        if key in self._instances:
+            return self._instances[key].value
+
+        Logger.log("w", "Tried to get value of setting %s that has no instance in container %s", key, repr(self))
         return None
 
     ##  Emitted whenever the value of an instance in this container changes.
@@ -62,22 +77,74 @@ class InstanceContainer(ContainerInterface.ContainerInterface, PluginObject):
     #   \param key \type{string} The key of the instance to set the value of.
     #   \param value The new value of the instance.
     def setValue(self, key, value):
-        pass
+        if key not in self._instances:
+            Logger.log("w", "Tried to set value of setting %s that has no instance in container %s", key, repr(self))
+            return
+
+        self._instances[key].setProperty("value", value)
 
     ##  \copydoc ContainerInterface::serialize
     #
     #   Reimplemented from ContainerInterface
-    def serialize(self):
-        return ""
+    def serialize(self, definition):
+        parser = configparser.ConfigParser(interpolation = None, empty_lines_in_values = False)
+
+        parser["general"] = {}
+        parser["general"]["version"] = self.Version
+        parser["general"]["name"] = self._name
+        parser["general"]["definition"] = definition.getId()
+
+        parser["metadata"] = {}
+        for key, value in self._metadata.items():
+            parser["metadata"][key] = value
+
+        parser["values"] = {}
+        for key, instance in self._instances:
+            parser["values"][key] = instance.value
+
+        stream = io.StringIO()
+        parser.write(stream)
+        return stream.getvalue()
 
     ##  \copydoc ContainerInterface::deserialize
     #
     #   Reimplemented from ContainerInterface
     def deserialize(self, serialized):
-        pass
+        parser = configparser.ConfigParser(interpolation = None, empty_lines_in_values = False)
+        parser.read_string(serialized)
+
+        if not "general" in parser or not "version" in parser["general"] or not "definition" in parser["general"]:
+            raise InvalidInstanceError("Missing required section 'general' or 'version' property")
+
+        if parser["general"].getint("version") != self.Version:
+            raise IncorrectInstanceVersionError("Reported version {0} but expected version {1}".format(parser["general"].getint("version"), self.Version))
+
+        self._name = parser["general"].get("name", self._id)
+
+        if "metadata" in parser:
+            self._metadata = parser["metadata"].copy()
+
+        if "values" in parser:
+            for key, value in parser["values"].items():
+                if not key in self._instances:
+
+                    self._instances[key] = SettingInstance.SettingInstance(definition, )
 
     ##  Find instances matching certain criteria.
     #
     #   \param filter \type{dict} A dictionary with key-value pairs that should match properties of the instances.
     def findInstances(self, filter):
         return []
+
+    def getInstance(self, key):
+        if key in self._instances:
+            return self._instances[key]
+
+        return None
+
+    def addInstance(self, instance):
+        key = instance.definition.key
+        if key in self._instances:
+            return
+
+        self._instances[key] = instance
--- tests/Settings/TestInstanceContainer.py
+++ tests/Settings/TestInstanceContainer.py
@@ -5,3 +5,51 @@ import UM.Settings
 
 def test_instance_container():
     container = UM.Settings.InstanceContainer()
+
+def test_advanced_setProperty(instance_container):
+    definition1 = UM.Settings.SettingDefinition("test_0", None)
+    definition1.deserialize({
+        "label": "Test 0",
+        "type": "float",
+        "description": "A Test Setting",
+        "default_value": 10.0,
+        "minimum": "test_1 / 10",
+    })
+
+    definition2 = UM.Settings.SettingDefinition("test_1", None)
+    definition2.deserialize({
+        "label": "Test 1",
+        "type": "float",
+        "description": "A Test Setting",
+        "default_value": 50.0,
+        "value": "test_0 * 5",
+        "maximum": "test_0 * 10"
+    })
+
+    # Manually set up relations between definition1 and definition2
+    # Normally this would be taken care of by the DefinitionContainer
+    definition1.relations.append(UM.Settings.SettingRelation.SettingRelation(owner = definition1, target = definition2, relation_type = UM.Settings.SettingRelation.RelationType.RequiredByTarget, role = "value"))
+    definition2.relations.append(UM.Settings.SettingRelation.SettingRelation(owner = definition2, target = definition1, relation_type = UM.Settings.SettingRelation.RelationType.RequiresTarget, role = "value"))
+    definition1.relations.append(UM.Settings.SettingRelation.SettingRelation(owner = definition1, target = definition2, relation_type = UM.Settings.SettingRelation.RelationType.RequiredByTarget, role = "maximum"))
+    definition2.relations.append(UM.Settings.SettingRelation.SettingRelation(owner = definition2, target = definition1, relation_type = UM.Settings.SettingRelation.RelationType.RequiresTarget, role = "maximum"))
+    definition1.relations.append(UM.Settings.SettingRelation.SettingRelation(owner = definition1, target = definition2, relation_type = UM.Settings.SettingRelation.RelationType.RequiresTarget, role = "minimum"))
+    definition2.relations.append(UM.Settings.SettingRelation.SettingRelation(owner = definition2, target = definition1, relation_type = UM.Settings.SettingRelation.RelationType.RequiredByTarget, role = "minimum"))
+
+    def1_instance = UM.Settings.SettingInstance(definition1, instance_container)
+    instance_container.addInstance(def1_instance)
+    def1_instance.setProperty("value", 20.0)
+
+    assert def1_instance.value == 20.0
+    assert def1_instance.minimum == 10.0
+
+    with pytest.raises(AttributeError):
+        assert def1_instance.maximum == 50.0
+
+    def2_instance = instance_container.getInstance("test_1")
+    assert def2_instance is not None
+    assert def2_instance.value == 100
+    assert def2_instance.maximum == 200
+
+    with pytest.raises(AttributeError):
+        assert def2_instance.minimum == 10.0
+
