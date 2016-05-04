8313648df906b0ca02d4462b84a757b067ba08bf
Arjen Hiemstra <ahiemstra@heimr.nl>
2016-05-04 17:26:21 +0200

    Add data to test validation of int values in Validator

--- tests/Settings/TestValidator.py
+++ tests/Settings/TestValidator.py
@@ -4,24 +4,20 @@
 import pytest
 import sys
 
-#from UM.Settings.Validators.FloatValidator import FloatValidator
 from UM.Settings.Validator import Validator, ValidatorState
-from UM.Signal import Signal
 
 ##  Fake setting instance that stands in for the real setting instance.
 #
 #   If anything should go wrong in the real setting instance, it won't
-#   influence the unit tests for the float validator.
+#   influence the unit tests for the validator.
 class MockSettingInstance:
     ##  Creates the mock setting instance.
     def __init__(self, value):
-        self.minimum_value = sys.float_info.min
-        self.maximum_value = sys.float_info.max
-        self.minimum_value_warning = sys.float_info.min
-        self.maximum_value_warning = sys.float_info.max
+        self.minimum_value = None
+        self.maximum_value = None
+        self.minimum_value_warning = None
+        self.maximum_value_warning = None
         self.value = value
-
-    propertyChanged = Signal()
 
 ##  Called before the first test function is executed.
 @pytest.fixture
@@ -35,42 +31,6 @@ def validator():
 def test_create(validator):
     assert validator.state == ValidatorState.Unknown
 
-###  Tests the changing of the maximum value.
-##
-##   \param validator A new validator from a fixture.
-#def test_setMaximum(validator):
-    #validator.validate() # To set the state to something else than Unknown.
-    #validator.setMaximum(3.14156)
-    #assert validator.getState() == ValidatorState.Unknown
-    #assert validator.getMaximum() == 3.14156
-
-###  Tests the changing of the maximum warning value.
-##
-##   \param validator A new validator from a fixture.
-#def test_setMaximumWarning(validator):
-    #validator.validate() # To set the state to something else than Unknown.
-    ##validator.setMaximumWarning(3.14156)
-    #assert validator.getState() == ValidatorState.Unknown
-    ##assert validator.getMaximumWarning() == 3.14156
-
-###  Tests the changing of the minimum value.
-##
-##   \param validator A new validator from a fixture.
-#def test_setMinimum(validator):
-    #validator.validate() # To set the state to something else than Unknown.
-    ##validator.setMinimum(3.14156)
-    #assert validator.state == ValidatorState.Unknown
-    ##assert validator.getMinimum() == 3.14156
-
-###  Tests the changing of the minimum warning value.
-##
-##   \param validator A new validator from a fixture.
-#def test_setMinimumWarning(validator):
-    ##validator.validate() # To set the state to something else than Unknown.
-    ##validator.setMinimumWarning(3.14156)
-    #assert validator.state == ValidatorState.Unknown
-    ##assert validator.getMinimumWarning() == 3.14156
-
 ##  The individual test cases for validate().
 #
 #   Each entry in the list is a test.
@@ -82,6 +42,9 @@ def test_create(validator):
 #   - A current value for the setting.
 #   - The answer: A state of the validator after validating the setting.
 test_validate_data = [
+    # Empty values
+    ({"description": "Everything None",      "minimum": None, "maximum": None,         "min_warning": None,         "max_warning": None, "current": None,         "answer": ValidatorState.Exception}),
+    # Floating point values
     ({"description": "Completely valid",     "minimum": 0.0,  "maximum": 10.0,         "min_warning": 1.0,          "max_warning": 9.0,  "current": 5.0,          "answer": ValidatorState.Valid}),
     ({"description": "Exactly MinWarning",   "minimum": 0.0,  "maximum": 10.0,         "min_warning": 1.0,          "max_warning": 9.0,  "current": 1.0,          "answer": ValidatorState.Valid}),
     ({"description": "Exactly MaxWarning",   "minimum": 0.0,  "maximum": 10.0,         "min_warning": 1.0,          "max_warning": 9.0,  "current": 9.0,          "answer": ValidatorState.Valid}),
@@ -93,10 +56,27 @@ test_validate_data = [
     ({"description": "Above Maximum",        "minimum": 0.0,  "maximum": 10.0,         "min_warning": 1.0,          "max_warning": 9.0,  "current": 11.0,         "answer": ValidatorState.MaximumError}),
     ({"description": "Float precision test", "minimum": 0.0,  "maximum": 100000.0,     "min_warning": 1.0,          "max_warning": 9.0,  "current": 100000.0,     "answer": ValidatorState.MaximumWarning}),
     ({"description": "NaN value",            "minimum": 0.0,  "maximum": 10.0,         "min_warning": 1.0,          "max_warning": 9.0,  "current": float("nan"), "answer": ValidatorState.Exception}),
-    ({"description": "NaN MinWarning",       "minimum": 0.0,  "maximum": 10.0,         "min_warning": float("nan"), "max_warning": 9.0,  "current": 5.0,          "answer": ValidatorState.Exception}),
+    ({"description": "NaN MinWarning",       "minimum": 0.0,  "maximum": 10.0,         "min_warning": float("nan"), "max_warning": 9.0,  "current": 1.0,          "answer": ValidatorState.Valid}),
     ({"description": "Infinity Maximum",     "minimum": 0.0,  "maximum": float("inf"), "min_warning": 1.0,          "max_warning": 9.0,  "current": 5.0,          "answer": ValidatorState.Valid}),
     ({"description": "Maximum < MaxWarning", "minimum": 0.0,  "maximum": 10.0,         "min_warning": 1.0,          "max_warning": 15.0, "current": 12.5,         "answer": ValidatorState.MaximumError}),
-    ({"description": "Maximum < Minimum",    "minimum": 15.0, "maximum": 10.0,         "min_warning": 1.0,          "max_warning": 9.0,  "current": 12.5,         "answer": ValidatorState.Exception})
+    ({"description": "Maximum < Minimum",    "minimum": 15.0, "maximum": 10.0,         "min_warning": 1.0,          "max_warning": 9.0,  "current": 12.5,         "answer": ValidatorState.Exception}),
+    ({"description": "None Maximum",         "minimum": 0.0,  "maximum": None,         "min_warning": 1.0,          "max_warning": 9.0,  "current": 11.0,         "answer": ValidatorState.MaximumWarning}),
+    ({"description": "None Minimum",         "minimum": None, "maximum": 10.0,         "min_warning": 1.0,          "max_warning": 9.0,  "current": -1.0,         "answer": ValidatorState.MinimumWarning}),
+    # Integer values
+    ({"description": "Completely valid",     "minimum": 0,    "maximum": 10,           "min_warning": 1,            "max_warning": 9,    "current": 5,            "answer": ValidatorState.Valid}),
+    ({"description": "Exactly MinWarning",   "minimum": 0,    "maximum": 10,           "min_warning": 1,            "max_warning": 9,    "current": 1,            "answer": ValidatorState.Valid}),
+    ({"description": "Exactly MaxWarning",   "minimum": 0,    "maximum": 10,           "min_warning": 1,            "max_warning": 9,    "current": 9,            "answer": ValidatorState.Valid}),
+    ({"description": "Below MinWarning",     "minimum": -1,   "maximum": 10,           "min_warning": 1,            "max_warning": 9,    "current": 0,            "answer": ValidatorState.MinimumWarning}),
+    ({"description": "Above MaxWarning",     "minimum": 0,    "maximum": 11,           "min_warning": 1,            "max_warning": 9,    "current": 10,           "answer": ValidatorState.MaximumWarning}),
+    ({"description": "Exactly Minimum",      "minimum": 0,    "maximum": 10,           "min_warning": 1,            "max_warning": 9,    "current": 0,            "answer": ValidatorState.MinimumWarning}),
+    ({"description": "Exactly Maximum",      "minimum": 0,    "maximum": 10,           "min_warning": 1,            "max_warning": 9,    "current": 10,           "answer": ValidatorState.MaximumWarning}),
+    ({"description": "Below Minimum",        "minimum": 0,    "maximum": 10,           "min_warning": 1,            "max_warning": 9,    "current": -1,           "answer": ValidatorState.MinimumError}),
+    ({"description": "Above Maximum",        "minimum": 0,    "maximum": 10,           "min_warning": 1,            "max_warning": 9,    "current": 11,           "answer": ValidatorState.MaximumError}),
+    ({"description": "Float precision test", "minimum": 0,    "maximum": 100000,       "min_warning": 1,            "max_warning": 9,    "current": 100000,       "answer": ValidatorState.MaximumWarning}),
+    ({"description": "Maximum < MaxWarning", "minimum": 0,    "maximum": 10,           "min_warning": 1,            "max_warning": 15,   "current": 13,           "answer": ValidatorState.MaximumError}),
+    ({"description": "Maximum < Minimum",    "minimum": 15,   "maximum": 10,           "min_warning": 1,            "max_warning": 9,    "current": 13,           "answer": ValidatorState.Exception}),
+    # Mixed values
+    ({"description": "Completely valid",     "minimum": 0,    "maximum": 10.0,         "min_warning": 1.0,          "max_warning": 9,    "current": 5.0,          "answer": ValidatorState.Valid}),
 ]
 
 @pytest.mark.parametrize("data", test_validate_data)
