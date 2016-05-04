c2dea66b9b4af1b53aaf1836cea9e139e333cb4d
Arjen Hiemstra <ahiemstra@heimr.nl>
2016-05-04 17:26:42 +0200

    Add a test for SettingInstance::validationState

--- tests/Settings/TestSettingInstance.py
+++ tests/Settings/TestSettingInstance.py
@@ -37,6 +37,7 @@ def test_setProperty(setting_definition, instance_container):
     instance.setProperty("value", 20.0)
     assert instance.value == 20.0
     assert instance.state == UM.Settings.InstanceState.User
+    assert instance.validationState == UM.Settings.ValidatorState.Valid
 
     with pytest.raises(AttributeError):
         instance.setProperty("something", 10)
@@ -50,3 +51,38 @@ def test_updateProperty(setting_definition, instance_container):
     # We are updating a property that is not value, so state should not change.
     assert instance.state == UM.Settings.InstanceState.Default
 
+    # Since we only update maximum_value, value will not be set and validation will have nothing to validate
+    assert instance.validationState == UM.Settings.ValidatorState.Unknown
+
+test_validationState_data = [
+    {"value": 10.0, "state": UM.Settings.ValidatorState.Valid},
+    {"value": 4.0, "state": UM.Settings.ValidatorState.MinimumWarning},
+    {"value": 19.0, "state": UM.Settings.ValidatorState.MaximumWarning},
+    {"value": -1.0, "state": UM.Settings.ValidatorState.MinimumError},
+    {"value": 33.0, "state": UM.Settings.ValidatorState.MaximumError},
+]
+@pytest.mark.parametrize("data", test_validationState_data)
+def test_validationState(data, instance_container):
+    definition = UM.Settings.SettingDefinition("test", None)
+    definition.deserialize({
+        "label": "Test",
+        "type": "float",
+        "description": "A Test Setting",
+        "default_value": 10.0,
+        "minimum_value": "0.0",
+        "maximum_value": "20.0",
+        "minimum_value_warning": "5.0",
+        "maximum_value_warning": "15.0",
+    })
+
+    instance = UM.Settings.SettingInstance(definition, instance_container)
+
+    # Ensure the instance is filled with proper values for the validation range
+    instance.updateProperty("minimum_value")
+    instance.updateProperty("maximum_value")
+    instance.updateProperty("minimum_value_warning")
+    instance.updateProperty("maximum_value_warning")
+
+    instance.setProperty("value", data["value"])
+
+    assert instance.validationState == data["state"]
