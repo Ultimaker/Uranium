18a2fde9b573e4db34e247cdd8dce6f506d8e34e
Ghostkeeper <rubend@tutanota.com>
2016-05-06 13:55:24 +0200

    Add test suite for SettingRelation
    Extremely simple test since SettingRelation is essentially a tuple.
    
    Contributes to issue CURA-1278.

---
+++ tests/Settings/TestSettingRelation.py
@@ -0,0 +1,25 @@
+# Copyright (c) 2016 Ultimaker B.V.
+# Uranium is released under the terms of the AGPLv3 or higher.
+
+import pytest
+
+import UM.Settings.SettingRelation
+
+def test_create():
+    with pytest.raises(ValueError):
+        relation = UM.Settings.SettingRelation.SettingRelation(None, 2, UM.Settings.SettingRelation.RelationType.RequiresTarget, "max")
+
+    with pytest.raises(ValueError):
+        relation = UM.Settings.SettingRelation.SettingRelation(1, None, UM.Settings.SettingRelation.RelationType.RequiresTarget, "max")
+
+    relation = UM.Settings.SettingRelation.SettingRelation(1, 2, UM.Settings.SettingRelation.RelationType.RequiresTarget, "max")
+    assert relation.owner == 1
+    assert relation.target == 2
+    assert relation.type == UM.Settings.SettingRelation.RelationType.RequiresTarget
+    assert relation.role == "max"
+
+    relation = UM.Settings.SettingRelation.SettingRelation(1, 2, UM.Settings.SettingRelation.RelationType.RequiredByTarget, "min")
+    assert relation.owner == 1
+    assert relation.target == 2
+    assert relation.type == UM.Settings.SettingRelation.RelationType.RequiredByTarget
+    assert relation.role == "min"
