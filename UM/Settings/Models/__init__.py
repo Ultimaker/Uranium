1d8e6f3a51ce80cbb6739b9a3fa1548ddd93612f
Jaime van Kessel <nallath@gmail.com>
2016-05-09 09:41:53 +0200

    Added stub for definitionContainersModel
    CURA-1278

--- UM/Settings/Models/__init__.py
+++ UM/Settings/Models/__init__.py
@@ -2,3 +2,4 @@
 # Uranium is released under the terms of the AGPLv3 or higher.
 
 from .SettingDefinitionsModel import SettingDefinitionsModel
+from .DefinitionContainersModel import DefinitionContainersModel
