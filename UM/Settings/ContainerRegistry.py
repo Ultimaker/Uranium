92960421bf4b5a9775b89c78d8b9f2143d8438d7
Arjen Hiemstra <ahiemstra@heimr.nl>
2016-05-16 17:49:51 +0200

    Expose ContainerRegistry::_findContainers as public method

--- UM/Settings/ContainerRegistry.py
+++ UM/Settings/ContainerRegistry.py
@@ -65,19 +65,52 @@ class ContainerRegistry:
     #
     #   \param kwargs \type{dict} A dictionary of keyword arguments containing keys and values that need to match the metadata of the DefinitionContainer.
     def findDefinitionContainers(self, **kwargs):
-        return self._findContainers(DefinitionContainer.DefinitionContainer, **kwargs)
+        return self.findContainers(DefinitionContainer.DefinitionContainer, **kwargs)
 
     ##  Find all InstanceContainer objects matching certain criteria.
     #
     #   \param kwargs \type{dict} A dictionary of keyword arguments containing keys and values that need to match the metadata of the InstanceContainer.
     def findInstanceContainers(self, **kwargs):
-        return self._findContainers(InstanceContainer.InstanceContainer, **kwargs)
+        return self.findContainers(InstanceContainer.InstanceContainer, **kwargs)
 
     ##  Find all ContainerStack objects matching certain criteria.
     #
     #   \param kwargs \type{dict} A dictionary of keyword arguments containing keys and values that need to match the metadata of the ContainerStack.
     def findContainerStacks(self, **kwargs):
-        return self._findContainers(ContainerStack.ContainerStack, **kwargs)
+        return self.findContainers(ContainerStack.ContainerStack, **kwargs)
+
+    ##  Find all container objects matching certain criteria
+    #
+    #   \param container_type If provided, return only objects that are instances or subclasses of container_type.
+    #   \param kwargs \type{dict} A dictionary of keyword arguments containing keys and values that need to match the metadata of the container.
+    #
+    #   \return A list of containers matching the search criteria, or an empty list if nothing was found.
+    def findContainers(self, container_type = None, **kwargs):
+        containers = []
+        for container in self._containers:
+            if container_type and not isinstance(container, container_type):
+                continue
+
+            matches_container = True
+            for key, value in kwargs.items():
+                if key == "id":
+                    if container.getId() != value:
+                        matches_container = False
+                    continue
+                if key == "definition":
+                    try:
+                        if container.getDefinition().getId() != value:
+                            matches_container = False
+                        continue
+                    except AttributeError:  # Only instanceContainers have a get definition. We can ignore all others.
+                        pass
+                if container.getMetaDataEntry(key) != value:
+                    matches_container = False
+
+            if matches_container:
+                containers.append(container)
+
+        return containers
 
     ##  Add a container type that will be used to serialize/deserialize containers.
     #
@@ -114,7 +147,7 @@ class ContainerRegistry:
             self._containers.append(new_container)
 
     def addContainer(self, container):
-        containers = self._findContainers(None, id = container.getId())
+        containers = self.findContainers(None, id = container.getId())
         if containers:
             Logger.log("w", "Container of type %s and id %s already added", repr(container.__class__), container.getId())
             return
@@ -123,7 +156,7 @@ class ContainerRegistry:
         self.containerAdded.emit(container)
 
     def removeContainer(self, container_id):
-        containers = self._findContainers(None, id = container_id)
+        containers = self.findContainers(None, id = container_id)
         if containers:
             self._containers.remove(containers[0])
             self.containerRemoved.emit(containers[0])
@@ -152,33 +185,6 @@ class ContainerRegistry:
             path = Resources.getStoragePath(Resources.DefinitionContainers, file_name)
             with SaveFile(path, "wt", -1, "utf-8") as f:
                 f.write(data)
-
-    def _findContainers(self, container_type, **kwargs):
-        containers = []
-        for container in self._containers:
-            if container_type and not isinstance(container, container_type):
-                continue
-
-            matches_container = True
-            for key, value in kwargs.items():
-                if key == "id":
-                    if container.getId() != value:
-                        matches_container = False
-                    continue
-                if key == "definition":
-                    try:
-                        if container.getDefinition().getId() != value:
-                            matches_container = False
-                        continue
-                    except AttributeError:  # Only instanceContainers have a get definition. We can ignore all others.
-                        pass
-                if container.getMetaDataEntry(key) != value:
-                    matches_container = False
-
-            if matches_container:
-                containers.append(container)
-
-        return containers
 
     ##  Get the singleton instance for this class.
     @classmethod
