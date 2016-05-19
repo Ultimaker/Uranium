3b48fbab4a8cdfdda378e8e534f6e12a2360fb71
Simon Edwards <s.edwards@ultimaker.com>
2016-05-19 14:04:46 +0200

    Updated the STLReader to use MeshBuilder. Contributes to CURA-1504

--- plugins/FileHandlers/STLReader/STLReader.py
+++ plugins/FileHandlers/STLReader/STLReader.py
@@ -3,17 +3,15 @@
 # Uranium is released under the terms of the AGPLv3 or higher.
 
 from UM.Mesh.MeshReader import MeshReader
-from UM.Mesh.MeshData import MeshData
+from UM.Mesh.MeshBuilder import MeshBuilder
 from UM.Logger import Logger
-from UM.Math.Matrix import Matrix
-from UM.Math.Vector import Vector
 from UM.Scene.SceneNode import SceneNode
 from UM.Job import Job
 
 import os
 import struct
-import math
 import time
+
 
 class STLReader(MeshReader):
     def __init__(self):
@@ -22,26 +20,26 @@ class STLReader(MeshReader):
 
     ## Decide if we need to use ascii or binary in order to read file
     def read(self, file_name):
-        mesh = None
-        scene_node = None
-
-        mesh = MeshData()
+        mesh_builder = MeshBuilder()
+        mesh_builder.setFileName(file_name)
         scene_node = SceneNode()
         f = open(file_name, "rb")
-        if not self._loadBinary(mesh, f):
+        if not self._loadBinary(mesh_builder, f):
             f.close()
             f = open(file_name, "rt")
             try:
-                self._loadAscii(mesh, f)
+                self._loadAscii(mesh_builder, f)
             except UnicodeDecodeError:
                 return None
             f.close()
 
         f.close()
-        time.sleep(0.1) #Yield somewhat to ensure the GUI has time to update a bit.
-        mesh.calculateNormals(fast = True)
+        time.sleep(0.1)  # Yield somewhat to ensure the GUI has time to update a bit.
+        mesh_builder.calculateNormals(fast=True)
 
-        Logger.log("d", "Loaded a mesh with %s vertices", mesh.getVertexCount())
+        mesh = mesh_builder.build()
+        Logger.log("d", "Loaded a mesh with %s vertices", mesh_builder.getVertexCount())
+        Logger.log('d', 'Loaded mesh: ' + mesh.toString())
         scene_node.setMeshData(mesh)
         return scene_node
 
@@ -49,14 +47,14 @@ class STLReader(MeshReader):
     ## Load the STL data from file by consdering the data as ascii.
     # \param mesh The MeshData object where the data is written to.
     # \param f The file handle
-    def _loadAscii(self, mesh, f):
+    def _loadAscii(self, mesh_builder, f):
         num_verts = 0
         for lines in f:
             for line in lines.split("\r"):
                 if "vertex" in line:
                     num_verts += 1
 
-        mesh.reserveFaceCount(num_verts / 3)
+        mesh_builder.reserveFaceCount(num_verts / 3)
         f.seek(0, os.SEEK_SET)
         vertex = 0
         face = [None, None, None]
@@ -66,7 +64,7 @@ class STLReader(MeshReader):
                     face[vertex] = line.split()[1:]
                     vertex += 1
                     if vertex == 3:
-                        mesh.addFace(
+                        mesh_builder.addFaceByPoints(
                             float(face[0][0]), float(face[0][2]), -float(face[0][1]),
                             float(face[1][0]), float(face[1][2]), -float(face[1][1]),
                             float(face[2][0]), float(face[2][2]), -float(face[2][1])
@@ -79,9 +77,9 @@ class STLReader(MeshReader):
     ## Load the STL data from file by consdering the data as Binary.
     # \param mesh The MeshData object where the data is written to.
     # \param f The file handle
-    def _loadBinary(self, mesh, f):
-        f.read(80) #Skip the header
-        
+    def _loadBinary(self, mesh_builder, f):
+        f.read(80)  # Skip the header
+
         num_faces = struct.unpack("<I", f.read(4))[0]
         # On ascii files, the num_faces will be big, due to 4 ascii bytes being seen as an unsigned int.
         if num_faces < 1 or num_faces > 1000000000:
@@ -92,10 +90,10 @@ class STLReader(MeshReader):
         if file_size < num_faces * 50 + 84:
             return False
 
-        mesh.reserveFaceCount(num_faces)
+        mesh_builder.reserveFaceCount(num_faces)
         for idx in range(0, num_faces):
             data = struct.unpack(b"<ffffffffffffH", f.read(50))
-            mesh.addFace(
+            mesh_builder.addFaceByPoints(
                 data[3], data[5], -data[4],
                 data[6], data[8], -data[7],
                 data[9], data[11], -data[10]
