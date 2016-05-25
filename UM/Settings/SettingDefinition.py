a0e7a5b3b535eb3b05797fcca164da24fe999014
Jaime van Kessel <nallath@gmail.com>
2016-05-25 11:54:01 +0200

    Filtering of setting defintion now always assumes lower case
    CURA-1278

--- UM/Settings/SettingDefinition.py
+++ UM/Settings/SettingDefinition.py
@@ -178,8 +178,8 @@ class SettingDefinition:
                 try:
                     if "*" in value:  # Don't look for exact match but if the value is contained in it.
                         key_value = getattr(self, key)
-                        value = value.strip("*")
-                        if value not in key_value:
+                        value = value.strip("*").lower()
+                        if value not in key_value.lower():
                             return False
                     else:
                         if getattr(self, key) != value:
