2abe549ff9ca290793b1b089545d2e05ed30546b
Jaime van Kessel <nallath@gmail.com>
2016-06-24 11:16:54 +0200

    Removed unused imports

--- UM/Qt/QtApplication.py
+++ UM/Qt/QtApplication.py
@@ -11,20 +11,14 @@ from PyQt5.QtQml import QQmlApplicationEngine, qmlRegisterType, qmlRegisterSingl
 from PyQt5.QtWidgets import QApplication, QSplashScreen
 from PyQt5.QtGui import QGuiApplication, QPixmap
 from PyQt5.QtCore import QTimer
-# Load the SVG image loader plugin. This module isn't directly used, but is needed for SVG icons.
-import PyQt5.QtSvg
-
 from UM.Application import Application
 from UM.Qt.QtRenderer import QtRenderer
 from UM.Qt.Bindings.Bindings import Bindings
-from UM.JobQueue import JobQueue
 from UM.Signal import Signal, signalemitter
 from UM.Resources import Resources
 from UM.Logger import Logger
 from UM.Preferences import Preferences
 from UM.i18n import i18nCatalog
-
-from UM.Settings.ContainerRegistry import ContainerRegistry
 
 # Raised when we try to use an unsupported version of a dependency.
 class UnsupportedVersionError(Exception):
