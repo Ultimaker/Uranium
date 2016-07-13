4798ace36be2169d1d252355173a655a7d03ddf7
Jaime van Kessel <nallath@gmail.com>
2016-07-13 17:23:37 +0200

    Property provider now checks if a value needs to be updated before doing so
    CURA-1842

--- UM/Settings/Models/SettingPropertyProvider.py
+++ UM/Settings/Models/SettingPropertyProvider.py
@@ -155,6 +155,16 @@ class SettingPropertyProvider(QObject):
         if isinstance(container, UM.Settings.DefinitionContainer):
             return
 
+        if property_name == "value":
+            for index in self._stack_levels:
+                if index > self._store_index:
+                    old_value = self._stack.getContainer(index).getProperty(self._key, property_name)
+                    if str(old_value) == str(property_value) and str(self._stack.getContainer(self._store_index).getProperty(self._key, "state")) != "InstanceState.Calculated":
+                        # If we change the setting so that it would be the same as a deeper setting, we can just remove
+                        # the value. Note that we only do this when this is not caused by the calculated state
+                        # In this case the setting does need to be set, as it needs to be stored in the user settings.
+                        self.removeFromContainer(self._store_index)
+                        return
         container.setProperty(self._key, property_name, property_value, self._stack)
 
     ##  Manually request the value of a property.
