68e62b56c836665e0446e2335000ae9a029302f7
fieldOfView <aldo@fieldofview.com>
2016-07-29 16:36:20 +0200

    Sort DefinitionContainersModel by category and name
    CURA-1593

--- UM/Settings/Models/DefinitionContainersModel.py
+++ UM/Settings/Models/DefinitionContainersModel.py
@@ -10,14 +10,16 @@ from UM.Settings.DefinitionContainer import DefinitionContainer
 #   changed.
 class DefinitionContainersModel(ListModel):
     NameRole = Qt.UserRole + 1          # Human readable name (string)
-    ManufacturerRole = Qt.UserRole + 2  # Manufacturer of definition / machine. (string)
-    IdRole = Qt.UserRole + 3            # Unique ID of Definition
+    IdRole = Qt.UserRole + 2            # Unique ID of Definition
+    CategoryRole = Qt.UserRole + 3      # Category of definition / machine. (string)
+    ManufacturerRole = Qt.UserRole + 4  # Manufacturer of definition / machine. (string)
 
     def __init__(self, parent = None):
         super().__init__(parent)
         self.addRoleName(self.NameRole, "name")
-        self.addRoleName(self.ManufacturerRole, "manufacturer")
         self.addRoleName(self.IdRole, "id")
+        self.addRoleName(self.CategoryRole, "category")
+        self.addRoleName(self.ManufacturerRole, "manufacturer")
 
         self._definition_containers = []
 
@@ -39,21 +41,14 @@ class DefinitionContainersModel(ListModel):
         self.clear()
         self._definition_containers = ContainerRegistry.getInstance().findDefinitionContainers(**self._filter_dict)
         for container in self._definition_containers:
-            # Use insertion sort to keep the list sorted by manufacturer.
-            # The view doesn't support sorting.
             item = { # Prepare an item for insertion.
                 "name": container.getName(),
-                "manufacturer": container.getMetaDataEntry("manufacturer", ""),
-                "id": container.getId()
+                "id": container.getId(),
+                "category": container.getMetaDataEntry("category", ""),
+                "manufacturer": container.getMetaDataEntry("manufacturer", "")
             }
-            manufacturer = item["manufacturer"]
-            for i, other_item in enumerate(self.items):
-                if manufacturer < other_item["manufacturer"]: # Need to insert it before this one.
-                    self.insertItem(i, item)
-                    break
-            else: # No existing manufacturer was greater, so put it at the end.
-                self.appendItem(item)
-        self.sort(lambda k: k["name"])
+            self.appendItem(item)
+        self.sort(lambda k: (k["category"].lower(), k["name"].lower()))
 
     ##  Set the filter of this model based on a string.
     #   \param filter_dict Dictionary to do the filtering by.
