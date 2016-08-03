62be51f72168cfbbc6f5770f1bf65c3d574b445a
Ghostkeeper <rubend@tutanota.com>
2016-08-03 15:34:05 +0200

    Restore check all functionality
    rowCount was not yet a pyqtSlot. It was required to determine whether the checkbox should be in the checked state, unchecked state or indeterminate state.
    
    Contributes to issue CURA-2039.

--- UM/Settings/Models/SettingDefinitionsModel.py
+++ UM/Settings/Models/SettingDefinitionsModel.py
@@ -4,7 +4,7 @@
 import collections
 import itertools
 
-from PyQt5.QtCore import Qt, QAbstractListModel, QVariant, QModelIndex, pyqtProperty, pyqtSignal, pyqtSlot
+from PyQt5.QtCore import Qt, QAbstractListModel, QVariant, QModelIndex, QObject, pyqtProperty, pyqtSignal, pyqtSlot
 
 from UM.Logger import Logger
 from UM.Preferences import Preferences
@@ -360,6 +360,7 @@ class SettingDefinitionsModel(QAbstractListModel):
         return result
 
     ##  Reimplemented from QAbstractListModel
+    @pyqtSlot(QObject, result = int)
     def rowCount(self, parent = None):
         if not self._container:
             return 0
