f164d41a0c336e375b3572c142f08dd30df89635
Jack Ha <j.ha@ultimaker.com>
2016-08-03 16:40:03 +0200

    Added comment CURA-2039

--- UM/Settings/Models/SettingDefinitionsModel.py
+++ UM/Settings/Models/SettingDefinitionsModel.py
@@ -360,6 +360,9 @@ class SettingDefinitionsModel(QAbstractListModel):
         return result
 
     ##  Reimplemented from QAbstractListModel
+    # Note that rowCount() is overridden from QAbstractItemModel. The signature of the method in that
+    # class is "int rowCount(const QModelIndex& parent)" which makes this slot declaration incorrect.
+    # TODO: fix the pointer when actually using this parameter.
     @pyqtSlot(QObject, result = int)
     def rowCount(self, parent = None):
         if not self._container:
