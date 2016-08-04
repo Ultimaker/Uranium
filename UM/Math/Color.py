94035e285f5a2da0e271b75e9458e2d11d780d57
fieldOfView <aldo@fieldofview.com>
2016-08-04 17:04:53 +0200

    Take alpha into consideration when converting to/from hex colors

--- UM/Math/Color.py
+++ UM/Math/Color.py
@@ -68,24 +68,35 @@ class Color:
             (value & 0xff000000) >> 24
         )
 
-    ##  Returns a new Color constructed from a 7-character string "#RRGGBB" format.
+    ##  Returns a new Color constructed from a 7- or 9-character string "#RRGGBB" or "#AARRGGBB" format.
     #
-    #   \param value A 7-character string representing a color in "#RRGGBB" format.
+    #   \param value A 7- or 9-character string representing a color in "#RRGGBB" or "#AARRGGBB" format.
     #   \return A Color constructed from the components of value.
     @staticmethod
-    def fromRGBString(value):
-        return Color(
-            int(value[1:3], 16) / 255,
-            int(value[3:5], 16) / 255,
-            int(value[5:7], 16) / 255,
-            1.0
-        )
+    def fromHexString(value):
+        if len(value) == 9:
+            return Color(
+                int(value[3:5], 16) / 255,
+                int(value[5:7], 16) / 255,
+                int(value[7:9], 16) / 255,
+                int(value[1:3], 16) / 255
+            )
+        else:
+            return Color(
+                int(value[1:3], 16) / 255,
+                int(value[3:5], 16) / 255,
+                int(value[5:7], 16) / 255,
+                1.0
+            )
 
     ##  Returns a 7-character string in "#RRGGBB" format representing the color.
     #
-    #   \return A 7-character string representing a color in "#RRGGBB" format.
-    def toRGBString(self):
+    #   \param include_alpha Whether to return a 7-character "#RRGGBB" or a 9 character "#AARRGGBB" format.
+    #   \return A 7- or 9-character string representing a color in "#AARRGGBB" format.
+    def toHexString(self, include_alpha = False):
         value = ((int(self._r * 255) & 255) << 16) + \
                 ((int(self._g * 255) & 255) << 8) + \
                 (int(self._b * 255) & 255)
+        if include_alpha:
+            value += (int(self._a * 255) & 255) << 24
         return "#%s" % hex(value)[2:]
