f1b874a41d594603eb7a59fd93d8d95b552e3ab0
fieldOfView <aldo@fieldofview.com>
2016-08-08 14:22:18 +0200

    Add getVisible to SettingDefinitionsModel
    CURA-1799

--- UM/Settings/Models/SettingDefinitionsModel.py
+++ UM/Settings/Models/SettingDefinitionsModel.py
@@ -304,6 +304,11 @@ class SettingDefinitionsModel(QAbstractListModel):
         if self._visibility_handler:
             self._visibility_handler.setVisible(self._visible)
 
+    ##  Get a single SettingDefinition's visible state
+    @pyqtSlot(str, result = bool)
+    def getVisible(self, key):
+        return key in self._visible
+
     @pyqtSlot(str, result = int)
     def getIndex(self, key):
         definitions = self._container.findDefinitions(key = key)
