0583c47a45951b71c793b0fbb374d85e747e61b1
fieldOfView <aldo@fieldofview.com>
2016-09-14 14:45:55 +0200

    Fix sorting instancecontainers by weight
    No need to sort twice

--- UM/Settings/Models/InstanceContainersModel.py
+++ UM/Settings/Models/InstanceContainersModel.py
@@ -71,7 +71,6 @@ class InstanceContainersModel(ListModel):
                 "readOnly": container.isReadOnly(),
                 "section": container.getMetaDataEntry(self._section_property, ""),
             })
-        items.sort(key = lambda k: (k["section"], k["id"]))
         self.setItems(items)
 
 
