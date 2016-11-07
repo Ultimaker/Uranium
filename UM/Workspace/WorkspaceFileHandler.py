0cb05d6fc5e9cc28335d71d0589db950e5d9ead3
Jaime van Kessel <nallath@gmail.com>
2016-11-07 16:04:01 +0100

    Made WorkspaceFileHandler use Filehandler as well
    CURA-1263

--- UM/Workspace/WorkspaceFileHandler.py
+++ UM/Workspace/WorkspaceFileHandler.py
@@ -2,27 +2,11 @@
 # Uranium is released under the terms of the AGPLv3 or higher.
 
 from UM.Logger import Logger
-from UM.PluginRegistry import PluginRegistry
+from UM.FileHandler.FileHandler import FileHandler
 
 
 ##  Central class for reading and writing workspaces.
 #   This class is created by Application and handles reading and writing workspace files.
-class WorkspaceFileHandler:
+class WorkspaceFileHandler(FileHandler):
     def __init__(self):
-        super().__init__()
-
-        self._workspace_readers = {}
-        self._workspace_writers = {}
-
-        PluginRegistry.addType("workspace_readers", self.addReader)
-        PluginRegistry.addType("workspace_writers", self.addWriter)
-
-    ##  Register a workspace reader
-    #   \param reader \type{WorkspaceReader} The WorkspaceReader to register.
-    def addReader(self, reader):
-        self._workspace_readers[reader.getsPluginId()] = reader
-
-    ##  Register a workspace writer
-    #   \param reader \type{WorkspaceWriter} The WorkspaceWriter to register.
-    def addWriter(self, writer):
-        self._workspace_writers[writer.getPluginId()] = writer
+        super().__init__("workspace_writers", "workspace_readers")
