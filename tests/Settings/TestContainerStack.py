534170c4cdfe142699fe834d0002d0af4fa43e2b
Arjen Hiemstra <ahiemstra@heimr.nl>
2016-12-07 16:29:02 +0100

    Update all serialised stack in tests to the new serialization format

--- tests/Settings/TestContainerStack.py
+++ tests/Settings/TestContainerStack.py
@@ -161,11 +161,16 @@ def test_deserialize_syntax_error(container_stack):
 def test_deserialize_wrong_version(container_stack, container_registry):
     container_registry.addContainer(UM.Settings.InstanceContainer("a")) # Make sure this container isn't the one it complains about.
 
-    serialised = """[general]
-name = Test
-id = testid
-containers = a
-version = -1""" # -1 should always be wrong.
+    serialised = """
+    [general]
+    name = Test
+    id = testid
+    version = -1
+
+    [containers]
+    0 = a
+    """ # -1 should always be wrong.
+
     with pytest.raises(IncorrectVersionError):
         container_stack.deserialize(serialised)
 
@@ -178,35 +183,57 @@ version = -1""" # -1 should always be wrong.
 def test_deserialize_missing_items(container_stack, container_registry):
     container_registry.addContainer(UM.Settings.InstanceContainer("a")) # Make sure this container isn't the one it complains about.
 
-    serialised_no_name = """[general]
-id = testid
-containers = a
-version = """ + str(UM.Settings.ContainerStack.Version)
+    serialised_no_name = """
+    [general]
+    id = testid
+    version = {version}
+
+    [containers]
+    0 = a
+    """.format(version = UM.Settings.ContainerStack.Version)
+
     with pytest.raises(InvalidContainerStackError):
         container_stack.deserialize(serialised_no_name)
 
-    serialised_no_id = """[general]
-name = Test
-containers = a
-version = """ + str(UM.Settings.ContainerStack.Version)
+    serialised_no_id = """
+    [general]
+    name = Test
+    version = {version}
+
+    [containers]
+    0 = a
+    """.format(version = UM.Settings.ContainerStack.Version)
+
     with pytest.raises(InvalidContainerStackError):
         container_stack.deserialize(serialised_no_id)
 
-    serialised_no_version = """[general]
-name = Test
-id = testid
-containers = a"""
+    serialised_no_version = """
+    [general]
+    name = Test
+    id = testid
+
+    [containers]
+    0 = a
+    """
+
     with pytest.raises(InvalidContainerStackError):
         container_stack.deserialize(serialised_no_version)
 
-    serialised_no_containers = """[general]
-name = Test
-id = testid
-version = """ + str(UM.Settings.ContainerStack.Version)
-    container_stack.deserialize(serialised_no_containers) # Missing containers is allowed.
+    serialised_no_containers = """
+    [general]
+    name = Test
+    id = testid
+    version = {version}
+    """.format(version = UM.Settings.ContainerStack.Version)
 
-    serialised_no_general = """[metadata]
-foo = bar"""
+    container_stack.deserialize(serialised_no_containers) # Missing containers is allowed.
+    assert container_stack.getContainers() == [] # Deserialize of an empty stack should result in an empty stack
+
+    serialised_no_general = """
+    [metadata]
+    foo = bar
+    """
+
     with pytest.raises(InvalidContainerStackError):
         container_stack.deserialize(serialised_no_general)
 
@@ -220,38 +247,59 @@ def test_deserialize_containers(container_stack, container_registry):
     container = UM.Settings.InstanceContainer("a")
     container_registry.addContainer(container)
 
-    serialised = """[general]
-name = Test
-id = testid
-containers = a
-version = """ + str(UM.Settings.ContainerStack.Version) # Test case where there is a container.
+    serialised = """
+    [general]
+    name = Test
+    id = testid
+    version = {version}
+
+    [containers]
+    0 = a
+    """.format(version = UM.Settings.ContainerStack.Version) # Test case where there is a container.
+
     container_stack.deserialize(serialised)
     assert container_stack.getContainers() == [container]
 
     container_stack = UM.Settings.ContainerStack(uuid.uuid4().int)
-    serialised = """[general]
-name = Test
-id = testid
-containers =
-version = """ + str(UM.Settings.ContainerStack.Version) # Test case where there is no container.
+    serialised = """
+    [general]
+    name = Test
+    id = testid
+    version = {version}
+
+    [containers]
+    """.format(version = UM.Settings.ContainerStack.Version) # Test case where there is no container.
+
     container_stack.deserialize(serialised)
     assert container_stack.getContainers() == []
 
     container_stack = UM.Settings.ContainerStack(uuid.uuid4().int)
-    serialised = """[general]
-name = Test
-id = testid
-containers = a,a
-version = """ + str(UM.Settings.ContainerStack.Version) # Test case where there are two of the same containers.
+    serialised = """
+    [general]
+    name = Test
+    id = testid
+    version = {version}
+
+    [containers]
+    0 = a
+    1 = a
+    """.format(version = UM.Settings.ContainerStack.Version) # Test case where there are two of the same containers.
+
     container_stack.deserialize(serialised)
     assert container_stack.getContainers() == [container, container]
 
     container_stack = UM.Settings.ContainerStack(uuid.uuid4().int)
-    serialised = """[general]
-name = Test
-id = testid
-containers = a,b
-version = """ + str(UM.Settings.ContainerStack.Version) # Test case where a container doesn't exist.
+    serialised = """
+    [general]
+    name = Test
+    id = testid
+    version = {version}
+
+    [containers]
+    0 = a
+    1 = b
+    """.format(version = UM.Settings.ContainerStack.Version) # Test case where a container doesn't exist.
+
     with pytest.raises(Exception):
         container_stack.deserialize(serialised)
 
