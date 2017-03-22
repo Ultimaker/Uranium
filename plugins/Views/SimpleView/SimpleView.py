745ad21ebd98a59c8288183655b67d5de5287c62
Jaime van Kessel <nallath@gmail.com>
2017-03-22 17:03:34 +0100

    Removed unused imports

--- plugins/Views/SimpleView/SimpleView.py
+++ plugins/Views/SimpleView/SimpleView.py
@@ -2,16 +2,12 @@
 # Uranium is released under the terms of the AGPLv3 or higher.
 
 from UM.Resources import Resources
-from UM.Application import Application
-from UM.Math.Color import Color
-from UM.Preferences import Preferences
 
 from UM.Scene.Iterator.DepthFirstIterator import DepthFirstIterator
 
 from UM.View.View import View
 from UM.View.GL.OpenGL import OpenGL
 
-import math
 
 ## Standard view for mesh models.
 class SimpleView(View):
