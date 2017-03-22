552dd04abb605d747c05a8b3e14aa68a3651395f
Jaime van Kessel <nallath@gmail.com>
2017-03-22 16:44:20 +0100

    Removed unused imports

--- tests/TestTools.py
+++ tests/TestTools.py
@@ -1,9 +1,7 @@
 # Copyright (c) 2017 Ultimaker B.V.
 # Uranium is released under the terms of the AGPLv3 or higher.
 
-import pytest
 from unittest.mock import MagicMock
-from unittest.mock import patch
 from UM.Application import Application
 
 from UM.Controller import Controller
