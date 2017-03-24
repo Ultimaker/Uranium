36cee123af653d3a701625396d6cf878e4fcbf25
Jaime van Kessel <nallath@gmail.com>
2017-03-24 14:50:47 +0100

    Removed debug print statement

--- UM/Scene/Scene.py
+++ UM/Scene/Scene.py
@@ -93,7 +93,6 @@ class Scene():
     #   \return The object if found, or None if not.
     def findObject(self, object_id):
         for node in BreadthFirstIterator(self._root):
-            print(id(node), object_id)
             if id(node) == object_id:
                 return node
         return None
