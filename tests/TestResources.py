a8f2f927bac6389c4442e64d56f1c8ae441eb979
Arjen Hiemstra <ahiemstra@heimr.nl>
2017-04-26 15:32:01 +0200

    Fix unit tests

--- tests/TestResources.py
+++ tests/TestResources.py
@@ -1,14 +1,20 @@
 # Copyright (c) 2017 Ultimaker B.V.
 # Uranium is released under the terms of the AGPLv3 or higher.
 
+import sys
 import os
 import platform
 from unittest import TestCase
+
+from PyQt5.QtCore import QCoreApplication
 
 from UM.Resources import Resources
 
 
 class TestResources(TestCase):
+    def setUp(self):
+        self.application = QCoreApplication(sys.argv)
+        self.application.setApplicationName("test")
 
     #
     # getConfigStorageRootPath() tests
@@ -18,37 +24,24 @@ class TestResources(TestCase):
             self.skipTest("not on Windows")
 
         config_root_path = Resources._getConfigStorageRootPath()
-        expected_config_root_path = os.getenv("APPDATA")
-        self.assertEqual(expected_config_root_path, config_root_path,
-                         "expected %s, got %s" % (expected_config_root_path, config_root_path))
+        expected_config_root_path = os.path.join(os.getenv("LOCALAPPDATA"), "test")
+        self.assertEqual(expected_config_root_path, config_root_path)
 
     def test_getConfigStorageRootPath_Linux(self):
         if platform.system() != "Linux":
             self.skipTest("not on Linux")
 
-        # no XDG_CONFIG_HOME defined
-        if "XDG_CONFIG_HOME" in os.environ:
-            del os.environ["XDG_CONFIG_HOME"]
         config_root_path = Resources._getConfigStorageRootPath()
-        expected_config_root_path = os.path.expanduser("~/.config")
-        self.assertEqual(expected_config_root_path, config_root_path,
-                         "expected %s, got %s" % (expected_config_root_path, config_root_path))
-
-        # XDG_CONFIG_HOME defined
-        os.environ["XDG_CONFIG_HOME"] = "/tmp"
-        config_root_path = Resources._getConfigStorageRootPath()
-        expected_config_root_path = "/tmp"
-        self.assertEqual(expected_config_root_path, config_root_path,
-                         "expected %s, got %s" % (expected_config_root_path, config_root_path))
+        expected_config_root_path = os.path.expanduser("~/.config/test")
+        self.assertEqual(expected_config_root_path, config_root_path)
 
     def test_getConfigStorageRootPath_Mac(self):
         if platform.system() != "Darwin":
             self.skipTest("not on mac")
 
         config_root_path = Resources._getConfigStorageRootPath()
-        expected_config_root_path = os.path.expanduser("~/Library/Application Support")
-        self.assertEqual(expected_config_root_path, config_root_path,
-                         "expected %s, got %s" % (expected_config_root_path, config_root_path))
+        expected_config_root_path = os.path.expanduser("~/Library/Preferences/test")
+        self.assertEqual(expected_config_root_path, config_root_path)
 
     #
     # getDataStorageRootPath() tests
@@ -58,33 +51,24 @@ class TestResources(TestCase):
             self.skipTest("not on Windows")
 
         data_root_path = Resources._getDataStorageRootPath()
-        self.assertIsNone(data_root_path, "expected None, got %s" % data_root_path)
+        expected_data_root_path = os.path.join(os.getenv("APPDATA"), "test")
+        self.assertEqual(expected_data_root_path, data_root_path)
 
     def test_getDataStorageRootPath_Linux(self):
         if platform.system() != "Linux":
             self.skipTest("not on Linux")
 
-        # no XDG_CONFIG_HOME defined
-        if "XDG_DATA_HOME" in os.environ:
-            del os.environ["XDG_DATA_HOME"]
-        data_root_path = Resources._getConfigStorageRootPath()
-        expected_data_root_path = os.path.expanduser("~/.local/share")
-        self.assertEqual(expected_data_root_path, data_root_path,
-                         "expected %s, got %s" % (expected_data_root_path, data_root_path))
-
-        # XDG_CONFIG_HOME defined
-        os.environ["XDG_DATA_HOME"] = "/tmp"
         data_root_path = Resources._getDataStorageRootPath()
-        expected_data_root_path = "/tmp"
-        self.assertEqual(expected_data_root_path, data_root_path,
-                         "expected %s, got %s" % (expected_data_root_path, data_root_path))
+        expected_data_root_path = os.path.expanduser("~/.local/share/test")
+        self.assertEqual(expected_data_root_path, data_root_path)
 
     def test_getDataStorageRootPath_Mac(self):
         if platform.system() != "Darwin":
             self.skipTest("not on mac")
 
         data_root_path = Resources._getDataStorageRootPath()
-        self.assertIsNone(data_root_path, "expected None, got %s" % data_root_path)
+        expected_data_root_path = os.expanduser("~/Library/Application Support/test")
+        self.assertEqual(expected_data_root_path, data_root_path)
 
     #
     # getCacheStorageRootPath() tests
@@ -94,22 +78,21 @@ class TestResources(TestCase):
             self.skipTest("not on Windows")
 
         cache_root_path = Resources._getCacheStorageRootPath()
-        expected_cache_root_path = os.getenv("LOCALAPPDATA")
-        self.assertEqual(expected_cache_root_path, cache_root_path,
-                         "expected %s, got %s" % (expected_cache_root_path, cache_root_path))
+        expected_cache_root_path = os.path.join(os.getenv("LOCALAPPDATA"), "test", "cache")
+        self.assertEqual(expected_cache_root_path, cache_root_path)
 
     def test_getCacheStorageRootPath_Linux(self):
         if platform.system() != "Linux":
             self.skipTest("not on Linux")
 
         cache_root_path = Resources._getCacheStorageRootPath()
-        expected_cache_root_path = os.path.expanduser("~/.cache")
-        self.assertEqual(expected_cache_root_path, cache_root_path,
-                         "expected %s, got %s" % (expected_cache_root_path, cache_root_path))
+        expected_cache_root_path = os.path.expanduser("~/.cache/test")
+        self.assertEqual(expected_cache_root_path, cache_root_path)
 
     def test_getCacheStorageRootPath_Mac(self):
         if platform.system() != "Darwin":
             self.skipTest("not on mac")
 
         cache_root_path = Resources._getCacheStorageRootPath()
+        expected_cache_root_path = os.path.expanduser("~/Library/Caches/test")
         self.assertIsNone("expected None, got %s" % cache_root_path)
