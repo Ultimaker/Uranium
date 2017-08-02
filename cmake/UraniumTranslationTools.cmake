dba5d1e346dfae51018b811560ddee539b6bb7b2
Ghostkeeper <rubend@tutanota.com>
2017-08-02 13:45:26 +0200

    Turn CURA_BINARY_DATA_DIRECTORY into a directory-type variable
    It's not an option. Options are boolean checkboxes. This should be a directory. It's used like a directory everywhere throughout the code.
    
    Contributes to issue CURA-4113.

--- cmake/UraniumTranslationTools.cmake
+++ cmake/UraniumTranslationTools.cmake
@@ -18,7 +18,7 @@ ENDMACRO()
 
 ## Translation tools:
 
-option(CURA_BINARY_DATA_DIRECTORY "Directory to the cura-binary-data repository")
+SET(CURA_BINARY_DATA_DIRECTORY CACHE PATH "Directory to the cura-binary-data repository")
 
 if(NOT CURA_BINARY_DATA_DIRECTORY AND NOT DEFINED $ENV{CURA_BINARY_DATA_DIRECTORY})
     message(STATUS "Using CURA_BINARY_DATA_DIRECTORY from set of environment variables...")
