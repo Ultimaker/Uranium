aed971f63c39ce52e9c20f315d45f553a443322d
Jaime van Kessel <nallath@gmail.com>
2017-09-28 15:42:39 +0200

    Fixed query cache not being cleard in certain cases where it should have been

--- UM/Settings/ContainerRegistry.py
+++ UM/Settings/ContainerRegistry.py
@@ -233,7 +233,7 @@ class ContainerRegistry(ContainerRegistryInterface):
 
         self._containers.append(container)
         self._id_container_cache[container.getId()] = container
-        self._clearQueryCacheByContainerType(type(container))
+        self._clearQueryCacheByContainer(container)
         self.containerAdded.emit(container)
 
     @UM.FlameProfiler.profile
@@ -249,7 +249,7 @@ class ContainerRegistry(ContainerRegistryInterface):
 
             if hasattr(container, "metaDataChanged"):
                 container.metaDataChanged.disconnect(self._onContainerMetaDataChanged)
-            self._clearQueryCacheByContainerType(type(container))
+            self._clearQueryCacheByContainer(container)
             self.containerRemoved.emit(container)
 
             Logger.log("d", "Removed container %s", container.getId())
@@ -280,7 +280,7 @@ class ContainerRegistry(ContainerRegistryInterface):
             del self._id_container_cache[container._id]
             container._id = new_id
             self._id_container_cache[container._id] = container # Keep cache up-to-date.
-        self._clearQueryCacheByContainerType(type(container))
+        self._clearQueryCacheByContainer(container)
         self.containerAdded.emit(container)
 
     def saveAll(self) -> None:
@@ -501,7 +501,19 @@ class ContainerRegistry(ContainerRegistryInterface):
     ##  Clear the query cache by using container type.
     #   This is a slightly smarter way of clearing the cache. Only queries that are of the same type (or without one)
     #   are cleared.
-    def _clearQueryCacheByContainerType(self, container_type):
+    def _clearQueryCacheByContainer(self, container):
+        # Use the base classes to clear the
+        if isinstance(container, DefinitionContainer):
+            container_type = DefinitionContainer
+        elif isinstance(container, InstanceContainer):
+            container_type = InstanceContainer
+        elif isinstance(container, ContainerStack):
+            container_type = ContainerStack
+        else:
+            Logger.log("w", "While clearing query cache, we got an unrecognised base type (%s). Clearing entire cache instead", type(container))
+            self._clearQueryCache()
+            return
+
         for key in list(self._query_cache.keys()):
             if self._query_cache[key].getContainerType() == container_type or self._query_cache[key].getContainerType() is None:
                 del self._query_cache[key]
