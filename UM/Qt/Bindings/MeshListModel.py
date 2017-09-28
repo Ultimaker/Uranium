208f87750af4bc877e2cb12c51225b1e192dba05
Jaime van Kessel <nallath@gmail.com>
2017-09-28 21:09:43 +0200

    Fixed moving to dummy not working correctly.
    USL-34

--- UM/Qt/Bindings/MeshListModel.py
+++ UM/Qt/Bindings/MeshListModel.py
@@ -55,7 +55,6 @@ class MeshListModel(ListModel):
                 if old_index == new_index: # No change.
                     return
 
-                old_model_entry = self.getItem(old_index) # Model data of moving node
                 new_model_entry = self.getItem(new_index) # Model data of location node should move to.
 
                 for parent_node in Application.getInstance().getController().getScene().getRoot().getAllChildren():
@@ -93,18 +92,16 @@ class MeshListModel(ListModel):
 
                         else:
                             # Switch happend to dummy?
-                            pass
+                            if old_index < new_index:
+                                # Move is such that parent needs to be reset.
+                                node.setParent(node.getParent().getParent())
                         self.updateList(node)
                         break
-                      #  break
-
-                #self.removeItem(old_index)
-                #self.insertItem(new_index,data)
 
     def updateList(self, trigger_node):
         self.clear()
         for root_child in self._scene.getRoot().getChildren():
-            if root_child.callDecoration("isGroup"): # Check if its a group node
+            if root_child.callDecoration("isGroup"):  # Check if its a group node
                 parent_key = id(root_child)
                 for node in DepthFirstIterator(root_child):
                     if root_child in self._collapsed_nodes:
@@ -116,18 +113,18 @@ class MeshListModel(ListModel):
                             "selected": Selection.isSelected(node),
                             "collapsed": node in self._collapsed_nodes,
                             "parent_key": parent_key,
-                            "is_group":bool(node.callDecoration("isGroup")),
-                            "is_dummy" : False
+                            "is_group": bool(node.callDecoration("isGroup")),
+                            "is_dummy": False
                             }
                     self.appendItem(data)
-                data = { "name":"Dummy",
+                data = { "name": "Dummy " + root_child.getName(),
                          "visibility": True,
                          "key": 0,
                          "selected": Selection.isSelected(node),
                          "collapsed": root_child in self._collapsed_nodes,
                          "parent_key": parent_key,
-                         "is_group":False,
-                         "is_dummy" : True
+                         "is_group": False,
+                         "is_dummy": True
                         }
                 self.appendItem(data)
 
