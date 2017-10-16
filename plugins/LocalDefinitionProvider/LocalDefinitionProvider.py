6caf11a9e51f981c32e0d3d21b40ec268ed7c9b1
Ghostkeeper <rubend@tutanota.com>
2017-10-16 11:30:02 +0200

    Implement loadMetadata
    This loads the metadata from the local files. Yet untested though, as the metadata loading itself isn't complete.
    
    Contributes to issue CURA-4243.

--- plugins/LocalDefinitionProvider/LocalDefinitionProvider.py
+++ plugins/LocalDefinitionProvider/LocalDefinitionProvider.py
@@ -1,13 +1,28 @@
 # Copyright (c) 2017 Ultimaker B.V.
 # Uranium is released under the terms of the LGPLv3 or higher.
 
+import os #To get the ID from a filename.
 from typing import Any, Dict
 
-from UM.Settings.ContainerProvider import ContainerProvider
+from UM.Settings.ContainerProvider import ContainerProvider #The class we're implementing.
+from UM.Settings.DefinitionContainer import DefinitionContainer #To parse JSON files and get their metadata.
+from UM.Resources import Resources
 
 class LocalDefinitionProvider(ContainerProvider):
-    def loadContainer(self, container_id) -> "ContainerInterface":
+    def loadContainer(self, container_id: str) -> "ContainerInterface":
         pass
 
-    def loadMetadata(self) -> Dict[str, Dict[str, Any]]:
-        pass
+    def loadMetadata(self, container_id: str) -> Dict[str, Dict[str, Any]]:
+        result = {}
+
+        for filename in Resources.getAllResourcesOfType(Resources.DefinitionContainers):
+            with open(filename) as f:
+                metadata = DefinitionContainer.getMetadataFromSerialized(f.read())
+            if metadata is None:
+                continue
+
+            #Always fill in the ID from the filename, rather than the ID in the metadata itself.
+            metadata["id"] = os.path.basename(filename).split(".")[0] #Instead of using the default splitext function, we deal with multiple extensions differently: We always split on the first point.
+            result[metadata["id"]] = metadata
+
+        return result
