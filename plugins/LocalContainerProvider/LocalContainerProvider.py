061483dbc5db797a2d924c3bca59e12797729d77
Ghostkeeper <rubend@tutanota.com>
2017-11-21 11:00:56 +0100

    Handle deserializeMetadata returning multiple files
    The rest of the files get side-loaded into the registry, which is a bit bad.
    
    Contributes to issue CURA-4243.

--- plugins/LocalContainerProvider/LocalContainerProvider.py
+++ plugins/LocalContainerProvider/LocalContainerProvider.py
@@ -66,7 +66,17 @@ class LocalContainerProvider(ContainerProvider):
         filename = self._id_to_path[container_id] #Raises KeyError if container ID does not exist in the (cache of the) files!
 
         with open(filename) as f:
-            return ContainerRegistry.mime_type_map[self._id_to_mime[container_id].name].deserializeMetadata(f.read(), container_id) #pylint: disable=no-member
+            clazz = ContainerRegistry.mime_type_map[self._id_to_mime[container_id].name]
+            result = clazz.deserializeMetadata(f.read(), container_id) #pylint: disable=no-member
+            for metadata in result:
+                if "id" not in metadata:
+                    Logger.log("w", "Metadata obtained from deserializeMetadata of {class_name} didn't contain an ID.".format(class_name = clazz.__name__))
+                    continue
+                #Side-load the metadata into the registry if we get multiple containers.
+                if metadata["id"] not in self._id_to_path: #This wouldn't get loaded normally.
+                    self._id_to_path[metadata["id"]] = filename
+                    ContainerRegistry.getInstance().metadata[metadata["id"]] = metadata
+            return result
 
     ##  Load a pre-parsed definition container.
     #
