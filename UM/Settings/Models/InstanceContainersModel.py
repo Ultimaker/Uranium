4e98a217c255bcb0cca28802a11ef048020156df
Ghostkeeper <rubend@tutanota.com>
2017-11-30 13:50:44 +0100

    Fix type of weight
    Since the metadata returns strings.
    
    Contributes to issue CURA-4243.

--- UM/Settings/Models/InstanceContainersModel.py
+++ UM/Settings/Models/InstanceContainersModel.py
@@ -86,7 +86,7 @@ class InstanceContainersModel(ListModel):
                 "metadata": container.getMetaData().copy(),
                 "readOnly": registry.isReadOnly(container.getId()),
                 "section": container.getMetaDataEntry(self._section_property, ""),
-                "weight": container.getMetaDataEntry("weight", 0)
+                "weight": int(container.getMetaDataEntry("weight", 0))
             })
         for container_metadata in self._instance_containers_metadata.values():
             result.append({
@@ -95,7 +95,7 @@ class InstanceContainersModel(ListModel):
                 "metadata": container_metadata.copy(),
                 "readOnly": registry.isReadOnly(container_metadata["id"]),
                 "section": container_metadata.get(self._section_property, ""),
-                "weight": container_metadata.get("weight", 0)
+                "weight": int(container_metadata.get("weight", 0))
             })
         yield from sorted(result, key = self._sortKey)
 
