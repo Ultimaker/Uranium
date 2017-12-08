301cba1d93e79da4edc08dbac30350cf199d0ed7
Thomas Karl Pietrowski <thopiekar@googlemail.com>
2017-12-08 19:46:57 +0100

    Multi-line function call
    Since some lines are written in multiple lines already, let's be more strict here.
    Also it is better readable.

--- UM/Application.py
+++ UM/Application.py
@@ -345,10 +345,13 @@ class Application:
     #   \param parser \type{argparse.ArgumentParser} The parser that will parse the command line.
     @classmethod
     def addCommandLineOptions(cls, parser):
-        parser.add_argument("--version", action="version", version="%(prog)s {0}".format(cls.getStaticVersion()))
+        parser.add_argument("--version",
+                            action="version",
+                            version="%(prog)s {0}".format(cls.getStaticVersion()))
         parser.add_argument("--external-backend",
                             dest="external-backend",
-                            action="store_true", default=False,
+                            action="store_true",
+                            default=False,
                             help="Use an externally started backend instead of starting it automatically. This is a debug feature to make it possible to run the engine with debug options enabled.")
 
     def addExtension(self, extension: "Extension"):
