a9f4ba30feedc6a299c17abe5301bf528f35c2ea
Aleksei S <a.sasin@ultimaker.com>
2018-01-03 16:47:15 +0100

    Adjusted camera 3D view CURA-4642

--- UM/Controller.py
+++ UM/Controller.py
@@ -375,16 +375,14 @@ class Controller:
         camera = self._scene.getActiveCamera()
         self._camera_tool.setOrigin(Vector(0, 100, 0))
         if coordinate == "home":
-            camera.setPosition(Vector(0, 300, 700))
+            camera.setPosition(Vector(0, 0, 700))
             camera.setPerspective(True)
             camera.lookAt(Vector(0, 100, 100))
             self._camera_tool.rotateCam(0, 0)
         elif coordinate == "3d":
-            camera.setPosition(Vector(-180, 250, 1000))
-
+            camera.setPosition(Vector(-750, 600, 700))
             camera.setPerspective(True)
             camera.lookAt(Vector(0, 100, 100))
-
             self._camera_tool.rotateCam(0, 0)
 
         else:
