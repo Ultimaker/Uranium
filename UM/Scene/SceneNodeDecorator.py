f8d8b0d8edeb669932e87beb7b8821fcda8cdc2e
Ghostkeeper <rubend@tutanota.com>
2018-01-23 09:19:01 +0100

    Fix circular imports
    Can't use SceneNode yet :(

--- UM/Scene/SceneNodeDecorator.py
+++ UM/Scene/SceneNodeDecorator.py
@@ -1,23 +1,19 @@
 # Copyright (c) 2018 Ultimaker B.V.
 # Uranium is released under the terms of the LGPLv3 or higher.
 
-MYPY = False
-if MYPY:
-    from UM.Scene.SceneNode import SceneNode
-
 ##      The point of a SceneNodeDecorator is that it can be added to a SceneNode, where it then provides decorations
 #       Decorations are functions of a SceneNodeDecorator that can be called (except for functions already defined
 #       in SceneNodeDecorator).
 #       \sa SceneNode
 class SceneNodeDecorator:
-    def __init__(self, node: SceneNode = None):
+    def __init__(self, node: "SceneNode" = None):
         super().__init__()
         self._node = node
         
-    def setNode(self, node: SceneNode) -> None:
+    def setNode(self, node: "SceneNode") -> None:
         self._node = node
 
-    def getNode(self) -> SceneNode:
+    def getNode(self) -> "SceneNode":
         return self._node
 
     ##  Clear all data associated with this decorator. This will be called before the decorator is removed
