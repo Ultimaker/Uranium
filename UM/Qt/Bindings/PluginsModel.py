07fbb1f78aa151ef1943fe48ce7053c37262910f
Ian Paschal <i.paschal@ultimaker.com>
2018-02-05 16:39:57 +0100

    CURA-4578 Manually installed plugins handled correctly
    3rd-party plugins which were manually installed were listed as uninstall-able instead of disable-able. The distinction between plugins which could be installed or disabled was previously made by whether or not it was 3rd party, but is now made by determining the install location.

--- UM/Qt/Bindings/PluginsModel.py
+++ UM/Qt/Bindings/PluginsModel.py
@@ -1,12 +1,15 @@
 # Copyright (c) 2015 Ultimaker B.V.
 # Uranium is released under the terms of the LGPLv3 or higher.
 
+import os
+
 from PyQt5.QtCore import Qt, QCoreApplication, pyqtSlot
 
 from UM.Application import Application
 from UM.Qt.ListModel import ListModel
 from UM.Logger import Logger
 from UM.PluginRegistry import PluginRegistry
+from UM.Resources import Resources
 
 class PluginsModel(ListModel):
     def __init__(self, view, parent = None):
@@ -31,8 +34,9 @@ class PluginsModel(ListModel):
         self.addRoleName(Qt.UserRole + 9, "status")
         self.addRoleName(Qt.UserRole + 10, "enabled")
         self.addRoleName(Qt.UserRole + 11, "required")
-        self.addRoleName(Qt.UserRole + 12, "can_upgrade")
-        self.addRoleName(Qt.UserRole + 13, "update_url")
+        self.addRoleName(Qt.UserRole + 12, "can_uninstall")
+        self.addRoleName(Qt.UserRole + 13, "can_upgrade")
+        self.addRoleName(Qt.UserRole + 14, "update_url")
 
 
         if view == "installed":
@@ -48,6 +52,7 @@ class PluginsModel(ListModel):
         # Get all active plugins from registry (list of strings):
         active_plugins = self._registry.getActivePlugins()
         installed_plugins = self._registry.getInstalledPlugins()
+        plugin_folder = os.path.join(Resources.getStoragePath(Resources.Resources), "plugins")
 
         # Metadata is used as the official list of "all plugins":
         for plugin_id in self._plugins:
@@ -59,9 +64,6 @@ class PluginsModel(ListModel):
                 continue
 
             props = metadata["plugin"]
-
-
-
 
             items.append({
                 # Static props from above are taken from the plugin's metadata:
@@ -78,6 +80,7 @@ class PluginsModel(ListModel):
                 "status": "installed" if metadata["id"] in installed_plugins else "available",
                 "enabled": True if view == "available" else metadata["id"] in active_plugins,
                 "required": metadata["id"] in self._required_plugins,
+                "can_uninstall": True if self._registry._locatePlugin(plugin_id, plugin_folder) else False,
                 "can_upgrade": False, # Default, potentially overwritten by plugin browser
                 "update_url": None # Default, potentially overwritten by plugin browser
             })
