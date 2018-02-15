9b11e6ea54e321b25bb0727ac72c6f7d01766ff8
Jaime van Kessel <nallath@gmail.com>
2018-02-15 21:23:02 +0100

    Added typing to iterators

--- UM/Scene/Iterator/BreadthFirstIterator.py
+++ UM/Scene/Iterator/BreadthFirstIterator.py
@@ -1,16 +1,15 @@
-# Copyright (c) 2015 Ultimaker B.V.
+# Copyright (c) 2018 Ultimaker B.V.
 # Uranium is released under the terms of the LGPLv3 or higher.
 
 from . import Iterator
+from UM.Scene.SceneNode import SceneNode
 
 
 class BreadthFirstIterator(Iterator.Iterator):
-    def __init__(self, scene_node):
-        super(BreadthFirstIterator, self).__init__(scene_node)  # Call super to make multiple inheritance work.
-    
-    def _fillStack(self):
-        if self._scene_node is not None:
-            self._node_stack.append(self._scene_node)
-            for node in self._node_stack:  # Loop through list, add children to back of list
-                self._node_stack.extend(node.getChildren())
+    def __init__(self, scene_node: SceneNode):
+        super().__init__(scene_node)
 
+    def _fillStack(self) -> None:
+        self._node_stack.append(self._scene_node)
+        for node in self._node_stack:  # Loop through list, add children to back of list
+            self._node_stack.extend(node.getChildren())
