437473233bd68fa1c3d76ccc7fd241f86917e910
Ghostkeeper <rubend@tutanota.com>
2018-02-27 11:03:07 +0100

    Fix copyright header
    While it's true that Thomas has copyright over part of this file, let's be consistent in who we name and who we don't name.
    
    Contributes to issue CURA-4912.

--- UM/Mesh/MeshBuilder.py
+++ UM/Mesh/MeshBuilder.py
@@ -1,5 +1,4 @@
-# Copyright (c) 2015 Ultimaker B.V.
-# Copyright (c) 2017 Thomas Karl Pietrowski
+# Copyright (c) 2018 Ultimaker B.V.
 # Uranium is released under the terms of the LGPLv3 or higher.
 
 from UM.Message import Message
