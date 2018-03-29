1812e885c3e244fae90f05b590aad491e8c91ba0
Jaime van Kessel <nallath@gmail.com>
2018-03-29 18:06:11 +0200

    Added typing to Scene

--- UM/Scene/Scene.py
+++ UM/Scene/Scene.py
@@ -1,20 +1,28 @@
 # Copyright (c) 2018 Ultimaker B.V.
 # Uranium is released under the terms of the LGPLv3 or higher.
 
-import functools #For partial to update files that were changed.
-import os.path #To watch files for changes.
-from PyQt5.QtCore import QFileSystemWatcher #To watch files for changes.
+import functools  # For partial to update files that were changed.
+import os.path  # To watch files for changes.
 import threading
+from typing import Optional, List
 
-from UM.i18n import i18nCatalog
+from PyQt5.QtCore import QFileSystemWatcher  # To watch files for changes.
+
+from UM.Decorators import deprecated
 from UM.Logger import Logger
-from UM.Mesh.ReadMeshJob import ReadMeshJob #To reload a mesh when its file was changed.
-from UM.Message import Message #To display a message for reloading files that were changed.
+from UM.Mesh.ReadMeshJob import ReadMeshJob  # To reload a mesh when its file was changed.
+from UM.Message import Message  # To display a message for reloading files that were changed.
 from UM.Scene.Camera import Camera
-from UM.Signal import Signal, signalemitter
 from UM.Scene.Iterator.BreadthFirstIterator import BreadthFirstIterator
+from UM.Signal import Signal, signalemitter
+from UM.i18n import i18nCatalog
 
 i18n_catalog = i18nCatalog("uranium")
+
+MYPY = False
+if MYPY:
+    from UM.Scene.SceneNode import SceneNode
+
 
 ##  Container object for the scene graph
 #
@@ -28,25 +36,25 @@ class Scene:
         self._root = SceneNode(name= "Root")
         self._root.setCalculateBoundingBox(False)
         self._connectSignalsRoot()
-        self._active_camera = None
+        self._active_camera = None  # type: Optional[Camera]
         self._ignore_scene_changes = False
         self._lock = threading.Lock()
 
-        #Watching file for changes.
+        # Watching file for changes.
         self._file_watcher = QFileSystemWatcher()
         self._file_watcher.fileChanged.connect(self._onFileChanged)
 
-    def _connectSignalsRoot(self):
+    def _connectSignalsRoot(self) -> None:
         self._root.transformationChanged.connect(self.sceneChanged)
         self._root.childrenChanged.connect(self.sceneChanged)
         self._root.meshDataChanged.connect(self.sceneChanged)
 
-    def _disconnectSignalsRoot(self):
+    def _disconnectSignalsRoot(self) -> None:
         self._root.transformationChanged.disconnect(self.sceneChanged)
         self._root.childrenChanged.disconnect(self.sceneChanged)
         self._root.meshDataChanged.disconnect(self.sceneChanged)
 
-    def setIgnoreSceneChanges(self, ignore_scene_changes):
+    def setIgnoreSceneChanges(self, ignore_scene_changes: bool) -> None:
         if self._ignore_scene_changes != ignore_scene_changes:
             self._ignore_scene_changes = ignore_scene_changes
             if self._ignore_scene_changes:
@@ -60,12 +68,14 @@ class Scene:
     #   assuming those threads also properly acquire the lock. Most notably, this
     #   prevents the rendering thread from rendering the scene while it is changing.
     #   Deprecated, use getSceneLock() instead.
-    def acquireLock(self):
+    @deprecated("Please use the getSceneLock instead", "3.3")
+    def acquireLock(self) -> None:
         self._lock.acquire()
 
     ##  Release the global scene lock.
     #   Deprecated, use getSceneLock() instead.
-    def releaseLock(self):
+    @deprecated("Please use the getSceneLock instead", "3.3")
+    def releaseLock(self) -> None:
         self._lock.release()
 
     ##  Gets the global scene lock.
@@ -77,11 +87,11 @@ class Scene:
         return self._lock
 
     ##  Get the root node of the scene.
-    def getRoot(self):
+    def getRoot(self) -> "SceneNode":
         return self._root
 
     ##  Change the root node of the scene
-    def setRoot(self, node):
+    def setRoot(self, node: "SceneNode") -> None:
         if self._root != node:
             if not self._ignore_scene_changes:
                 self._disconnectSignalsRoot()
@@ -93,10 +103,10 @@ class Scene:
     rootChanged = Signal()
 
     ##  Get the camera that should be used for rendering.
-    def getActiveCamera(self):
+    def getActiveCamera(self) -> Optional[Camera]:
         return self._active_camera
 
-    def getAllCameras(self):
+    def getAllCameras(self) -> List[Camera]:
         cameras = []
         for node in BreadthFirstIterator(self._root):
             if isinstance(node, Camera):
@@ -105,7 +115,7 @@ class Scene:
 
     ##  Set the camera that should be used for rendering.
     #   \param name The name of the camera to use.
-    def setActiveCamera(self, name):
+    def setActiveCamera(self, name: str) -> None:
         camera = self.findCamera(name)
         if camera:
             self._active_camera = camera
@@ -121,29 +131,29 @@ class Scene:
     #   \param object_id The id of the object to search for, as returned by the python id() method.
     #
     #   \return The object if found, or None if not.
-    def findObject(self, object_id):
+    def findObject(self, object_id: int) -> Optional["SceneNode"]:
         for node in BreadthFirstIterator(self._root):
             if id(node) == object_id:
                 return node
         return None
 
-    def findCamera(self, name):
+    def findCamera(self, name: str) -> Optional[Camera]:
         for node in BreadthFirstIterator(self._root):
             if isinstance(node, Camera) and node.getName() == name:
                 return node
 
     ##  Add a file to be watched for changes.
     #   \param file_path The path to the file that must be watched.
-    def addWatchedFile(self, file_path):
+    def addWatchedFile(self, file_path: str) -> None:
         self._file_watcher.addPath(file_path)
 
     ##  Remove a file so that it will no longer be watched for changes.
     #   \param file_path The path to the file that must no longer be watched.
-    def removeWatchedFile(self, file_path):
+    def removeWatchedFile(self, file_path: str) -> None:
         self._file_watcher.removePath(file_path)
 
     ##  Triggered whenever a file is changed that we currently have loaded.
-    def _onFileChanged(self, file_path):
+    def _onFileChanged(self, file_path: str) -> None:
         if not os.path.isfile(self.file_path): #File doesn't exist any more.
             return
 
@@ -162,7 +172,7 @@ class Scene:
     #   \param nodes The list of nodes that needs to be reloaded.
     #   \param message The message that triggered the action to reload them.
     #   \param action The button that triggered the action to reload them.
-    def _reloadNodes(self, nodes, message, action):
+    def _reloadNodes(self, nodes: List["SceneNode"], message, action) -> None:
         if action != "reload":
             return
         for node in nodes:
@@ -176,7 +186,7 @@ class Scene:
     ##  Triggered when reloading has finished.
     #
     #   This then puts the resulting mesh data in the node.
-    def _reloadJobFinished(self, job):
+    def _reloadJobFinished(self, job: ReadMeshJob) -> None:
         for node in job.getResult():
             mesh_data = node.getMeshData()
             if mesh_data:
