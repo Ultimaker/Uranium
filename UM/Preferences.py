75b6464127586b1d73102cfdcaf438d4b6adb19c
Diego Prado Gesto <d.pradogesto@ultimaker.com>
2018-04-23 11:31:45 +0200

    CURA-5188 Increase version number of the preference files, since one preference change its default value.

--- UM/Preferences.py
+++ UM/Preferences.py
@@ -21,7 +21,7 @@ MimeTypeDatabase.addMimeType(
 #       Typical preferences would be window size, standard machine, etc.
 @signalemitter
 class Preferences:
-    Version = 5
+    Version = 6
 
     def __init__(self):
         super().__init__()
