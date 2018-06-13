91e9e7011553644ffabd5c43a0c36af504cfb087
Ghostkeeper <rubend@tutanota.com>
2018-06-13 17:14:44 +0200

    Ignore places where MyPy is wrong
    Stupid MyPy.
    
    Contributes to issue CURA-5330.

--- UM/Qt/QtApplication.py
+++ UM/Qt/QtApplication.py
@@ -374,7 +374,7 @@ class QtApplication(QApplication, Application):
     @property
     def isVisible(self) -> bool:
         if self._main_window is not None:
-            return self._main_window.visible
+            return self._main_window.visible #type: ignore #MyPy doesn't realise that self._main_window cannot be None here.
         return False
 
     def getTheme(self) -> Optional[Theme]:
@@ -529,7 +529,7 @@ class QtApplication(QApplication, Application):
             return None
         path = QUrl.fromLocalFile(qml_file_path)
         component = QQmlComponent(self._qml_engine, path)
-        result_context = QQmlContext(self._qml_engine.rootContext())
+        result_context = QQmlContext(self._qml_engine.rootContext()) #type: ignore #MyPy doens't realise that self._qml_engine can't be None here.
         if context_properties is not None:
             for name, value in context_properties.items():
                 result_context.setContextProperty(name, value)
