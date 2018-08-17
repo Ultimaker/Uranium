5e1d0d1eb78bfe3ebbf7dd6a9f050d62f9e6836f
Lipu Fei <lipu.fei815@gmail.com>
2018-08-17 10:24:31 +0200

    Fix copyright header
    It's Uranium not Cura...

--- UM/Math/ShapelyUtil.py
+++ UM/Math/ShapelyUtil.py
@@ -1,5 +1,5 @@
 # Copyright (c) 2018 Ultimaker B.V.
-# Cura is released under the terms of the LGPLv3 or higher.
+# Uranium is released under the terms of the LGPLv3 or higher.
 
 from typing import TYPE_CHECKING
 
