658623365455d9ad8a87e58632b79d5e3a4c0ba9
Lipu Fei <lipu.fei815@gmail.com>
2018-08-17 10:23:19 +0200

    Fix polygon2ShapelyPolygon()
    CURA-5653

--- UM/Math/ShapelyUtil.py
+++ UM/Math/ShapelyUtil.py
@@ -10,10 +10,4 @@ if TYPE_CHECKING:
 # Converts a UM.Polygon into a shapely Polygon.
 #
 def polygon2ShapelyPolygon(polygon: "Polygon") -> shapely.geometry.Polygon:
-    xmin = min(p[0] for p in polygon.getPoints())
-    xmax = max(p[0] for p in polygon.getPoints())
-    ymin = min(p[1] for p in polygon.getPoints())
-    ymax = max(p[1] for p in polygon.getPoints())
-
-    return shapely.geometry.box(xmin, ymin, xmax, ymax)
-    #return shapely.geometry.Polygon([tuple(p) for p in polygon.getPoints()])
+    return shapely.geometry.Polygon([tuple(p) for p in polygon.getPoints()])
