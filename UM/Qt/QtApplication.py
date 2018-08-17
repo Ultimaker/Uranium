4e1b654fb37951f0ed028d2f890465e08d14854d
Lipu Fei <lipu.fei815@gmail.com>
2018-08-17 10:52:21 +0200

    Revert "fix for CURA-5645 -- forgot to exclude other types of files from 'include saved projects in recent files'"
    This reverts commit 04756ac8da3853f35319706643f45d987b7daaa8.

--- UM/Qt/QtApplication.py
+++ UM/Qt/QtApplication.py
@@ -263,9 +263,6 @@ class QtApplication(QApplication, Application):
         elif (not isinstance(job, ReadMeshJob) and not isinstance(job, ReadFileJob)) or not job.getResult():
             return
 
-        if isinstance(job, WriteFileJob) and not job.getFileName().endswith(".curaproject.3mf"):
-            return
-
         if isinstance(job, (ReadMeshJob, ReadFileJob, WriteFileJob)):
             self.addFileToRecentFiles(job.getFileName())
 
