36b87cb803bfd31a07c021a2aa28a6dae0729128
Jaime van Kessel <nallath@gmail.com>
2018-10-03 17:45:13 +0200

    Fixed another minor grammar issue with the updateCheckerJob

--- plugins/UpdateChecker/UpdateCheckerJob.py
+++ plugins/UpdateChecker/UpdateCheckerJob.py
@@ -76,7 +76,7 @@ class UpdateCheckerJob(Job):
                                 message = Message(text = content_message, title = title_message)
                                 message.addAction("download", i18n_catalog.i18nc("@action:button", "Download"), "[no_icon]", "[no_description]")
 
-                                message.addAction("new_features", i18n_catalog.i18nc("@action:button", "Learn more about new features"),"[no_icon]", "[no_description]",
+                                message.addAction("new_features", i18n_catalog.i18nc("@action:button", "Learn more about the new features"), "[no_icon]", "[no_description]",
                                                   button_style = Message.ActionButtonStyle.LINK,
                                                   button_align = Message.ActionButtonStyle.BUTTON_ALIGN_LEFT)
 
