80ea47dd9ed8ab5c2a5457a8ae0dd37104d11c0d
Aleksei S <a.sasin@ultimaker.com>
2018-10-31 12:10:51 +0100

    Disable plugin if the SDK version is not valid any more. Instead of removing the matadata. CURA-5763

--- UM/PluginRegistry.py
+++ UM/PluginRegistry.py
@@ -399,8 +399,8 @@ class PluginRegistry(QObject):
 
         #If API version is incompatible, don't load it.
         if self._metadata[plugin_id].get("plugin", {}).get("api", 0) != self.APIVersion:
-            Logger.log("w", "Plugin %s uses an incompatible API version, ignoring", plugin_id)
-            del self._metadata[plugin_id]
+            Logger.log("w", "Plugin %s uses an incompatible API version, disabling", plugin_id)
+            self.disablePlugin(plugin_id)
             return
 
         try:
