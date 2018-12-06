8d249a4530e83111ea831d9ffe0f2a160fa852e6
Jaime van Kessel <nallath@gmail.com>
2018-12-06 20:07:33 +0100

    Add property to GroupDecorator to prevent the node from deleting itself when it's empty

--- UM/Scene/GroupDecorator.py
+++ UM/Scene/GroupDecorator.py
@@ -7,11 +7,12 @@ if TYPE_CHECKING:
 
 
 class GroupDecorator(SceneNodeDecorator):
-    def __init__(self) -> None:
+    def __init__(self, remove_when_empty: bool = True) -> None:
         super().__init__()
         # Used to keep track of previous parent when an empty group removes itself from the scene.
         # We keep this option so that it's possible to undo it.
         self._old_parent = None  # type: Optional[SceneNode]
+        self._remove_when_empty = remove_when_empty
 
     def setNode(self, node: "SceneNode") -> None:
         super().setNode(node)
@@ -27,6 +28,9 @@ class GroupDecorator(SceneNodeDecorator):
     def _onChildrenChanged(self, node: "SceneNode") -> None:
         if self._node is None:
             return
+        if not self._remove_when_empty:
+            return
+
         if not self._node.hasChildren():
             # A group that no longer has children may remove itself from the scene
             self._old_parent = self._node.getParent()
