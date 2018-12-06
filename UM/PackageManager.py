8ebd76ac836643d45377ca3d31119f8135f7858f
Jaime van Kessel <nallath@gmail.com>
2018-12-06 17:22:37 +0100

    Prevent a bad zip curapackage from bringing down the application

--- UM/PackageManager.py
+++ UM/PackageManager.py
@@ -368,23 +368,26 @@ class PackageManager(QObject):
     # Gets package information from the given file.
     def getPackageInfo(self, filename: str) -> Dict[str, Any]:
         package_json = {}  # type: Dict[str, Any]
-        with zipfile.ZipFile(filename) as archive:
-            # Go through all the files and use the first successful read as the result
-            for file_info in archive.infolist():
-                if file_info.filename.endswith("package.json"):
-                    Logger.log("d", "Found potential package.json file '%s'", file_info.filename)
-                    try:
-                        with archive.open(file_info.filename, "r") as f:
-                            package_json = json.loads(f.read().decode("utf-8"))
+        try:
+            with zipfile.ZipFile(filename) as archive:
+                # Go through all the files and use the first successful read as the result
+                for file_info in archive.infolist():
+                    if file_info.filename.endswith("package.json"):
+                        Logger.log("d", "Found potential package.json file '%s'", file_info.filename)
+                        try:
+                            with archive.open(file_info.filename, "r") as f:
+                                package_json = json.loads(f.read().decode("utf-8"))
 
-                        # Add by default properties
-                        package_json["is_active"] = True
-                        package_json["is_bundled"] = False
-                        package_json["is_installed"] = False
-                        break
-                    except:
-                        Logger.logException("e", "Failed to load potential package.json file '%s' as text file.",
-                                            file_info.filename)
+                            # Add by default properties
+                            package_json["is_active"] = True
+                            package_json["is_bundled"] = False
+                            package_json["is_installed"] = False
+                            break
+                        except:
+                            Logger.logException("e", "Failed to load potential package.json file '%s' as text file.",
+                                                file_info.filename)
+        except zipfile.BadZipFile:
+            Logger.logException("e", "Failed to unpack the file %s", filename)
         return package_json
 
     # Gets the license file content if present in the given package file.
