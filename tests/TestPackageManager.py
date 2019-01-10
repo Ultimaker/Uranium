bdb89dd0a03945aa2b448a4b09d8b66774a4bc2e
Lipu Fei <lipu.fei815@gmail.com>
2019-01-10 14:47:07 +0100

    Add unit test for _comparePackageVersions
    CURA-6005

---
+++ tests/TestPackageManager.py
@@ -0,0 +1,55 @@
+# Copyright (c) 2018 Ultimaker B.V.
+# Uranium is released under the terms of the LGPLv3 or higher.
+
+import pytest
+import unittest.mock
+
+from UM.PackageManager import PackageManager
+
+
+@unittest.mock.patch.object(PackageManager, "__init__", lambda *args, **kwargs: None)
+def test_comparePackageVersions():
+    test_cases = [
+        # same versions
+        {"info_dict1": {"sdk_version": "1",
+                        "package_version": "1.0"},
+         "info_dict2": {"sdk_version": "1",
+                        "package_version": "1.0"},
+         "expected_result": 0},
+
+        # same package versions, different sdk versions
+        {"info_dict1": {"sdk_version": "1",
+                        "package_version": "1.0"},
+         "info_dict2": {"sdk_version": "3",
+                        "package_version": "1.0"},
+         "expected_result": -1},
+
+        # different package versions, same sdk versions
+        {"info_dict1": {"sdk_version": "1",
+                        "package_version": "3.0"},
+         "info_dict2": {"sdk_version": "1",
+                        "package_version": "1.0"},
+         "expected_result": 1},
+
+        # different package versions, different sdk versions  #1
+        {"info_dict1": {"sdk_version": "1",
+                        "package_version": "3.0"},
+         "info_dict2": {"sdk_version": "3",
+                        "package_version": "1.0"},
+         "expected_result": -1},
+
+        # different package versions, different sdk versions  #2
+        {"info_dict1": {"sdk_version": "7",
+                        "package_version": "3.0"},
+         "info_dict2": {"sdk_version": "3",
+                        "package_version": "6.0"},
+         "expected_result": -1},
+    ]
+
+    package_manager = PackageManager()
+    for test_case_dict in test_cases:
+        info_dict1 = test_case_dict["info_dict1"]
+        info_dict2 = test_case_dict["info_dict2"]
+        expected_result = test_case_dict["expected_result"]
+
+        assert expected_result == package_manager._comparePackageVersions(info_dict1, info_dict2)
