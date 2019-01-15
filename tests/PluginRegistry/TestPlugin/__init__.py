df6a3204642cb466ecb6ae93e256697230f8937a
Lipu Fei <lipu.fei815@gmail.com>
2019-01-15 11:19:24 +0100

    Remove heading whitespaces
    CURA-6116

--- tests/PluginRegistry/TestPlugin/__init__.py
+++ tests/PluginRegistry/TestPlugin/__init__.py
@@ -1,4 +1,4 @@
-    # Copyright (c) 2015 Ultimaker B.V.
+# Copyright (c) 2015 Ultimaker B.V.
 # Uranium is released under the terms of the LGPLv3 or higher.
 
 from UM.PluginObject import PluginObject
