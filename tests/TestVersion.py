dc5d51af4def5f450df07cf1edc6660a63e43b44
Jaime van Kessel <nallath@gmail.com>
2019-01-25 17:30:56 +0100

    Added a few missing branches that weren't tested yet

--- tests/TestVersion.py
+++ tests/TestVersion.py
@@ -5,7 +5,7 @@ from UM.Version import Version
 from unittest import TestCase
 import pytest
 
-major_versions = [Version("1"), Version(b"1"), Version(1), Version([1]), Version(["1"]), Version("1."), Version("MOD-1"), Version("1B")]
+major_versions = [Version("1"), Version(b"1"), Version(1), Version([1]), Version(["1"]), Version("1."), Version("MOD-1"), Version("1B"), Version(Version("1"))]
 major_minor_versions = [Version("1.2"), Version("1-2"), Version("1_2"), Version(b"1.2"), Version("1.2BETA"), Version([1, 2]), Version(["1", "2"]), Version([1, "2"]), Version([1, b"2"])]
 major_minor_revision_versions = [Version("1.2.3"), Version("1.2.3BETA"), Version("1_2-3"), Version(b"1.2.3"), Version([1, 2, 3]), Version(["1", "2", "3"]), Version([1, "2", 3]), Version("MOD-1.2.3"), Version(["1", 2, b"3"])]
 
@@ -65,6 +65,28 @@ def test_versionPostfix():
     assert version <= Version("1.2.3-alpha.5")
     assert version < Version("1.2.3-alpha.5")
 
+
 def test_versionWeirdCompares():
     version = Version("1.2.3-alpha.4")
     assert not version == 12
+
+
+def test_wrongType():
+    version = Version(None)
+    assert version == Version("0")
+
+def test_compareStrings():
+    version_string = "1.0.0"
+    version = Version(version_string)
+    assert version == version_string
+    assert version >= version_string
+
+    assert version < "2.0.0"
+    assert version <= "2.0.0"
+    assert "0" < version
+
+    assert Version("1.0.0") > Version("1.0.0-alpha.7")
+
+    # Defend people from ignoring the typing.
+    assert not version > None
+    assert not version < None
