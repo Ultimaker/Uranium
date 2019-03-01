d0224bf40e21eb39785491d88ed86c9ae5e5e8a6
Jaime van Kessel <nallath@gmail.com>
2019-03-01 17:24:15 +0100

    Add few tests for the vector operations

--- tests/Math/TestVector.py
+++ tests/Math/TestVector.py
@@ -18,16 +18,15 @@ class TestVector(unittest.TestCase):
 
     def test_getData(self):
         numpy.testing.assert_array_almost_equal(self._vector.getData(), numpy.array([1,0,0]))
-        pass
 
     def test_angleBetweenVectors(self):
         second_vector = Vector(1,0,0)
         third_vector = Vector(0,1,0)
         fourth_vector = Vector(0,0,1)
         # Check if angle with itself is 0
-        self.assertEqual(self._vector.angleToVector(second_vector),0)
+        self.assertEqual(self._vector.angleToVector(second_vector), 0)
         # Check if angle between the two vectors that are rotated in equal angle but different direction are the same
-        self.assertEqual(self._vector.angleToVector(third_vector),self._vector.angleToVector(fourth_vector))
+        self.assertEqual(self._vector.angleToVector(third_vector), self._vector.angleToVector(fourth_vector))
 
     def test_normalize(self):
         vector = Vector(10, 10, 10)
@@ -48,6 +47,41 @@ class TestVector(unittest.TestCase):
 
         self.assertEqual(Vector(0, -1, 0), -v)
         self.assertEqual(Vector(0, 1, 0), v) # - should have no side effects
+
+    def test_compare(self):
+        short_vector = Vector(0, 1, 0)
+        long_vector = Vector(200, 300, 500)
+
+        assert short_vector < long_vector
+        assert long_vector > short_vector
+        assert not long_vector == None
+
+    def test_add(self):
+        vector = Vector(0, 1, 0)
+        vector2 = Vector(1, 0, 1)
+        assert vector + vector2 == Vector(1, 1, 1)
+        assert vector + vector2.getData() == Vector(1, 1, 1)
+        vector += vector2
+        assert vector == Vector(1, 1, 1)
+
+    def test_subtract(self):
+        vector = Vector(2, 2, 2)
+        vector2 = Vector(1, 1, 1)
+        assert vector - vector2 == Vector(1, 1, 1)
+        assert vector - vector2.getData() == Vector(1, 1, 1)
+        assert vector2 - vector == Vector(-1, -1, -1)
+        assert vector2 - vector.getData() == Vector(-1, -1, -1)
+        vector -= vector2
+        assert vector == Vector(1, 1, 1)
+
+    def test_multiply(self):
+        vector = Vector(2, 2, 2)
+        vector2 = Vector(2, 2, 2)
+        assert vector * vector2 == Vector(4, 4, 4)
+        assert vector * 2 == Vector(4, 4, 4)
+        assert vector.scale(vector2) == Vector(4, 4, 4)
+        vector *= vector2
+        assert vector == Vector(4, 4, 4)
 
 if __name__ == "__main__":
     unittest.main()
