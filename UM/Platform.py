8928bbc4f8666fe491590a683716b28c2dba6126
digitalfrost <gerald.hofmaier@gmail.com>
2019-08-17 16:55:54 +0200

    Simplify Platform class

--- UM/Platform.py
+++ UM/Platform.py
@@ -2,40 +2,35 @@
 # Cura is released under the terms of the LGPLv3 or higher.
 
 import sys
-
+import collections 
 
 ##  Convenience class to simplify OS checking and similar platform-specific handling.
 class Platform:
-    class PlatformType:
-        Windows = 1
-        Linux = 2
-        OSX = 3
-        Other = 4
+
+    platforms = collections.defaultdict(lambda : '4')  # Return 4 if the OS is not Windows, Linux or OSX
+    platforms = {'win32': 1,    #Windows
+                 'linux': 2,    #Linux
+                 'darwin': 3,   #OSX
+                }
+
+    __platform_type = platforms[sys.platform]
 
     ##  Check to see if we are currently running on OSX.
     @classmethod
     def isOSX(cls) -> bool:
-        return cls.__platform_type == cls.PlatformType.OSX
+        return cls.__platform_type == cls.platforms['darwin']
 
     ##  Check to see if we are currently running on Windows.
     @classmethod
     def isWindows(cls) -> bool:
-        return cls.__platform_type == cls.PlatformType.Windows
+        return cls.__platform_type == cls.platforms['win32']
 
     ##  Check to see if we are currently running on Linux.
     @classmethod
     def isLinux(cls) -> bool:
-        return cls.__platform_type == cls.PlatformType.Linux
+        return cls.__platform_type == cls.platforms['linux']
 
     ##  Get the platform type.
     @classmethod
     def getType(cls) -> int:
         return cls.__platform_type
-
-    __platform_type = PlatformType.Other
-    if sys.platform == "win32":
-        __platform_type = PlatformType.Windows
-    elif sys.platform == "linux":
-        __platform_type = PlatformType.Linux
-    elif sys.platform == "darwin":
-        __platform_type = PlatformType.OSX
