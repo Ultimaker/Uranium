d506bcbae316cffca9f0cfdcb9679b0537ebafa9
digitalfrost <gerald.hofmaier@gmail.com>
2019-08-19 14:55:53 +0200

    pylint: disable=unsubscriptable-object
    The warning is because args in initialized as None
    See https://github.com/PyCQA/pylint/issues/1498
    False positive with unsubscriptable-object...

--- UM/PluginError.py
+++ UM/PluginError.py
@@ -13,12 +13,12 @@ class PluginError(Exception):
 ##  Raised when a plugin could not be found.
 class PluginNotFoundError(PluginError):
     def __str__(self):
-        name = self.args[0]
+        name = self.args[0]     # pylint: disable=unsubscriptable-object
         return "Could not find plugin " + name
 
 
 ##  Raised when a plugin provides incorrect metadata.
 class InvalidMetaDataError(PluginError):
    def __str__(self):
-        name = self.args[0]
+        name = self.args[0]     # pylint: disable=unsubscriptable-object
         return "Invalid metadata for plugin " + name
