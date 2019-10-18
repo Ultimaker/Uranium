915ef903a7c169d26a459ed8f08c3da914f31998
Remco Burema <r.burema@ultimaker.com>
2019-10-18 17:00:27 +0200

    Update selection tests with face selection.
    part of 3T-sessions

--- tests/Scene/TestSelection.py
+++ tests/Scene/TestSelection.py
@@ -10,19 +10,23 @@ from UM.Operations.TranslateOperation import TranslateOperation
 
 class TestSelection(TestCase):
     def setUp(self):
+        Selection.clearFace()
         Selection.clear()
 
     def tearDown(self):
+        Selection.clearFace()
         Selection.clear()
 
     def test_addRemoveSelection(self):
         node_1 = SceneNode()
         Selection.add(node_1)
+        Selection.setFace(node_1, 99)
 
         assert Selection.getAllSelectedObjects() == [node_1]
 
         Selection.remove(node_1)
         assert Selection.getAllSelectedObjects() == []
+        assert Selection.getSelectedFace() is None
 
     def test_getSelectedObject(self):
         node_1 = SceneNode()
@@ -76,3 +80,59 @@ class TestSelection(TestCase):
         assert node_1.getPosition() == Vector(20, 40, 60)
         # And node 2 only once
         assert node_2.getPosition() == Vector(10, 20, 30)
+
+    def test_faceSelectMode(self):
+        Selection.hoverFaceChanged = MagicMock()
+
+        Selection.setFaceSelectMode(True)
+        assert Selection.getFaceSelectMode()
+
+        Selection.setFaceSelectMode(True)
+        Selection.setFaceSelectMode(False)
+        assert not Selection.getFaceSelectMode()
+        assert Selection.hoverFaceChanged.emit.call_count == 2
+
+    def test_toggleFace(self):
+        Selection.selectedFaceChanged = MagicMock()
+        node_1 = SceneNode()
+        node_2 = SceneNode()
+
+        assert Selection.getSelectedFace() is None
+
+        Selection.toggleFace(node_1, 91)
+        assert Selection.getSelectedFace() is (node_1, 91)
+        Selection.toggleFace(node_2, 92)
+        assert Selection.getSelectedFace() is (node_2, 92)
+        Selection.toggleFace(node_2, 93)
+        assert Selection.getSelectedFace() is (node_2, 93)
+        Selection.toggleFace(node_2, 93)
+        assert Selection.getSelectedFace() is None
+        Selection.toggleFace(node_2, 93)
+        assert Selection.getSelectedFace() is (node_2, 93)
+
+        Selection.clearFace()
+        assert Selection.getSelectedFace() is None
+
+        assert Selection.selectedFaceChanged.emit.call_count == 5
+
+    def test_hoverFace(self):
+        Selection.hoverFaceChanged = MagicMock()
+        node_1 = SceneNode()
+
+        assert Selection.getHoverFace() is None
+
+        Selection.hoverFace(node_1, 81)
+        Selection.hoverFace(node_1, 81)
+        assert Selection.getHoverFace() is (node_1, 81)
+        assert Selection.selectedFaceChanged.emit.call_count == 1
+
+        Selection.unhoverFace()
+        assert Selection.getHoverFace() is None
+
+        Selection.hoverFace(node_1, 82)
+        Selection.hoverFace(node_1, 83)
+        assert Selection.selectedFaceChanged.emit.call_count == 4
+
+        Selection.clearFace()
+        assert Selection.getHoverFace() is None
+        assert Selection.selectedFaceChanged.emit.call_count == 5
