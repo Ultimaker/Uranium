fc06826dd715293835c77fd8b5acba03581bb221
Lipu Fei <lipu.fei815@gmail.com>
2019-12-19 16:07:39 +0100

    Add timeout handling (experimental) in HttpRequestManager
    CURA-6387

--- UM/TaskManagement/HttpRequestManager.py
+++ UM/TaskManagement/HttpRequestManager.py
@@ -3,10 +3,11 @@
 
 from collections import deque
 from threading import RLock
+import time
 import uuid
 from typing import Callable, Deque, Dict, Set, Union, Optional
 
-from PyQt5.QtCore import QObject, QUrl, Qt
+from PyQt5.QtCore import QObject, QUrl, Qt, QTimer
 from PyQt5.QtNetwork import QNetworkAccessManager, QNetworkRequest, QNetworkReply
 
 from UM.Logger import Logger
@@ -25,6 +26,7 @@ __all__ = ["HttpRequestData", "HttpRequestManager"]
 #  - error_callback (optional): The callback function for handling errors.
 #  - download_progress_callback (optional): The callback function for handling download progress.
 #  - upload_progress_callback (optional): The callback function for handling upload progress.
+#  - timeout (optional): The timeout in seconds for this request. Must be a positive number if present.
 #  - reply: The QNetworkReply for this request. It will only present after this request gets processed.
 #
 class HttpRequestData:
@@ -35,6 +37,7 @@ class HttpRequestData:
                  error_callback: Optional[Callable[["QNetworkReply", "QNetworkReply.NetworkError"], None]] = None,
                  download_progress_callback: Optional[Callable[[int, int], None]] = None,
                  upload_progress_callback: Optional[Callable[[int, int], None]] = None,
+                 timeout: Optional[float] = None,
                  reply: Optional["QNetworkReply"] = None) -> None:
         self._request_id = request_id
         self.http_method = http_method.lower()
@@ -44,11 +47,44 @@ class HttpRequestData:
         self.error_callback = error_callback
         self.download_progress_callback = download_progress_callback
         self.upload_progress_callback = upload_progress_callback
+        self._timeout = timeout
         self.reply = reply
+
+        # The timestamp when this request was initially issued to the QNetworkManager. This field to used to track and
+        # manage timeouts (if set) for the requests.
+        self._start_time = None  # type: Optional[float]
+        self._timeout_time = None  # type: Optional[float]  # For convenience, = start_time + timeout
+        self.is_aborted_due_to_timeout = False
 
     @property
     def request_id(self) -> str:
         return self._request_id
+
+    @property
+    def timeout(self) -> Optional[float]:
+        return self._timeout
+
+    @property
+    def timeout_time(self) -> Optional[float]:
+        return self._timeout_time
+
+    # Sets the start time of this request. This is called when this request is issued to the QNetworkManager.
+    def setStartTime(self, start_time: float) -> None:
+        self._start_time = start_time
+        if self._timeout is not None:
+            self._timeout_time = self._start_time + self._timeout
+
+    # Checks with the given timestamp (in seconds) to see if this request has exceeded its timeout.
+    # If no timeout is specified for this request, this check will return False.
+    def hasExceededTimeout(self, now: float) -> bool:
+        if self._timeout_time is None:
+            return False
+        return now > self._timeout_time
+
+    def getTimeToTimeout(self, now: float) -> Optional[float]:
+        if self._timeout_time is None:
+            return None
+        return self._timeout_time - now
 
     # Since Qt 5.12, pyqtSignal().connect() will return a Connection instance that represents a connection. This
     # Connection instance can later be used to disconnect for cleanup purpose. We are using Qt 5.10 and this feature
@@ -77,10 +113,11 @@ class HttpRequestData:
             if len(self.data) > 10:
                 data += "..."
 
-        return "request[{id}][{method}][{url}][{data}]".format(id = self._request_id[:8],
-                                                               method = self.http_method,
-                                                               url = self.request.url(),
-                                                               data = data)
+        return "request[{id}][{method}][{url}][timeout={timeout}][{data}]".format(id = self._request_id[:8],
+                                                                                  method = self.http_method,
+                                                                                  url = self.request.url(),
+                                                                                  timeout = self._timeout,
+                                                                                  data = data)
 
 
 #
@@ -102,6 +139,12 @@ class HttpRequestData:
 #  - upload_progress_callback: This function will be invoked whenever the upload progress changed. (bound to
 #       QNetworkReply.downloadProgress signal)
 #       Its signature should be "def callback(bytesSent: int, bytesTotal: int) -> None" or other compatible form.
+#
+#  - timeout (EXPERIMENTAL): The timeout is seconds for a request. This is the timeout since the request was first
+#       issued to the QNetworkManager. NOTE that this timeout is NOT the timeout between each response from the other
+#       party, but the timeout for the complete request. So, if you have a very slow network which takes 2 hours to
+#       download a 1MB file, and for this request you set a timeout of 10 minutes, the request will be aborted after
+#       10 minutes if it's not finished.
 #
 class HttpRequestManager(QObject):
 
@@ -134,6 +177,11 @@ class HttpRequestManager(QObject):
         self._request_lock = RLock()
         self._process_requests_scheduled = False
 
+        # Timer for managing timeouts.
+        self._timeout_timer = QTimer(parent = self)
+        self._timeout_timer.setSingleShot(True)
+        self._timeout_timer.timeout.connect(self._triggerTimeoutCheck)
+
     # Public API for creating an HTTP GET request.
     # Returns an HttpRequestData instance that represents this request.
     def get(self, url: str,
@@ -141,11 +189,13 @@ class HttpRequestManager(QObject):
             callback: Optional[Callable[["QNetworkReply"], None]] = None,
             error_callback: Optional[Callable[["QNetworkReply", "QNetworkReply.NetworkError"], None]] = None,
             download_progress_callback: Optional[Callable[[int, int], None]] = None,
-            upload_progress_callback: Optional[Callable[[int, int], None]] = None) -> "HttpRequestData":
+            upload_progress_callback: Optional[Callable[[int, int], None]] = None,
+            timeout: Optional[float] = None) -> "HttpRequestData":
         return self._createRequest("get", url, headers_dict = headers_dict,
                                    callback = callback, error_callback = error_callback,
                                    download_progress_callback = download_progress_callback,
-                                   upload_progress_callback = upload_progress_callback)
+                                   upload_progress_callback = upload_progress_callback,
+                                   timeout = timeout)
 
     # Public API for creating an HTTP PUT request.
     # Returns an HttpRequestData instance that represents this request.
@@ -155,11 +205,13 @@ class HttpRequestManager(QObject):
             callback: Optional[Callable[["QNetworkReply"], None]] = None,
             error_callback: Optional[Callable[["QNetworkReply", "QNetworkReply.NetworkError"], None]] = None,
             download_progress_callback: Optional[Callable[[int, int], None]] = None,
-            upload_progress_callback: Optional[Callable[[int, int], None]] = None) -> "HttpRequestData":
+            upload_progress_callback: Optional[Callable[[int, int], None]] = None,
+            timeout: Optional[float] = None) -> "HttpRequestData":
         return self._createRequest("put", url, headers_dict = headers_dict, data = data,
                                    callback = callback, error_callback = error_callback,
                                    download_progress_callback = download_progress_callback,
-                                   upload_progress_callback = upload_progress_callback)
+                                   upload_progress_callback = upload_progress_callback,
+                                   timeout = timeout)
 
     # Public API for creating an HTTP POST request. Returns a unique request ID for this request.
     # Returns an HttpRequestData instance that represents this request.
@@ -169,11 +221,13 @@ class HttpRequestManager(QObject):
              callback: Optional[Callable[["QNetworkReply"], None]] = None,
              error_callback: Optional[Callable[["QNetworkReply", "QNetworkReply.NetworkError"], None]] = None,
              download_progress_callback: Optional[Callable[[int, int], None]] = None,
-             upload_progress_callback: Optional[Callable[[int, int], None]] = None) -> "HttpRequestData":
+             upload_progress_callback: Optional[Callable[[int, int], None]] = None,
+             timeout: Optional[float] = None) -> "HttpRequestData":
         return self._createRequest("post", url, headers_dict = headers_dict, data = data,
                                    callback = callback, error_callback = error_callback,
                                    download_progress_callback = download_progress_callback,
-                                   upload_progress_callback = upload_progress_callback)
+                                   upload_progress_callback = upload_progress_callback,
+                                   timeout = timeout)
 
     # Public API for aborting a given HttpRequestData. If the request is not pending or in progress, nothing
     # will be done.
@@ -185,7 +239,7 @@ class HttpRequestManager(QObject):
 
             # If the request is currently in progress, abort it.
             if request in self._current_requests:
-                if request.reply is not None:
+                if request.reply is not None and request.reply.isRunning():
                     request.reply.abort()
                     Logger.log("d", "%s aborted", request)
 
@@ -198,7 +252,12 @@ class HttpRequestManager(QObject):
                        callback: Optional[Callable[["QNetworkReply"], None]] = None,
                        error_callback: Optional[Callable[["QNetworkReply", "QNetworkReply.NetworkError"], None]] = None,
                        download_progress_callback: Optional[Callable[[int, int], None]] = None,
-                       upload_progress_callback: Optional[Callable[[int, int], None]] = None) -> "HttpRequestData":
+                       upload_progress_callback: Optional[Callable[[int, int], None]] = None,
+                       timeout: Optional[float] = None) -> "HttpRequestData":
+        assert(timeout is None or timeout > 0)
+        if timeout is not None and timeout <= 0:
+            raise ValueError("Timeout must be a positive number if provided, but [%s] was given" % timeout)
+
         request = QNetworkRequest(QUrl(url))
 
         # Make sure that Qt handles redirects
@@ -225,7 +284,8 @@ class HttpRequestManager(QObject):
                                        callback = callback,
                                        error_callback = error_callback,
                                        download_progress_callback = download_progress_callback,
-                                       upload_progress_callback = upload_progress_callback)
+                                       upload_progress_callback = upload_progress_callback,
+                                       timeout = timeout)
 
         with self._request_lock:
             Logger.log("d", "%s has been queued", request_data)
@@ -238,6 +298,44 @@ class HttpRequestManager(QObject):
                 Logger.log("d", "process requests call has been scheduled")
 
         return request_data
+
+    # For easier debugging, so you know when the call is triggered by the timeout timer.
+    def _triggerTimeoutCheck(self) -> None:
+        Logger.log("d", "Timeout checkpoint triggered.")
+        self._checkRequetsForTimeouts()
+
+    def _checkRequetsForTimeouts(self) -> None:
+        has_timeout_timer_rescheduled = False
+        with self._request_lock:
+            now = time.time()
+            # Sort all currently in progress requests by their remaining times to timeout.
+            #  - Abort the requests that have exceeded the timeout limit.
+            #  - Schedule the timeout Timer to the next most recent timeout as a checkpoint.
+            requests_with_timeout = filter(lambda r: r.timeout is not None, self._current_requests)
+            sorted_requests = sorted(requests_with_timeout, key = lambda r: r.getTimeToTimeout(now))
+            for request in sorted_requests:
+                if request.hasExceededTimeout(now):
+                    # Abort the requests that have exceeded the timeout limit.
+                    if request.reply is not None and request.reply.isRunning():
+                        request.reply.abort()
+                        request.is_aborted_due_to_timeout = True
+                        Logger.log("d", "%s aborted due to timeout [%s sec]", request, request.timeout)
+                else:
+                    # Schedule the timeout Timer to the next most recent timeout as a checkpoint.
+                    next_check_time = request.getTimeToTimeout(now) + 1.0  # TODO: doc this, use 1 extra second margin.
+                    self._timeout_timer.stop()
+                    self._timeout_timer.setInterval(next_check_time * 1000)
+                    self._timeout_timer.start()
+                    has_timeout_timer_rescheduled = True
+                    Logger.log("d", "Timeout timer scheduled in [%s] sec for request", request)
+                    break
+
+        if not has_timeout_timer_rescheduled:
+            self._timeout_timer.stop()
+
+        Logger.log("d", "Timeout check done.")
+        # We don't need to reschedule _processRequest() because abort() will trigger finished() which will
+        # eventually try to schedule _processRequest().
 
     # Processes the next request in the pending queue. Stops if there is no more pending requests. It also stops if
     # the maximum number of concurrent requests has been reached.
@@ -263,6 +361,7 @@ class HttpRequestManager(QObject):
     # Processes the given HttpRequestData by issuing the request using QNetworkAccessManager and moves the
     # request into the currently in-progress list.
     def _processRequest(self, request_data: "HttpRequestData") -> None:
+        now = time.time()
         Logger.log("d", "Start processing %s", request_data)
 
         # get the right http_method function and prepare arguments.
@@ -285,6 +384,8 @@ class HttpRequestManager(QObject):
 
         with self._request_lock:
             self._current_requests.add(request_data)
+            request_data.setStartTime(now)
+            self._checkRequetsForTimeouts()
 
     def _onRequestError(self, request_data: "HttpRequestData", error: "QNetworkReply.NetworkError") -> None:
         error_string = None
@@ -314,10 +415,16 @@ class HttpRequestManager(QObject):
 
         # continue to process the next request
         self._processRequestsInQueue()
+        self._checkRequetsForTimeouts()  # TODO: optimize this
 
     def _onRequestFinished(self, request_data: "HttpRequestData") -> None:
-        # Do nothing if a request was aborted.
-        if request_data.reply is not None and request_data.reply.error() == QNetworkReply.OperationCanceledError:
+        # See https://doc.qt.io/archives/qt-5.10/qnetworkreply.html#abort
+        # Calling QNetworkReply.abort() will also trigger finished(), so we need to know if a request was finished or
+        # aborted. This can be done by checking if the error is QNetworkReply.OperationCanceledError.
+        #
+        #  - Do nothing if the request was aborted by the user.
+        #  - Call the timeout callback if the request was aborted due to timeout.
+        if request_data.reply is not None and  request_data.reply.error() == QNetworkReply.OperationCanceledError:
             Logger.log("d", "%s was aborted, do nothing", request_data)
             return
 
@@ -345,3 +452,4 @@ class HttpRequestManager(QObject):
 
         # continue to process the next request
         self._processRequestsInQueue()
+        self._checkRequetsForTimeouts()  # TODO: optimize this
