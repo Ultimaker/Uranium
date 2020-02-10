816fbd8d0d14dd621500bd0a8221edffdc1c33c9
Ghostkeeper <rubend@tutanota.com>
2020-02-10 11:35:16 +0100

    Increment SDK version to 7.1
    Contributes to issue CURA-7205.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,6 @@
-# Copyright (c) 2018 Ultimaker B.V.
+# Copyright (c) 2020 Ultimaker B.V.
 # Uranium is released under the terms of the LGPLv3 or higher.
+
 from typing import cast
 from unittest.mock import MagicMock
 
@@ -19,7 +20,7 @@ class FixtureApplication(Application):
     engineCreatedSignal = Signal()
 
     def __init__(self):
-        super().__init__(name = "test", version = "1.0", api_version = "7.0.0")
+        super().__init__(name = "test", version = "1.0", api_version = "7.1.0")
         super().initialize()
         Signal._signalQueue = self
 
