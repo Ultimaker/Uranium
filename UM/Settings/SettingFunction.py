52c2f8004f373d346e556c607976c04a128ada04
Jaime van Kessel <nallath@gmail.com>
2020-03-03 14:32:53 +0100

    Fix typing

--- UM/Settings/SettingFunction.py
+++ UM/Settings/SettingFunction.py
@@ -221,7 +221,7 @@ class _SettingExpressionVisitor(ast.NodeVisitor):
         """
         raise IllegalMethodError("Slices are not allowed")
 
-    def visit_Str(self, node: ast.AST) -> None:
+    def visit_Str(self, node: ast.Str) -> None:
         """This one is used before Python 3.8 to visit string types.
         
         visit_Str will be marked as deprecated from Python 3.8 and onwards.
@@ -234,7 +234,7 @@ class _SettingExpressionVisitor(ast.NodeVisitor):
         if node.s not in self._knownNames and node.s not in dir(builtins):  # type: ignore #AST uses getattr stuff, so ignore type of node.s.
             self.keys.add(node.s)  # type: ignore
 
-    def visit_Constant(self, node: ast.AST) -> None:
+    def visit_Constant(self, node: ast.Constant) -> None:
         """This one is used on Python 3.8+ to visit string types."""
         # The blacklisting is done just in case (All function calls should be whitelisted. The blacklist is to make
         # extra sure that certain calls are *not* made!)
