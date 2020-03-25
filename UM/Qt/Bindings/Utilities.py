0294e4b3b54e043f0d8d2dfd1a3a2a168043f69a
Kostas Karmas <konskarm@gmail.com>
2020-03-25 16:52:54 +0100

    Split the openUrl function for testability
    Added an extra function that does the checking for valid and allowed
    url schemes. This way we can assume that QDesktopServices.openUrl works
    as intended, and we can test only the function that checks whether a
    url scheme is allowed and valid (urlHasValidScheme).
    
    CURA-7310

--- UM/Qt/Bindings/Utilities.py
+++ UM/Qt/Bindings/Utilities.py
@@ -16,34 +16,52 @@ class UrlUtil(QObject):
     """
     Helper class used to open URLs from QML.
     """
-    __valid_uri_schemes = ("http", "https")
+    __valid_uri_schemes = ("http", "https")  # Schemes considered valid to be used when calling the custom openUrl
 
     def __init__(self, parent = None):
         super().__init__(parent)
 
-    @pyqtSlot(str, list)
-    def openUrl(self, target_url: str, schemes: List[str]) -> bool:
+    @pyqtSlot(str, list, result = bool)
+    def openUrl(self, target_url: str, allowed_schemes: List[str]) -> bool:
         """
-        Checks whether the target_url has an allowed scheme and, if it does, it opens the URL. If the target_url has a
-        disallowed or invalid scheme, then it logs an error. This function can be called inside QML files.
+        Opens the target_url if it has an allowed and valid scheme. This function can be called inside QML files.
 
         :param target_url: The URL string to be opened e.g. 'https://example.org'
-        :param schemes: A list of the schemes that are allowed to be opened e.g. ['http', 'https']
+        :param allowed_schemes: A list of the schemes that are allowed to be opened e.g. ['http', 'https']
         :return: True if the URL opens successfully, False if an invalid scheme is used
         """
-        allowed_schemes = set()
-        for s in schemes:
-            if s in self.__valid_uri_schemes:
-                allowed_schemes.add(s)
-        parse_result = urlparse(target_url)
+        if self.urlHasValidScheme(target_url, allowed_schemes):
+            QDesktopServices.openUrl(QUrl(target_url))
+            return True
+        return False
+
+    def urlHasValidScheme(self, url: str, input_schemes: List[str]) -> bool:
+        """
+        Checks if the scheme of the url is in the valid URL schemes and whether it has been allowed. If an invalid
+        scheme was attempted to be allowed, the function logs a warning. If the url has a disallowed scheme, it logs an
+        error.
+
+        :param url: URL string to be checked
+        :param input_schemes: A list of the schemes that are allowed to be opened e.g. ['http', 'https']
+        :return: True if the URL has a scheme that is allowed and valid, False otherwise
+        """
+        allowed_schemes = set(self.__valid_uri_schemes) & set(input_schemes)
+        if set(input_schemes) - allowed_schemes:
+            Logger.log("w", "Attempted to allow invalid schemes {invalid}. Valid URL schemes that can be allowed "
+                            "are {valid}.".format(
+                                                invalid=set(input_schemes) - allowed_schemes,
+                                                valid=self.__valid_uri_schemes)
+                       )
+        parse_result = urlparse(url)
         if parse_result.scheme in allowed_schemes:
-            return QDesktopServices.openUrl(QUrl(target_url))
+            return True
         else:
-            Logger.log("e", "Attempted to open URL '{uri}'. The scheme '{scheme}' is not in the allowed schemes '"
-                            "{allowed_schemes}'.".format(
-                                                            uri = target_url,
-                                                            scheme = parse_result.scheme,
-                                                            allowed_schemes = allowed_schemes))
+            Logger.log("e", "Attempted to open URL '{uri}'. The scheme '{scheme}' is not in the allowed schemes "
+                            "{allowed_schemes}.".format(
+                                                        uri=url,
+                                                        scheme=parse_result.scheme,
+                                                        allowed_schemes=allowed_schemes)
+                       )
             return False
 
 
