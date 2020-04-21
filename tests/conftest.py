55f087e7e062acf9ee94a109c9c7c1e3875b4de7
Nino van Hooff <ninovanhooff@gmail.com>
2020-04-21 10:38:30 +0200

    Update sdk version to 7.1.1
    CURA-7383

--- tests/conftest.py
+++ tests/conftest.py
@@ -20,7 +20,7 @@ class FixtureApplication(Application):
     engineCreatedSignal = Signal()
 
     def __init__(self):
-        super().__init__(name = "test", version = "1.0", api_version = "7.1.0")
+        super().__init__(name = "test", version = "1.0", api_version = "7.1.1")
         super().initialize()
         Signal._signalQueue = self
 
