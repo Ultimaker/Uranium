1ef3d207afc3fb0e27f1f6b187069ed6c1d560b2
Ghostkeeper <rubend@tutanota.com>
2020-04-23 13:28:39 +0200

    Add typing
    Found during investigation of Sentry issue CURA-KT.

--- UM/Operations/RotateOperation.py
+++ UM/Operations/RotateOperation.py
@@ -1,22 +1,25 @@
-# Copyright (c) 2015 Ultimaker B.V.
+# Copyright (c) 2020 Ultimaker B.V.
 # Uranium is released under the terms of the LGPLv3 or higher.
 
+from typing import Union, TYPE_CHECKING
 from UM.Scene.SceneNode import SceneNode
 from UM.Math.Vector import Vector
 
 from . import Operation
 
+if TYPE_CHECKING:
+    from UM.Math.Quaternion import Quaternion  # To store the amount of rotation in each dimension.
+
 class RotateOperation(Operation.Operation):
     """Operation that rotates a scene node."""
 
-    def __init__(self, node, rotation, rotate_around_point = Vector(0, 0, 0)):
+    def __init__(self, node: SceneNode, rotation: "Quaternion", rotate_around_point: Vector = Vector(0, 0, 0)) -> None:
         """Initialises the operation.
         
         :param node: The node to rotate.
         :param rotation: A transformation quaternion that rotates a space. This
         rotation is applied on the node.
-        :param kwargs: Key-word arguments, including:
-        - rotate_around_point: A point around which to rotate the node.
+        :param rotate_around_point: The point to rotate around.
         """
 
         super().__init__()
@@ -25,19 +28,19 @@ class RotateOperation(Operation.Operation):
         self._rotation = rotation #A rotation matrix to rotate the node with.
         self._rotate_around_point = rotate_around_point #Around what point should the rotation be done?
 
-    def undo(self):
+    def undo(self) -> None:
         """Undoes the rotation, rotating the node back."""
 
         self._node.setTransformation(self._old_transformation)
 
-    def redo(self):
+    def redo(self) -> None:
         """Redoes the rotation, rotating the node again."""
 
         self._node.setPosition(-self._rotate_around_point)
         self._node.rotate(self._rotation, SceneNode.TransformSpace.World)
         self._node.setPosition(self._rotate_around_point)
 
-    def mergeWith(self, other):
+    def mergeWith(self, other: "RotateOperation") -> Union[bool, "RotateOperation"]:
         """Merges this operation with another RotateOperation.
         
         This prevents the user from having to undo multiple operations if they
@@ -59,7 +62,7 @@ class RotateOperation(Operation.Operation):
         op._old_transformation = other._old_transformation #Use the _old_transformation of the oldest rotation in the new operation.
         return op
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         """Returns a programmer-readable representation of this operation.
         
         :return: A programmer-readable representation of this operation.
