89f887b2e0ccf7f5b8ab227bc7cd41d6e77625cf
Christian Clauss <cclauss@me.com>
2020-07-02 16:27:35 +0200

    Update profile_getbytearray.py

--- tests/benchmarks/MeshData/profile_getbytearray.py
+++ tests/benchmarks/MeshData/profile_getbytearray.py
@@ -1,6 +1,7 @@
 # Copyright (c) 2015 Ultimaker B.V.
 # Uranium is released under the terms of the LGPLv3 or higher.
 
+import UM.FlameProfiler import profile
 from UM.Mesh.MeshData import MeshData
 
 @profile
