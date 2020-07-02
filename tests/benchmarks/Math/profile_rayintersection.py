756955762037b947b08a691f504b3259399cb1aa
Christian Clauss <cclauss@me.com>
2020-07-02 16:30:35 +0200

    from UM.FlameProfiler import profile

--- tests/benchmarks/Math/profile_rayintersection.py
+++ tests/benchmarks/Math/profile_rayintersection.py
@@ -1,6 +1,7 @@
 # Copyright (c) 2015 Ultimaker B.V.
 # Uranium is released under the terms of the LGPLv3 or higher.
 
+from UM.FlameProfiler import profile
 from UM.Math.AxisAlignedBox import AxisAlignedBox
 from UM.Math.Ray import Ray
 from UM.Math.Vector import Vector
