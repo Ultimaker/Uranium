278ea8136cf07bb3e56ec64eb23f5e52d1527352
Ghostkeeper <rubend@tutanota.com>
2020-09-02 00:06:34 +0200

    Make scale origin optional
    Currently we always provide it, but if I want to use Uranium as library I'd want to not always have to think about this.
    
    Contributes to issue CURA-7118.

--- UM/Math/Polygon.py
+++ UM/Math/Polygon.py
@@ -133,16 +133,19 @@ class Polygon:
         point_matrix += point_on_axis
         return Polygon(point_matrix.getA()[::-1])
 
-    def scale(self, factor: float, origin: List[float]) -> "Polygon":
+    def scale(self, factor: float, origin: Optional[List[float]] = None) -> "Polygon":
         """
         Scales this polygon around a certain origin point.
         :param factor: The scaling factor.
         :param origin: Origin point around which to scale. As the scale factor
         approaches 0, all coordinates will approach this origin point. As the
         scale factor grows, all coordinates will move away from this origin
-        point.
+        point. If `None`, the 0,0 coordinate will be used.
         :return: A transformed polygon.
         """
+        if origin is None:
+            origin = [0, 0, 0]
+
         transformation = numpy.identity(4) * factor  # Just the scaling matrix.
         delta_scale = factor - 1
         transformation[3][0] = delta_scale * -origin[0]
