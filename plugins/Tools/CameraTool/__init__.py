6af78532bcb87d5edf8093f02eee314c51154e1e
Jaime van Kessel <nallath@gmail.com>
2021-04-02 17:00:13 +0200

    Move arcus import to test of the cameratool
    This fixes the cameratool tests due to the pycapsule issue

--- plugins/Tools/CameraTool/__init__.py
+++ plugins/Tools/CameraTool/__init__.py
@@ -1,11 +1,6 @@
 # Copyright (c) 2021 Ultimaker B.V.
 # Uranium is released under the terms of the LGPLv3 or higher.
 
-# Workaround for a race condition on certain systems where there
-# is a race condition between Arcus and PyQt. Importing Arcus
-# first seems to prevent Sip from going into a state where it
-# tries to create PyQt objects on a non-main thread.
-import Arcus  # @UnusedImport
 from . import CameraTool
 
 
