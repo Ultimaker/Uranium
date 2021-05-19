2a52a76562f275a2182a9e2c988e0b97b18b8cd8
Ghostkeeper <rubend@tutanota.com>
2021-05-19 16:51:00 +0200

    Cast subtypes of union in _setUniformValueDirect
    Mypy isn't able to figure that out by itself from the if statement checking for the type.
    
    Contributes to Sentry issue CURA-24H.

--- UM/View/GL/ShaderProgram.py
+++ UM/View/GL/ShaderProgram.py
@@ -3,7 +3,7 @@
 
 import ast
 import configparser
-from typing import List, Union
+from typing import cast, List, Union
 
 from PyQt5.QtGui import QOpenGLShader, QOpenGLShaderProgram, QVector2D, QVector3D, QVector4D, QMatrix4x4, QColor
 from UM.Logger import Logger
@@ -334,23 +334,30 @@ class ShaderProgram:
     def _matrixToQMatrix4x4(self, m):
         return QMatrix4x4(m.getData().flatten())
 
-    def _setUniformValueDirect(self, uniform: int, value: Union[Vector, Matrix, Color, List[List[float]], float]) -> None:
+    def _setUniformValueDirect(self, uniform: int, value: Union[Vector, Matrix, Color, List[float], List[List[float]], float]) -> None:
         if type(value) is Vector:
+            value = cast(Vector, value)
             self._shader_program.setUniformValue(uniform, QVector3D(value.x, value.y, value.z))
         elif type(value) is Matrix:
-            self._shader_program.setUniformValue(uniform, self._matrixToQMatrix4x4(value))
+            self._shader_program.setUniformValue(uniform, self._matrixToQMatrix4x4(cast(Matrix, value)))
         elif type(value) is Color:
+            value = cast(Color, value)
             self._shader_program.setUniformValue(uniform,
                 QColor(round(value.r * 255), round(value.g * 255), round(value.b * 255), round(value.a * 255)))
-        elif type(value) is list and type(value[0]) is list and len(value[0]) == 4:
+        elif type(value) is list and type(cast(List[List[float]], value)[0]) is list and len(cast(List[List[float]], value)[0]) == 4:
+            value = cast(List[List[float]], value)
             self._shader_program.setUniformValue(uniform, self._matrixToQMatrix4x4(Matrix(value)))
-        elif type(value) is list and len(value) == 2:
+        elif type(value) is list and len(cast(List[float], value)) == 2:
+            value = cast(List[float], value)
             self._shader_program.setUniformValue(uniform, QVector2D(value[0], value[1]))
-        elif type(value) is list and len(value) == 3:
+        elif type(value) is list and len(cast(List[float], value)) == 3:
+            value = cast(List[float], value)
             self._shader_program.setUniformValue(uniform, QVector3D(value[0], value[1], value[2]))
-        elif type(value) is list and len(value) == 4:
+        elif type(value) is list and len(cast(List[float], value)) == 4:
+            value = cast(List[float], value)
             self._shader_program.setUniformValue(uniform, QVector4D(value[0], value[1], value[2], value[3]))
-        elif type(value) is list and type(value[0]) is list and len(value[0]) == 2:
+        elif type(value) is list and type(cast(List[List[float]], value)[0]) is list and len(cast(List[List[float]], value)[0]) == 2:
+            value = cast(List[List[float]], value)
             self._shader_program.setUniformValueArray(uniform, [QVector2D(i[0], i[1]) for i in value])
         else:
-            self._shader_program.setUniformValue(uniform, value)
+            self._shader_program.setUniformValue(uniform, cast(float, value))
