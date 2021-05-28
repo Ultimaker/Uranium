0e917a77beedd4d92605ca55c65e7341aabc00c6
Jaime van Kessel <nallath@gmail.com>
2021-05-28 15:44:44 +0200

    Move files to storage based on storage file
    Right now it just provides a bit of convenience, but we also need this
    so that we can still have plugins being signed. Currently signed plugins
    can't use the storage, since it modifies them. The signining validation
    will break after the move
    
    CURA-8270

--- UM/PluginRegistry.py
+++ UM/PluginRegistry.py
@@ -15,6 +15,7 @@ from typing import Any, Callable, Dict, List, Optional, Tuple, TYPE_CHECKING
 from PyQt5.QtCore import QCoreApplication
 from PyQt5.QtCore import QObject, pyqtSlot, QUrl, pyqtProperty, pyqtSignal
 
+from UM.CentralFileStorage import CentralFileStorage
 from UM.Logger import Logger
 from UM.Message import Message
 from UM.Platform import Platform
@@ -624,6 +625,24 @@ class PluginRegistry(QObject):
                 if current_version > highest_version:
                     highest_version = current_version
                     final_location = loc
+
+
+        # Move data (if any) to central storage
+        central_storage_file = os.path.join(final_location, plugin_id, "central_storage.json")
+        if os.path.exists(central_storage_file):
+            try:
+                with open(central_storage_file, "r", encoding="utf-8") as file_stream:
+                    self._handleStoringFiles(file_stream.read(), os.path.join(final_location, plugin_id))
+
+            except:
+                print("NOPE NOPE")
+                pass
+
+        '''if "files_for_central_storage" in plugin_metadata:
+            Logger.log("d", f"Moving files to central storage for {plugin_id}")
+            for file_path, file_id, version_string, hash in plugin_metadata["files_for_central_storage"]:
+                CentralFileStorage.store(file_path, file_id, Version(version_string))'''
+
         try:
             file, path, desc = imp.find_module(plugin_id, [final_location])
         except Exception:
@@ -677,6 +696,15 @@ class PluginRegistry(QObject):
                 return os.path.abspath(os.path.join(folder_path, ".."))
 
         return None
+
+    def _handleStoringFiles(self, file_data, plugin_path):
+        try:
+            file_manifest = json.loads(file_data)
+        except json.decoder.JSONDecodeError:
+            Logger.logException("e", "Failed to parse central_storage.json")
+
+        for file_to_move in file_manifest:
+            CentralFileStorage.store(os.path.join(plugin_path, file_to_move[0]), file_to_move[1], Version(file_to_move[2]))
 
     #   Load the plugin data from the stream and in-place update the metadata.
     def _parsePluginInfo(self, plugin_id, file_data, meta_data):
