0a9c201db7fd886936bc075d7c71a9e1d4dcc5c3
Konstantinos Karmas <konskarm@gmail.com>
2021-06-08 15:30:12 +0200

    Fix failing test
    Since the hash is only tested on Enterprise edition, we need to make sure that it is set as enterprise before retrieving the wrong hash.
    
    CURA-8291

--- tests/TestCentralFileStorage.py
+++ tests/TestCentralFileStorage.py
@@ -91,10 +91,11 @@ def test_retrieveNonExistent():
     with unittest.mock.patch("UM.Resources.Resources.getDataStoragePath", lambda: "test_central_storage/4.9"):
         pytest.raises(FileNotFoundError, lambda: CentralFileStorage.retrieve("non_existent_file", "0123456789ABCDEF"))
 
-def test_retrieveWrongHash():
+def test_retrieveWrongHashOnEnterprise():
     """
     Tests retrieving a file that has a wrong hash.
     """
     with unittest.mock.patch("UM.Resources.Resources.getDataStoragePath", lambda: "test_central_storage/4.9"):
+        CentralFileStorage.setIsEnterprise(True)
         CentralFileStorage.store(TEST_FILE_PATH, "myfile")
         pytest.raises(IOError, lambda: CentralFileStorage.retrieve("myfile", TEST_FILE_HASH2))
