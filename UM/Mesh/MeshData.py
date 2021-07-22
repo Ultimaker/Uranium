550f7fc283acd844ca65b2745f683251cbc09a04
Jaime van Kessel <nallath@gmail.com>
2021-07-22 14:52:35 +0200

    Gracefully handle if convexhull could not be created due to tempfile issue
    Note that this doesn't actually fix the issue, it just prevents it from pulling
    down the entire application with it.
    
    Contributes to  Ultimaker/Cura#9644

--- UM/Mesh/MeshData.py
+++ UM/Mesh/MeshData.py
@@ -515,8 +515,10 @@ def createConvexHull(vertex_data: numpy.ndarray) -> scipy.spatial.ConvexHull:
         Logger.log("w", "Loaded model may be low-dimensional, apply QJ to make it full dimensional.")
         try:
             hull_result = scipy.spatial.ConvexHull(vertex_data, qhull_options="QJ")
-        except scipy.spatial.qhull.QhullError as e:
-            Logger.log("e", "Couldn't construct convex hull around mesh (using faux hull instead): {err}".format(err = str(e)))
+        except (scipy.spatial.qhull.QhullError, OSError):
+            # The OS error can sometimes occur on windows.
+            # See https://github.com/scipy/scipy/issues/8850 for more info.
+            Logger.logException("e", "Couldn't construct convex hull around mesh (using faux hull instead)")
             hull_result = scipy.spatial.ConvexHull(numpy.array([[0, 0, 0], [0, 0, 1], [0, 1, 0], [0, 1, 1], [1, 0, 0], [1, 0, 1], [1, 1, 0], [1, 1, 1]]))  # Cube, known to always succeed.
     return hull_result
 
