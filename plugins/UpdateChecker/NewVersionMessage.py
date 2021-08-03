47bc828b5afda40118c222e88e7819fe225147cb
Jaime van Kessel <nallath@gmail.com>
2021-08-03 12:56:13 +0200

    Apply suggestions from code review
    CURA-7689
    
    Co-authored-by: Konstantinos Karmas <konskarm@gmail.com>

--- plugins/UpdateChecker/NewVersionMessage.py
+++ plugins/UpdateChecker/NewVersionMessage.py
@@ -9,17 +9,17 @@ class NewVersionMessage(Message):
     def __init__(self, application_display_name: str, newest_version: Version) -> None:
         super().__init__(
             text = I18N_CATALOG.i18nc("@info:status",
-                                        "{application_name} {version_number} is available!".format(
-                                        application_name=application_display_name,
-                                        version_number=newest_version)),
+                                        "{application_name} {version_number} is available!").format(
+                                        application_name = application_display_name,
+                                        version_number = newest_version),
             title = I18N_CATALOG.i18nc("@info:status",
-                                         "{application_name} {version_number} provides a better and more reliable printing experience.".format(
+                                         "{application_name} {version_number} provides a better and more reliable printing experience.").format(
                                          application_name = application_display_name,
-                                         version_number = newest_version)))
+                                         version_number = newest_version))
 
         self.addAction("download", I18N_CATALOG.i18nc("@action:button", "Download"), "[no_icon]", "[no_description]")
 
         self.addAction("new_features", I18N_CATALOG.i18nc("@action:button", "Learn more"), "[no_icon]",
                           "[no_description]",
-                          button_style=Message.ActionButtonStyle.LINK,
-                          button_align=Message.ActionButtonAlignment.ALIGN_LEFT)
+                          button_style = Message.ActionButtonStyle.LINK,
+                          button_align = Message.ActionButtonAlignment.ALIGN_LEFT)
