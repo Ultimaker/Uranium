b504fd6eec76deda4d917c961e2cc99b6e2c920b
Jelle Spijker <spijker.jelle@gmail.com>
2021-08-10 17:02:36 +0200

    Simplified recipe
    This is basically a header only recipe and can be seen as a pure
    Python package. just copying the UM, plugins and resources
    should be enough

--- conanfile.py
+++ conanfile.py
@@ -15,12 +15,9 @@ class UraniumConan(ConanFile):
     topics = ("conan", "python", "pyqt5", "qt", "3d-graphics", "3d-models", "python-framework")
     settings = "os", "compiler", "build_type", "arch"
     exports = "LICENSE"
-    options = {
-        "python_version": "ANY"
-    }
-    default_options = {
-        "python_version": "3.8"
-    }
+    exports_sources = "plugins/*", "resources/*", "UM/*"
+    no_copy_source = True
+
     scm = {
         "type": "git",
         "subfolder": ".",
@@ -28,44 +25,14 @@ class UraniumConan(ConanFile):
         "revision": "auto"
     }
 
-    def configure(self):
-        self.options["Arcus"].python_version = self.options.python_version
-
-    def build_requirements(self):
-        self.build_requires("cmake/[>=3.16.2]")
-
     def requirements(self):
         self.requires(f"Arcus/4.10.0@ultimaker/testing")
 
-    def generate(self):
-        cmake = CMakeDeps(self)
-        cmake.generate()
-
-        tc = CMakeToolchain(self)
-        tc.variables["Python_VERSION"] = self.options.python_version
-        tc.generate()
-
-    _cmake = None
-
-    def configure_cmake(self):
-        if self._cmake:
-            return self._cmake
-        self._cmake = CMake(self)
-        self._cmake.configure()
-        return self._cmake
-
-    def build(self):
-        cmake = self.configure_cmake()
-        cmake.build()
-
     def package(self):
-        cmake = self.configure_cmake()
-        cmake.install()
-        self.copy("*", src = os.path.join(self.package_folder, "lib", f"python{self.options.python_version}",
-                                          "site-packages"), dst = "site-packages")
-        self.copy("*", src = os.path.join(self.package_folder, "lib", "uranium"), dst = "site-packages")
+        self.copy("*", src = os.path.join(self.source_folder, "plugins"), dst = os.path.join("site-packages", "plugins"))
+        self.copy("*", src = os.path.join(self.source_folder, "resources"), dst = os.path.join("site-packages", "resources"))
+        self.copy("*", src = os.path.join(self.source_folder, "UM"), dst = os.path.join("site-packages", "UM"))
         self.copy("*.cmake", src = os.path.join(self.package_folder, "share"), dst = "cmake", keep_path=False)
-        tools.rmdir(os.path.join(self.package_folder, "lib"))
 
     def package_info(self):
         if self.in_local_cache:
