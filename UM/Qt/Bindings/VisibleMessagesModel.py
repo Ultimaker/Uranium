26465f6d399a5b609d6f3e69125a6b4dbbbac3f6
Jaime van Kessel <nallath@gmail.com>
2021-08-31 12:58:19 +0200

    Import union from typing
    CURA-8500

--- UM/Qt/Bindings/VisibleMessagesModel.py
+++ UM/Qt/Bindings/VisibleMessagesModel.py
@@ -1,5 +1,6 @@
-# Copyright (c) 2015 Ultimaker B.V.
+# Copyright (c) 2021 Ultimaker B.V.
 # Uranium is released under the terms of the LGPLv3 or higher.
+from typing import Union
 
 from PyQt5.QtCore import Qt, QUrl
 
