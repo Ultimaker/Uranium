40f5e31b333f158b0a58b95cfe72518e6348418b
Jelle Spijker <spijker.jelle@gmail.com>
2021-09-08 11:15:58 +0200

    Bump up to 4.11

--- conanfile.py
+++ conanfile.py
@@ -7,22 +7,18 @@ from conan.tools.cmake import CMakeToolchain, CMakeDeps, CMake
 
 class UraniumConan(ConanFile):
     name = "Uranium"
-    version = "4.10.0"
+    version = "4.11.0"
     license = "LGPL-3.0"
     author = "Ultimaker B.V."
     url = "https://github.com/Ultimaker/uranium"
     description = "A Python framework for building Desktop applications."
     topics = ("conan", "python", "pyqt5", "qt", "3d-graphics", "3d-models", "python-framework")
     settings = "os", "compiler", "build_type", "arch"
+    revision_mode = "scm"
+    build_policy = "missing"
     exports = "LICENSE"
     exports_sources = "plugins/*", "resources/*", "UM/*"
     no_copy_source = True
-    options = {
-        "python_version": "ANY"
-    }
-    default_options = {
-        "python_version": "3.8"
-    }
     scm = {
         "type": "git",
         "subfolder": ".",
@@ -30,11 +26,9 @@ class UraniumConan(ConanFile):
         "revision": "auto"
     }
 
-    def configure(self):
-        self.options["Arcus"].python_version = self.options.python_version
-
     def requirements(self):
-        self.requires(f"Arcus/4.10.0@ultimaker/testing")
+        self.requires(f"Python/3.8.10@python/testing")
+        self.requires(f"Arcus/4.11.0@ultimaker/testing")
 
     def package(self):
         self.copy("*", src = os.path.join(self.source_folder, "plugins"), dst = os.path.join("site-packages", "plugins"))
