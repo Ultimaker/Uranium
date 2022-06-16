f5adb0f9afffd9cc895080aca555578d23974ebc
j.delarago <joeydelarago@gmail.com>
2022-06-16 16:05:36 +0200

    Revert fix from CURA-7154 for numpy-stl failing on linux which should be resolved by the upgrade to python 3.9.
    CURA-9321

--- plugins/FileHandlers/STLReader/STLReader.py
+++ plugins/FileHandlers/STLReader/STLReader.py
@@ -1,9 +1,8 @@
-# Copyright (c) 2020 Ultimaker B.V.
+# Copyright (c) 2022 Ultimaker B.V.
 # Copyright (c) 2013 David Braam
 # Uranium is released under the terms of the LGPLv3 or higher.
 
 import os
-import platform
 import struct
 
 import numpy
@@ -15,23 +14,8 @@ from UM.Mesh.MeshBuilder import MeshBuilder
 from UM.Mesh.MeshReader import MeshReader
 from UM.MimeTypeDatabase import MimeTypeDatabase, MimeType
 from UM.Scene.SceneNode import SceneNode
-
-use_numpystl = False
-
-try:
-    # Work around for CURA-7154. Some models crash with a segfault, but only when packed.
-    if platform.system() != "Linux":
-        import stl  # numpy-stl lib
-        import stl.mesh
-
-        # Increase max count. (100 million should be okay-ish)
-        stl.stl.MAX_COUNT = 100000000
-        use_numpystl = True
-    else:
-        Logger.log("w", "Not loading numpy-stl due to linux issue")
-except ImportError:
-    Logger.log("w", "Could not find numpy-stl, falling back to slower code.")
-    # We have our own fallback code.
+import stl  # numpy-stl lib
+import stl.mesh
 
 
 class STLReader(MeshReader):
@@ -47,28 +31,11 @@ class STLReader(MeshReader):
         )
         self._supported_extensions = [".stl"]
 
-    def load_file(self, file_name, mesh_builder, _use_numpystl = False):
-        file_read = False
-        if _use_numpystl:
-            Logger.log("i", "Using NumPy-STL to load STL data.")
-            try:
-                self._loadWithNumpySTL(file_name, mesh_builder)
-                file_read = True
-            except:
-                Logger.logException("e", "Reading file failed with Numpy-STL!")
-
-        if not file_read:
-            Logger.log("i", "Using legacy code to load STL data.")
-            f = open(file_name, "rb")
-            if not self._loadBinary(mesh_builder, f):
-                f.close()
-                f = open(file_name, "rt", encoding = "utf-8")
-                try:
-                    self._loadAscii(mesh_builder, f)
-                except UnicodeDecodeError:
-                    return None
-                f.close()
-            Job.yieldThread()  # Yield somewhat to ensure the GUI has time to update a bit.
+    def load_file(self, file_name, mesh_builder):
+        try:
+            self._loadWithNumpySTL(file_name, mesh_builder)
+        except:
+            Logger.logException("e", "Reading stl file failed.")
 
         mesh_builder.calculateNormals(fast = True)
         mesh_builder.setFileName(file_name)
@@ -79,28 +46,28 @@ class STLReader(MeshReader):
         mesh_builder = MeshBuilder()
         scene_node = SceneNode()
 
-        self.load_file(file_name, mesh_builder, _use_numpystl = use_numpystl)
+        self.load_file(file_name, mesh_builder)
 
         mesh = mesh_builder.build()
 
-        if use_numpystl:
+        verts = mesh.getVertices()
+        # In some cases numpy stl reads incorrectly and the result is that the Z values are all 0
+        # Add new error cases if you find them.
+        if numpy.amin(verts[:, 1]) == numpy.amax(verts[:, 1]):
+            # Something may have gone wrong in numpy stl, start over without numpy stl
+            Logger.log("w", "All Z coordinates are the same using numpystl, trying again without numpy stl.")
+            mesh_builder = MeshBuilder()
+            self.load_file(file_name, mesh_builder)
+            mesh = mesh_builder.build()
             verts = mesh.getVertices()
-            # In some cases numpy stl reads incorrectly and the result is that the Z values are all 0
-            # Add new error cases if you find them.
-            if numpy.amin(verts[:, 1]) == numpy.amax(verts[:, 1]):
-                # Something may have gone wrong in numpy stl, start over without numpy stl
-                Logger.log("w", "All Z coordinates are the same using numpystl, trying again without numpy stl.")
-                mesh_builder = MeshBuilder()
-                self.load_file(file_name, mesh_builder, _use_numpystl = False)
-                mesh = mesh_builder.build()
 
-                verts = mesh.getVertices()
-                if numpy.amin(verts[:, 1]) == numpy.amax(verts[:, 1]):
-                    Logger.log("e", "All Z coordinates are still the same without numpy stl... let's hope for the best")
+            if numpy.amin(verts[:, 1]) == numpy.amax(verts[:, 1]):
+                Logger.log("e", "All Z coordinates are still the same without numpy stl... let's hope for the best")
 
         if mesh_builder.getVertexCount() == 0:
             Logger.log("d", "File did not contain valid data, unable to read.")
             return None  # We didn't load anything.
+
         scene_node.setMeshData(mesh)
         Logger.log("d", "Loaded a mesh with %s vertices", mesh_builder.getVertexCount())
 
--- plugins/FileHandlers/STLReader/tests/TestStlReader.py
+++ plugins/FileHandlers/STLReader/tests/TestStlReader.py
@@ -16,14 +16,13 @@ def test_readASCII():
         result = reader.read(ascii_path)
     assert result
 
-    if STLReader.use_numpystl:
-        # If the system the test runs on supports numpy stl, we should also check the non numpy stl option.
-        f = open(ascii_path, "rt", encoding = "utf-8")
-        mesh_builder = MeshBuilder()
-        reader._loadAscii(mesh_builder, f)
-        mesh_builder.calculateNormals(fast=True)
+    # If the system the test runs on supports numpy stl, we should also check the non numpy stl option.
+    f = open(ascii_path, "rt", encoding = "utf-8")
+    mesh_builder = MeshBuilder()
+    reader._loadAscii(mesh_builder, f)
+    mesh_builder.calculateNormals(fast=True)
 
-        assert mesh_builder.getVertexCount() != 0
+    assert mesh_builder.getVertexCount() != 0
 
 
 def test_readBinary():
@@ -32,13 +31,12 @@ def test_readBinary():
     with patch("UM.Application.Application.getInstance"):
         result = reader.read(binary_path)
 
-    if STLReader.use_numpystl:
-        # If the system the test runs on supporst numpy stl, we should also check the non numpy stl option.
-        f = open(binary_path, "rb")
-        mesh_builder = MeshBuilder()
-        reader._loadBinary(mesh_builder, f)
-        mesh_builder.calculateNormals(fast=True)
+    # If the system the test runs on supporst numpy stl, we should also check the non numpy stl option.
+    f = open(binary_path, "rb")
+    mesh_builder = MeshBuilder()
+    reader._loadBinary(mesh_builder, f)
+    mesh_builder.calculateNormals(fast=True)
 
-        assert mesh_builder.getVertexCount() != 0
+    assert mesh_builder.getVertexCount() != 0
     assert result
 
