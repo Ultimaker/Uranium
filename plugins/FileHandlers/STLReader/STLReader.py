5ae038a1baa2b7f0716cff02045ffaa5042eb150
j.delarago <joeydelarago@gmail.com>
2022-06-20 11:23:57 +0200

    Remove unused ascii and binary loading function for stl files Remove test for these removed functions
    Revert removing increased maximum vertex count for numpy-stl so that stl files with up to one hundred million vertexes can be loaded (default is ten million, too low!)
    
    CURA-9321

--- plugins/FileHandlers/STLReader/STLReader.py
+++ plugins/FileHandlers/STLReader/STLReader.py
@@ -2,13 +2,8 @@
 # Copyright (c) 2013 David Braam
 # Uranium is released under the terms of the LGPLv3 or higher.
 
-import os
-import struct
-
 import numpy
-from typing import cast
 
-from UM.Job import Job
 from UM.Logger import Logger
 from UM.Mesh.MeshBuilder import MeshBuilder
 from UM.Mesh.MeshReader import MeshReader
@@ -17,6 +12,7 @@ from UM.Scene.SceneNode import SceneNode
 import stl  # numpy-stl lib
 import stl.mesh
 
+stl.stl.MAX_COUNT = 100000000
 
 class STLReader(MeshReader):
     def __init__(self) -> None:
@@ -87,70 +83,3 @@ class STLReader(MeshReader):
             self._swapColumns(vertices, 1, 2)
 
             mesh_builder.addVertices(vertices)
-
-    # Private
-    def _loadAscii(self, mesh_builder, f):
-        """Load the STL data from file by considering the data as ascii.
-
-        :param mesh_builder: The MeshData object where the data is written to.
-        :param f: The file handle
-        """
-
-        num_verts = 0
-        for lines in f:
-            for line in lines.split("\r"):
-                if "vertex" in line:
-                    num_verts += 1
-
-        mesh_builder.reserveFaceCount(int(num_verts / 3))
-        f.seek(0, os.SEEK_SET)
-        vertex = 0
-        face = [None, None, None]
-        for lines in f:
-            for line in lines.split("\r"):
-                if "vertex" in line:
-                    face[vertex] = line.split()[1:]
-                    vertex += 1
-                    if vertex == 3:
-                        mesh_builder.addFaceByPoints(
-                            float(face[0][0]), float(face[0][2]), -float(face[0][1]),
-                            float(face[1][0]), float(face[1][2]), -float(face[1][1]),
-                            float(face[2][0]), float(face[2][2]), -float(face[2][1])
-                        )
-                        vertex = 0
-
-                Job.yieldThread()
-
-    # Private
-    def _loadBinary(self, mesh_builder, f):
-        """Load the STL data from file by consdering the data as Binary.
-        :param mesh: The MeshData object where the data is written to.
-        :param f: The file handle
-        """
-
-        f.read(80)  # Skip the header
-
-        try:
-            num_faces = cast(int, struct.unpack("<I", f.read(4))[0])
-        except struct.error:  # Can't unpack it if the file didn't have 4 bytes in it.
-            return False
-        # On ascii files, the num_faces will be big, due to 4 ascii bytes being seen as an unsigned int.
-        if num_faces < 1 or num_faces > 1000000000:
-            return False
-        f.seek(0, os.SEEK_END)
-        file_size = f.tell()
-        f.seek(84, os.SEEK_SET)
-        if file_size < num_faces * 50 + 84:
-            return False
-
-        mesh_builder.reserveFaceCount(num_faces)
-        for idx in range(0, num_faces):
-            data = struct.unpack(b"<ffffffffffffH", f.read(50))
-            mesh_builder.addFaceByPoints(
-                data[3], data[5], -data[4],
-                data[6], data[8], -data[7],
-                data[9], data[11], -data[10]
-            )
-            Job.yieldThread()
-
-        return True
