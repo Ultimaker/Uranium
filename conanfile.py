954445c5d03512bcebcb2361340dff1204f9c658
jspijker <j.spijker@ultimaker.com>
2023-02-22 20:19:00 +0100

    Fixed building translations

--- conanfile.py
+++ conanfile.py
@@ -9,20 +9,18 @@ from conan.tools.microsoft import unix_path
 from conan.tools.scm import Version
 from conan.errors import ConanInvalidConfiguration
 
-required_conan_version = ">=1.52.0"
+required_conan_version = ">=1.56.0"
 
 
 class UraniumConan(ConanFile):
     name = "uranium"
     license = "LGPL-3.0"
-    author = "Ultimaker B.V."
+    author = "UltiMaker"
     url = "https://github.com/Ultimaker/uranium"
     description = "A Python framework for building Desktop applications."
-    topics = ("conan", "python", "pyqt5", "qt", "3d-graphics", "3d-models", "python-framework")
-    build_policy = "missing"
+    topics = ("conan", "python", "pyqt6", "qt", "3d-graphics", "3d-models", "python-framework")
     exports = "LICENSE*"
     settings = "os", "compiler", "build_type", "arch"
-    no_copy_source = True
 
     python_requires = "umbase/[>=0.1.7]@ultimaker/stable"
     python_requires_extend = "umbase.UMBaseConanfile"
@@ -33,37 +31,6 @@ class UraniumConan(ConanFile):
     default_options = {
         "devtools": False,
     }
-
-    def export_sources(self):
-        copy(self, "plugins", self.recipe_folder, self.export_sources_folder)
-        copy(self, "resources", self.recipe_folder, self.export_sources_folder)
-        copy(self, "tests", self.recipe_folder, self.export_sources_folder)
-        copy(self, "UM", self.recipe_folder, self.export_sources_folder)
-        copy(self, "requirements.txt", self.recipe_folder, self.export_sources_folder)
-        copy(self, "requirements-dev.txt", self.recipe_folder, self.export_sources_folder)
-
-    def set_version(self):
-        if self.version is None:
-            self.version = self._umdefault_version()
-
-    def configure(self):
-        self.options["pyarcus"].shared = True
-        self.options["cpython"].shared = True
-
-    def validate(self):
-        if self.version:
-            if Version(self.version) <= Version("4"):
-                raise ConanInvalidConfiguration("Only versions 5+ are support")
-
-    def requirements(self):
-        self.requires("pyarcus/5.2.2")
-        self.requires("cpython/3.10.4")
-
-    def build_requirements(self):
-        if self.options.devtools:
-            if self.settings.os != "Windows" or self.conf.get("tools.microsoft.bash:path", check_type = str):
-                # FIXME: once m4, autoconf, automake are Conan V2 ready use self.win_bash and add gettext as base tool_requirement
-                self.tool_requires("gettext/0.21", force_host_context=True)
 
     @property
     def _base_dir(self):
@@ -106,28 +73,61 @@ class UraniumConan(ConanFile):
             py_interp = Path(*[f'"{p}"' if " " in p else p for p in py_interp.parts])
         return py_interp
 
-    def build(self):
+    def export_sources(self):
+        copy(self, "*", os.path.join(self.recipe_folder, "plugins"), os.path.join(self.export_sources_folder, "plugins"))
+        copy(self, "*", os.path.join(self.recipe_folder, "resources"), os.path.join(self.export_sources_folder, "resources"), excludes = "*.mo")
+        copy(self, "*", os.path.join(self.recipe_folder, "tests"), os.path.join(self.export_sources_folder, "tests"))
+        copy(self, "*", os.path.join(self.recipe_folder, "UM"), os.path.join(self.export_sources_folder, "UM"))
+        copy(self, "requirements.txt", self.recipe_folder, self.export_sources_folder)
+        copy(self, "requirements-dev.txt", self.recipe_folder, self.export_sources_folder)
+
+    def set_version(self):
+        if self.version is None:
+            self.version = self._umdefault_version()
+
+    def configure(self):
+        self.options["pyarcus"].shared = True
+        self.options["cpython"].shared = True
+
+    def validate(self):
+        if self.version:
+            if Version(self.version) <= Version("4"):
+                raise ConanInvalidConfiguration("Only versions 5+ are support")
+
+    def requirements(self):
+        self.requires("pyarcus/5.2.2")
+        self.requires("cpython/3.10.4")
+
+    def build_requirements(self):
         if self.options.devtools:
             if self.settings.os != "Windows" or self.conf.get("tools.microsoft.bash:path", check_type = str):
                 # FIXME: once m4, autoconf, automake are Conan V2 ready use self.win_bash and add gettext as base tool_requirement
-                for po_file in self.source_path.joinpath("resources", "i18n").glob("**/*.po"):
-                    mo_file = self.build_path.joinpath(po_file.with_suffix('.mo').relative_to(self.source_path))
-                    mkdir(self, str(unix_path(self, mo_file.parent)))
-                    cpp_info = self.dependencies["gettext"].cpp_info
-                    self.run(f"{cpp_info.bindirs[0]}/msgfmt {po_file} -o {mo_file} -f", env="conanbuild", ignore_errors=True)
+                self.tool_requires("gettext/0.21", force_host_context=True)
 
     def generate(self):
-        vb = VirtualBuildEnv(self)
-        vb.generate()
-
         if self.options.devtools:
-            if self.settings.os != "Windows" or self.conf.get("tools.microsoft.bash:path", check_type = str):
+            if self.settings.os != "Windows" or self.conf.get("tools.microsoft.bash:path", check_type=str):
+                vb = VirtualBuildEnv(self)
+                vb.generate()
+
                 # FIXME: once m4, autoconf, automake are Conan V2 ready use self.win_bash and add gettext as base tool_requirement
                 cpp_info = self.dependencies["gettext"].cpp_info
                 for po_file in self.source_path.joinpath("resources", "i18n").glob("**/*.po"):
                     pot_file = self.source_path.joinpath("resources", "i18n", po_file.with_suffix('.pot').name)
                     mkdir(self, str(unix_path(self, pot_file.parent)))
-                    self.run(f"{cpp_info.bindirs[0]}/msgmerge --no-wrap --no-fuzzy-matching -width=140 -o {po_file} {po_file} {pot_file}", env = "conanbuild", ignore_errors=True)
+                    self.run(
+                        f"{cpp_info.bindirs[0]}/msgmerge --no-wrap --no-fuzzy-matching -width=140 -o {po_file} {po_file} {pot_file}",
+                        env="conanbuild", ignore_errors=True)
+
+    def build(self):
+        if self.options.devtools:
+            if self.settings.os != "Windows" or self.conf.get("tools.microsoft.bash:path", check_type = str):
+                # FIXME: once m4, autoconf, automake are Conan V2 ready use self.win_bash and add gettext as base tool_requirement
+                for po_file in self.source_path.joinpath("resources", "i18n").glob("**/*.po"):
+                    mo_file = self.build_path.parent.joinpath(po_file.with_suffix('.mo').relative_to(self.source_path))
+                    mkdir(self, str(unix_path(self, mo_file.parent)))
+                    cpp_info = self.dependencies["gettext"].cpp_info
+                    self.run(f"{cpp_info.bindirs[0]}/msgfmt {po_file} -o {mo_file} -f", env="conanbuild", ignore_errors=True)
 
     def layout(self):
         self.folders.source = "."
@@ -138,19 +138,19 @@ class UraniumConan(ConanFile):
         self.cpp.package.resdirs = ["resources", "plugins", "pip_requirements"]  # Note: pip_requirements should be the last item in the list
 
     def package(self):
-        copy(self, "*", src = self.source_path.joinpath("UM"), dst = self.package_path.joinpath(self.cpp.package.libdirs[0]))
-        copy(self, "*", src = self.source_path.joinpath("resources"), dst = self.package_path.joinpath(self.cpp.package.resdirs[0]), excludes="*.po")
-        copy(self, "*.mo", src = self.build_path.joinpath("resources"), dst = self.package_path.joinpath(self.cpp.package.resdirs[0]))
-        copy(self, "*", src = self.source_path.joinpath("plugins"), dst = self.package_path.joinpath(self.cpp.package.resdirs[1]))
-        copy(self, "requirement*.txt", src = self.source_path, dst = self.package_path.joinpath(self.cpp.package.resdirs[-1]))
+        copy(self, "*", src = os.path.join(self.source_folder, "UM"), dst = os.path.join(self.package_folder, self.cpp.package.libdirs[0]))
+        copy(self, "*", src = os.path.join(self.source_folder, "resources"), dst = os.path.join(self.package_folder, self.cpp.package.resdirs[0]), excludes="*.po")
+        copy(self, "*.mo", src = os.path.join(self.source_folder, "resources"), dst = os.path.join(self.package_folder, self.cpp.package.resdirs[0]))
+        copy(self, "*", src = os.path.join(self.source_folder, "plugins"), dst = os.path.join(self.package_folder, self.cpp.package.resdirs[1]))
+        copy(self, "requirement*.txt", src = self.source_folder, dst = os.path.join(self.package_folder, self.cpp.package.resdirs[-1]))
 
     def package_info(self):
         self.user_info.pip_requirements = "requirements.txt"
         self.user_info.pip_requirements_build = "requirements-dev.txt"
 
         if self.in_local_cache:
-            self.runenv_info.append_path("PYTHONPATH", str(Path(self.cpp_info.lib_paths[0]).parent))
-            self.runenv_info.append_path("PYTHONPATH", self.cpp_info.res_paths[1])
+            self.runenv_info.append_path("PYTHONPATH", os.path.join(self.package_folder, "site-packages"))
+            self.runenv_info.append_path("PYTHONPATH", os.path.join(self.package_folder, "plugins"))
         else:
             self.runenv_info.append_path("PYTHONPATH", self.source_folder)
             self.runenv_info.append_path("PYTHONPATH", os.path.join(self.source_folder, "plugins"))
