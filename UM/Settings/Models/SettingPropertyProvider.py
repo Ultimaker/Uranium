630647d9481ab589d5a3ccbe6b59ec3db9a492aa
saumya.jain <saumya.jain@ultimaker.com>
2023-09-28 16:43:23 +0200

    CURA-10967
    set property value updated to take into account if resolve is present or not.

--- UM/Settings/Models/SettingPropertyProvider.py
+++ UM/Settings/Models/SettingPropertyProvider.py
@@ -181,12 +181,13 @@ class SettingPropertyProvider(QObject):
         return self._stack_levels
 
     @pyqtSlot(str, "QVariant")
-    def setPropertyValue(self, property_name, property_value):
+    def setPropertyValue(self, property_name, property_value, *, skip_resolve = False):
         """Set the value of a property.
 
         :param stack_index: At which level in the stack should this property be set?
         :param property_name: The name of the property to set.
         :param property_value: The value of the property to set.
+        :param skip_resolve: to skip resolve function and force property value
         """
 
         if not self._stack or not self._key:
@@ -220,13 +221,13 @@ class SettingPropertyProvider(QObject):
                         # the value. Note that we only do this when this is not caused by the calculated state
                         # In this case the setting does need to be set, as it needs to be stored in the user settings.
                         self.removeFromContainer(self._store_index)
-                        return
+                        continue
                     else:  # First value that we encountered was different, stop looking & continue as normal.
                         break
 
         # _remove_unused_value is used when the stack value differs from the effective value
         # i.e. there is a resolve function
-        if self._property_map.value(property_name) == property_value and self._remove_unused_value:
+        if self._property_map.value(property_name) == property_value and self._remove_unused_value and not skip_resolve:
             return
 
         container.setProperty(self._key, property_name, property_value)
