e1fa5d4cdac622db7ee5dbbc351dcf4456cba68e
Remco Burema <r.burema@ultimaker.com>
2024-02-28 22:05:20 +0100

    Fix number of calls (tests).
    done as part of CURA-10149

--- tests/Scene/TestSelection.py
+++ tests/Scene/TestSelection.py
@@ -113,7 +113,7 @@ class TestSelection:
         Selection.clearFace()
         assert Selection.getSelectedFace() is None
 
-        assert Selection.selectedFaceChanged.emit.call_count == 6
+        assert Selection.selectedFaceChanged.emit.call_count == 7
 
     def test_hoverFace(self):
         Selection.hoverFaceChanged = MagicMock()
