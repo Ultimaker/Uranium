ddb0484d12497364b1a7fb3a1cdc858aca56729e
Erwan MATHIEU <erwan.mathieu@ultimaker.com>
2024-06-10 15:43:07 +0200

    Bump cpython and conan version

--- conanfile.py
+++ conanfile.py
@@ -9,7 +9,7 @@ from conan.tools.microsoft import unix_path
 from conan.tools.scm import Version
 from conan.errors import ConanInvalidConfiguration
 
-required_conan_version = ">=1.58.0 <2.0.0"
+required_conan_version = ">=1.64.0 <2.0.0"
 
 
 class UraniumConan(ConanFile):
@@ -111,7 +111,7 @@ class UraniumConan(ConanFile):
     def requirements(self):
         for req in self.conan_data["requirements"]:
             self.requires(req)
-        self.requires("cpython/3.10.4@ultimaker/stable")
+        self.requires("cpython/3.12.2")
         self.requires("openssl/3.2.0")
         self.requires("protobuf/3.21.12")
 
